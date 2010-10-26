6116666e7cbcba47c18e97575246b99c5f0a1281
Licho <jalich@gmail.com>
2010-10-26 01:31:40 +0000

    fixed default address in zero-k.info (now points to new site)
    svn path=/trunk/other/; revision=314

--- Zero-K.info/asp.net/Global.asax.cs
+++ Zero-K.info/asp.net/Global.asax.cs
@@ -15,7 +15,7 @@ namespace ZeroKWeb
 
 			//routes.MapRoute("Default", "{controller}/{action}/{id}", new { controller = "Home", action = "Index", id = UrlParameter.Optional });
 			routes.MapRoute("Default", "{controller}.mvc/{action}/{id}", new { controller = "Home", action = "Index", id = UrlParameter.Optional});
-			routes.MapRoute("Root", "", new { controller = "Home", action = "Index", id = "" });
+			//routes.MapRoute("Root", "", new { controller = "Home", action = "Index", id = "" });
 		}
 
 		protected void Application_Start()
