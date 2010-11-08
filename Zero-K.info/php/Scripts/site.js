dedefb5baac6ca2a5c32b2f7fc40ca50fd6fc66d
Licho <jalich@gmail.com>
2010-11-08 01:58:22 +0000

    fixes for icons screwups
    svn path=/trunk/other/; revision=526

---
+++ Zero-K.info/php/Scripts/site.js
@@ -0,0 +1,9 @@
+﻿$(document).ready(function () {
+	$('a.delete').click(function () {
+		var answer = confirm('Really delete?');
+		return answer // answer is a boolean
+	});
+
+	$(".rating").stars();
+});
+
