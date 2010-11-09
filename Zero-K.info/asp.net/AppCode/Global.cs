ca313d3fb03d8ef1c10ee77cc0d4c260fe3c47f1
Licho <jalich@gmail.com>
2010-11-09 02:16:05 +0000

    fixes
    svn path=/trunk/other/; revision=542

--- Zero-K.info/asp.net/AppCode/Global.cs
+++ Zero-K.info/asp.net/AppCode/Global.cs
@@ -9,7 +9,7 @@ namespace ZeroKWeb
 		public static int AccountID { get { if (IsAccountAuthorized) return Account.AccountID; else return 0; } }
 		public static Account Account { get { return HttpContext.Current.User as Account; } }
 		public static bool IsAccountAuthorized { get { return HttpContext.Current.User as Account != null; } }
-		public static bool IsLobbyAccess { get { return HttpContext.Current.Request.Cookies[GlobalConst.LobbyAccessCookieName] != null; } }
+		public static bool IsLobbyAccess { get { return HttpContext.Current.Request.Cookies[GlobalConst.LobbyAccessCookieName] != null && IsAccountAuthorized; } }
 
 
 	}
