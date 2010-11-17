18124fae75af5f409414ab29fe7d315b430942c9
Licho <jalich@gmail.com>
2010-11-17 01:32:15 +0000

    Added EngineDownload class -takes spring version, automatically downloads latest minor revision of given engine version and installs it (to writabledatadir/engine/0.82.6 )
    svn path=/trunk/other/; revision=646

---
+++ Shared/PlasmaDownloader/EngineDownload.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Net;
+using System.Threading;
+using PlasmaShared;
+
+namespace PlasmaDownloader
+{
+  public class EngineDownload: Download
+  {
+    const string EngineDownloadPath = "http://springrts.com/dl/";
+    readonly string version;
+    WebClient wc;
+    readonly string writableDataDirectory;
+
+    public EngineDownload(string version, string writableDataDirectory)
+    {
+      this.writableDataDirectory = writableDataDirectory;
+      this.version = version;
+      Name = "Spring " + version;
+    }
+
+    public void Start()
+    {
+      Utils.StartAsync(() =>
+        {
+          for (var i = 9; i >= 0; i--)
+          {
+            var source = string.Format("{0}spring_{1}.{2}.exe", EngineDownloadPath, version, i > 0 ? i.ToString() : "");
+            // if i==-1 we tested without version number
+            IndividualProgress = 10 - i;
+            if (VerifyFile(source))
+            {
+              wc = new WebClient() { Proxy = null };
+              var target = Path.GetTempFileName() + ".exe";
+              wc.DownloadProgressChanged += (s, e) =>
+                {
+                  Length = (int)(e.TotalBytesToReceive);
+                  IndividualProgress = 10 + 0.8*e.ProgressPercentage;
+                };
+              wc.DownloadFileCompleted += (s, e) =>
+                {
+                  if (e.Cancelled)
+                  {
+                    Trace.TraceInformation("Download {0} cancelled", Name);
+                    Finish(false);
+                  }
+                  else if (e.Error != null)
+                  {
+                    Trace.TraceWarning("Error downloading {0}: {1}", Name, e.Error);
+                    Finish(false);
+                  }
+                  else
+                  {
+                    Trace.TraceInformation("Installing {0}", source);
+                    var timer = new Timer((o) => { IndividualProgress += (100 - IndividualProgress)/10; }, null, 1000, 1000);
+                    var p = new Process();
+                    p.StartInfo = new ProcessStartInfo(target, string.Format("/S /D={0}", Utils.MakePath(writableDataDirectory, "engine", version)));
+                    p.Exited += (s2, e2) =>
+                      {
+                        timer.Dispose();
+                        if (p.ExitCode != 0)
+                        {
+                          Trace.TraceInformation("Install of {0} failed: {1}", Name, p.ExitCode);
+                          Finish(false);
+                        }
+                        else
+                        {
+                          Trace.TraceInformation("Install of {0} complete", Name);
+                          Finish(true);
+                        }
+                      };
+                    p.EnableRaisingEvents = true;
+                    p.Start();
+                  }
+                };
+              Trace.TraceInformation("Downloading {0}", source);
+              wc.DownloadFileAsync(new Uri(source), target, this);
+              return;
+            }
+          }
+          Trace.TraceInformation("Cannot find {0}", Name);
+          Finish(false);
+        });
+    }
+
+
+    static bool VerifyFile(string url)
+    {
+      try
+      {
+        var request = WebRequest.Create(url);
+        request.Method = "HEAD";
+        request.Timeout = 4000;
+        var res = request.GetResponse();
+        var len = res.ContentLength;
+        request.Abort();
+        return len > 100000;
+      }
+      catch (Exception ex)
+      {
+        return false;
+      }
+    }
+  }
+}
