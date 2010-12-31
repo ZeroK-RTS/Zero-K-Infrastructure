b4e0d5afcbfa75e305c86aaac46599dac7eb9393
Licho <jalich@gmail.com>
2010-12-31 00:50:16 +0000

    some link fixes and added lua images
    svn path=/trunk/other/; revision=1117

---
+++ Zero-K.info/asp.net/AppCode/WikiHandler.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+
+namespace ZeroKWeb
+{
+  public class WikiHandler
+  {
+    public static string LoadWiki(string node) {
+      var wc = new WebClient();
+      if (String.IsNullOrEmpty(node)) node = "Intro";
+
+      var ret = wc.DownloadString("http://code.google.com/p/zero-k/wiki/" + node);
+
+      var idx = ret.IndexOf("<div id=\"wikiheader\"");
+      if (idx > -1) idx = ret.IndexOf("</span>", idx) + 7;
+      var idx2 = ret.LastIndexOf("<div class=\"collapse\">");
+      if (idx2 == -1) idx2 = ret.LastIndexOf("<br>");
+
+      if (idx > -1 && idx2 > -1) ret = ret.Substring(idx, idx2-idx);
+
+      ret = ret.Replace("href=\"/p/zero-k/wiki/", "href=\"");
+      ret = ret.Replace("href=\"/", "href=\"http://code.google.com/");
+
+
+      ret = String.Concat("<div class=''><div>", ret);
+      return ret;
+    }
+  }
+}
--- Zero-K.info/asp.net/Controllers/WikiController.cs
+++ Zero-K.info/asp.net/Controllers/WikiController.cs
@@ -9,20 +9,7 @@ namespace ZeroKWeb.Controllers
     // GET: /Wiki/
     public ActionResult Index(string node)
     {
-      var wc = new WebClient();
-      if (string.IsNullOrEmpty(node)) node = "Intro";
-
-      var ret = wc.DownloadString("http://code.google.com/p/zero-k/wiki/" + node);
-
-      var idx = ret.IndexOf("<div id=\"wikiheader\"");
-      if (idx > -1) idx = ret.IndexOf("</span>", idx) + 7;
-      var idx2 = ret.LastIndexOf("<div class=\"collapse\">");
-      if (idx2 == -1) idx2 = ret.LastIndexOf("<br>");
-
-      if (idx > -1 && idx2 > -1) ret = ret.Substring(idx, idx2-idx);
-
-      ret = ret.Replace("href=\"/p/zero-k/wiki/", "href=\"");
-      ret = ret.Replace("href=\"/", "href=\"http://code.google.com/");
+      string ret = WikiHandler.LoadWiki(node);
 
       return View(new WikiData() { Content = new MvcHtmlString(ret) });
     }
