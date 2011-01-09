b379a715762dc28284c679edb75089e09483bb4c
yotaxp <yotaxp@localhost>
2011-01-09 03:48:21 +0000

    Selected 'tab' now lights up.  Also added support for flashing 'tabs'.
    svn path=/trunk/other/; revision=1230

---
+++ ZeroKLobby/Controls/HeaderButton.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ZeroKLobby.Controls {
+
+    public class HeaderButton : Button {
+        static HeaderButton() {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(HeaderButton), new FrameworkPropertyMetadata(typeof(HeaderButton)));
+        }
+
+        #region IsSelected
+
+        /// <summary>
+        /// IsSelected Dependency Property
+        /// </summary>
+        public static readonly DependencyProperty IsSelectedProperty =
+            DependencyProperty.Register("IsSelected", typeof(bool), typeof(HeaderButton),
+                new PropertyMetadata((bool)false));
+
+        /// <summary>
+        /// Gets or sets the IsSelected property.
+        /// </summary>
+        public bool IsSelected {
+            get { return (bool)GetValue(IsSelectedProperty); }
+            set { SetValue(IsSelectedProperty, value); }
+        }
+
+        #endregion
+
+        #region IsAlerting
+
+        /// <summary>
+        /// IsAlerting Dependency Property
+        /// </summary>
+        public static readonly DependencyProperty IsAlertingProperty =
+            DependencyProperty.Register("IsAlerting", typeof(bool), typeof(HeaderButton),
+                new PropertyMetadata((bool)false));
+
+        /// <summary>
+        /// Gets or sets the IsAlerting property.
+        /// </summary>
+        public bool IsAlerting {
+            get { return (bool)GetValue(IsAlertingProperty); }
+            set { SetValue(IsAlertingProperty, value); }
+        }
+
+        #endregion
+    }
+}
---
+++ ZeroKLobby/IsPrefixValueConverter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Data;
+using System.Text.RegularExpressions;
+
+namespace ZeroKLobby {
+    public class IsPrefixValueConverter : IValueConverter {
+        private string _prefix = "";
+        private Regex regex = null; 
+        public string Prefix {
+            get { return _prefix; }
+            set {
+                _prefix = value;
+                regex = null;
+            }
+        }
+
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture) {
+            if (value == null) return false;
+
+            if (!(value is string))
+                throw new InvalidOperationException("Value must be of type 'string'.");
+
+            if (regex == null)
+                regex = new Regex("^" + Regex.Escape(_prefix) + "(?:/|$)", RegexOptions.IgnoreCase);
+
+            return regex.IsMatch((string)value);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture) {
+            throw new NotImplementedException();
+        }
+    }
+}
