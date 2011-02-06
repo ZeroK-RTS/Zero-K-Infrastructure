1fe48ce81fcd519963e8b977ea5b9aee70df16ea
Licho <jalich@gmail.com>
2011-02-06 12:21:43 +0000

    do not put stuff into lobby history if it's probably web redirect Fixes issue 270
    svn path=/trunk/other/; revision=1626

--- ZeroKLobby/MicroLobby/BrowserControl.xaml.cs
+++ ZeroKLobby/MicroLobby/BrowserControl.xaml.cs
@@ -2,111 +2,108 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Windows;
-using System.Windows.Forms;
+using System.Windows.Controls;
 using System.Windows.Navigation;
-using ZeroKLobby.Notifications;
 using ZkData;
-using Control = System.Windows.Controls.Control;
-using Cursors = System.Windows.Input.Cursors;
-using UserControl = System.Windows.Controls.UserControl;
-using WebBrowser = System.Windows.Controls.WebBrowser;
 
 namespace ZeroKLobby.MicroLobby
 {
-	/// <summary>
-	/// Interaction logic for MissionControl.xaml
-	/// </summary>
-	public partial class BrowserControl: UserControl, INavigatable
-	{
-		
-		public BrowserControl()
-		{
-      try {
+  /// <summary>
+  /// Interaction logic for MissionControl.xaml
+  /// </summary>
+  public partial class BrowserControl: UserControl, INavigatable
+  {
+    int navigatedIndex = 0;
+    readonly List<string> navigatedPlaces = new List<string>();
+    bool navigating = false;
+    string navigatingTo = null;
+
+    public BrowserControl()
+    {
+      try
+      {
         WindowsApi.InternetSetCookie(Config.BaseUrl, GlobalConst.LobbyAccessCookieName, "1");
-      } catch (Exception ex) {
+      }
+      catch (Exception ex)
+      {
         Trace.TraceWarning("Unable to set ZK cookie:{0}", ex);
       }
       if (Process.GetCurrentProcess().ProcessName == "devenv") return;
       InitializeComponent();
-		}
+    }
 
 
-		public string PathHead { get { return "http://"; } }
+    public string PathHead { get { return "http://"; } }
 
-		string navigatingTo = null;
-
-	  List<string> navigatedPlaces = new List<string>();
-	  int navigatedIndex = 0;
-    bool navigating = false;
-
-		public bool TryNavigate(params string[] path)
-		{
-
+    public bool TryNavigate(params string[] path)
+    {
       var pathString = String.Join("/", path);
-			if (!pathString.StartsWith(PathHead)) return false;
-			if (WebBrowser.Source != null && pathString == WebBrowser.Source.OriginalString) return true;
+      if (!pathString.StartsWith(PathHead)) return false;
+      if (WebBrowser.Source != null && pathString == WebBrowser.Source.OriginalString) return true;
 
       if (navigatingTo == pathString) return true; // already navigating there
 
-      if (navigatedIndex > 1 && navigatedPlaces[navigatedIndex-2] == pathString)
+      if (navigatedIndex > 1 && navigatedPlaces[navigatedIndex - 2] == pathString)
       {
-        navigatedIndex-=2;
+        navigatedIndex -= 2;
         WebBrowser.GoBack();
         return true;
       }
-      
+
       //navigatingTo = pathString;
-			WebBrowser.Navigate(pathString);
-	
-			return true;
-		}
+      WebBrowser.Navigate(pathString);
 
-		public bool Hilite(HiliteLevel level, params string[] path)
-		{
-			return false;
-		}
+      return true;
+    }
 
-		public string GetTooltip(params string[] path)
-		{
-			throw new NotImplementedException();
-		}
+    public bool Hilite(HiliteLevel level, params string[] path)
+    {
+      return false;
+    }
 
-		void WebBrowser_Navigated(object sender, NavigationEventArgs e)
-		{
+    public string GetTooltip(params string[] path)
+    {
+      throw new NotImplementedException();
+    }
+
+    void UserControl_Loaded(object sender, RoutedEventArgs e)
+    {
+      if (Process.GetCurrentProcess().ProcessName == "devenv") return;
+    }
+
+    void WebBrowser_Navigated(object sender, NavigationEventArgs e)
+    {
       navigating = false;
       navigatingTo = null;
       if (navigatedIndex == navigatedPlaces.Count) navigatedPlaces.Add(e.Uri.ToString());
       else navigatedPlaces[navigatedIndex] = e.Uri.ToString();
-		  navigatedIndex++;
-		}
+      navigatedIndex++;
+    }
 
-		void WebBrowser_Navigating(object sender, NavigatingCancelEventArgs e)
-		{
+    void WebBrowser_Navigating(object sender, NavigatingCancelEventArgs e)
+    {
+      var alreadyNavigating = navigating == true; // if already navigating, its probably redirect from website
       navigating = true;
-	    if (navigatingTo != e.Uri.ToString())
-			{
-				navigatingTo = e.Uri.ToString();
-				if (navigatingTo.Contains("@"))
-				{
-				  e.Cancel = true;
+      if (navigatingTo != e.Uri.ToString())
+      {
+        navigatingTo = e.Uri.ToString();
+        if (navigatingTo.Contains("@"))
+        {
+          e.Cancel = true;
           navigatingTo = null;
           navigating = false;
-				}
-				Program.MainWindow.navigationControl.Path = Uri.UnescapeDataString(e.Uri.ToString());
-			}
-		}
+        }
+        if (!alreadyNavigating) Program.MainWindow.navigationControl.Path = Uri.UnescapeDataString(e.Uri.ToString());
+      }
+    }
 
-		private void webBrowser_Loaded(object sender, RoutedEventArgs e)
-		{
-			if (Process.GetCurrentProcess().ProcessName == "devenv") return;
-			UrlSecurityZone.InternetSetFeatureEnabled(UrlSecurityZone.InternetFeaturelist.DISABLE_NAVIGATION_SOUNDS, UrlSecurityZone.SetFeatureOn.PROCESS, true);
-			//UrlSecurityZone.InternetSetFeatureEnabled(UrlSecurityZone.InternetFeaturelist.OBJECT_CACHING, UrlSecurityZone.SetFeatureOn.PROCESS, false);
-			
-		}
-
-		private void UserControl_Loaded(object sender, RoutedEventArgs e)
-		{
-			if (Process.GetCurrentProcess().ProcessName == "devenv") return;
-		}
-	}
+    void webBrowser_Loaded(object sender, RoutedEventArgs e)
+    {
+      if (Process.GetCurrentProcess().ProcessName == "devenv") return;
+      UrlSecurityZone.InternetSetFeatureEnabled(UrlSecurityZone.InternetFeaturelist.DISABLE_NAVIGATION_SOUNDS,
+                                                UrlSecurityZone.SetFeatureOn.PROCESS,
+                                                true);
+      //UrlSecurityZone.InternetSetFeatureEnabled(UrlSecurityZone.InternetFeaturelist.OBJECT_CACHING, UrlSecurityZone.SetFeatureOn.PROCESS, false);
+    }
+  }
 }
