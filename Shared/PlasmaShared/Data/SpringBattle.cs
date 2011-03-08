11681eafe34dadf29110cc560e8670baae97ee31
Licho <jalich@gmail.com>
2011-03-08 13:26:39 +0000

    refactored time factoring code to function
    svn path=/trunk/other/; revision=1923

--- Shared/PlasmaShared/Data/SpringBattle.cs
+++ Shared/PlasmaShared/Data/SpringBattle.cs
@@ -3,174 +3,146 @@ using System.Linq;
 
 namespace ZkData
 {
-  partial class SpringBattle
-  {
-    public string FullTitle
-    {
-      get { return string.Format("B{0} {1} on {2} ({3})", SpringBattleID, PlayerCount, ResourceByMapResourceID.InternalName, BattleType); }
-    }
-
-    public string BattleType
-    {
-      get
-      {
-        string type = "Multiplayer";
-        if (PlayerCount <= 1) type = "Singleplayer";
-        if (HasBots) type = "Bots";
-        if (IsMission) type = "Mission";
-        return type;
-      }
-    }
-
-    
-    public void CalculateElo()
-    {
-      if (IsEloProcessed || Duration < 120)
-      {
-        IsEloProcessed = true;
-        return;
-      }
-
-
-      if (IsMission || HasBots || PlayerCount < 2)
-      {
-        
-		WinnerTeamXpChange = GlobalConst.XpForMissionOrBotsVictory;
-		LoserTeamXpChange = GlobalConst.XpForMissionOrBots;
-
-		if (Duration < 480)
+	partial class SpringBattle
+	{
+		public string BattleType
 		{
-		  WinnerTeamXpChange = 0;
-		  LoserTeamXpChange = 0;
+			get
+			{
+				var type = "Multiplayer";
+				if (PlayerCount <= 1) type = "Singleplayer";
+				if (HasBots) type = "Bots";
+				if (IsMission) type = "Mission";
+				return type;
+			}
 		}
-		else if (Duration < 720)
+		public string FullTitle { get { return string.Format("B{0} {1} on {2} ({3})", SpringBattleID, PlayerCount, ResourceByMapResourceID.InternalName, BattleType); } }
+
+
+		public void CalculateElo()
 		{
-		  WinnerTeamXpChange = (int)(WinnerTeamXpChange*(Duration-720)/240.0);
-		  LoserTeamXpChange = (int)(LoserTeamXpChange*(Duration-720)/240.0);
+			if (IsEloProcessed || Duration < 120)
+			{
+				IsEloProcessed = true;
+				return;
+			}
+
+			if (IsMission || HasBots || PlayerCount < 2)
+			{
+				WinnerTeamXpChange = FactorTime(GlobalConst.XpForMissionOrBotsVictory, Duration);
+				LoserTeamXpChange = FactorTime(GlobalConst.XpForMissionOrBots, Duration);
+
+				foreach (var a in SpringBattlePlayers.Where(x => !x.IsSpectator))
+				{
+					if (a.IsInVictoryTeam)
+					{
+						a.Account.XP += WinnerTeamXpChange.Value;
+						a.XpChange = WinnerTeamXpChange.Value;
+					}
+					else
+					{
+						a.Account.XP += LoserTeamXpChange.Value;
+						a.XpChange = LoserTeamXpChange.Value;
+					}
+				}
+
+				IsEloProcessed = true;
+				return;
+			}
+
+			double winnerW = 0;
+			double loserW = 0;
+			double winnerInvW = 0;
+			double loserInvW = 0;
+
+			double winnerElo = 1;
+			double loserElo = 1;
+
+			var losers = SpringBattlePlayers.Where(x => !x.IsSpectator && !x.IsInVictoryTeam).Select(x => new { Player = x, x.Account }).ToList();
+			var winners = SpringBattlePlayers.Where(x => !x.IsSpectator && x.IsInVictoryTeam).Select(x => new { Player = x, x.Account }).ToList();
+
+			if (losers.Count == 0 || winners.Count == 0)
+			{
+				IsEloProcessed = true;
+				return;
+			}
+
+			foreach (var r in winners)
+			{
+				var elo = r.Account.Elo;
+				var w = r.Account.EloWeight;
+				winnerW += w;
+				winnerInvW += r.Account.EloInvWeight;
+				winnerElo += elo*w;
+			}
+			foreach (var r in losers)
+			{
+				var elo = r.Account.Elo;
+				var w = r.Account.EloWeight;
+				loserW += w;
+				loserInvW += r.Account.EloInvWeight;
+				loserElo += elo*w;
+			}
+
+			winnerElo = winnerElo/winnerW;
+			loserElo = loserElo/loserW;
+
+			var eWin = 1/(1 + Math.Pow(10, (loserElo - winnerElo)/400));
+			var eLose = 1/(1 + Math.Pow(10, (winnerElo - loserElo)/400));
+
+			var sumCount = losers.Count + winners.Count;
+			var scoreWin = Math.Sqrt(sumCount/2.0)*32*(1 - eWin)/winnerInvW;
+			var scoreLose = Math.Sqrt(sumCount/2.0)*32*(0 - eLose)/loserInvW;
+
+			var sumW = winnerW + loserW;
+
+			WinnerTeamXpChange = (int)(20 + (300 + 600*(1 - eWin))/(2.0 + winners.Count));
+			WinnerTeamXpChange = FactorTime(WinnerTeamXpChange, Duration);
+
+			foreach (var r in winners)
+			{
+				var change = (float)(scoreWin*r.Account.EloInvWeight);
+				r.Player.EloChange = change;
+				r.Account.Elo += change;
+
+				r.Account.XP += WinnerTeamXpChange.Value;
+				r.Player.XpChange = WinnerTeamXpChange;
+
+				if (r.Account.EloWeight < GlobalConst.EloWeightMax)
+				{
+					r.Account.EloWeight = (float)(r.Account.EloWeight + ((sumW - r.Account.EloWeight)/(sumCount - 1))/GlobalConst.EloWeightLearnFactor);
+					if (r.Account.EloWeight > GlobalConst.EloWeightMax) r.Account.EloWeight = (float)GlobalConst.EloWeightMax;
+				}
+			}
+
+			LoserTeamXpChange = (int)(20 + (200 + 400*(1 - eLose))/(2.0 + losers.Count));
+			LoserTeamXpChange = FactorTime(LoserTeamXpChange, Duration);
+
+			foreach (var r in losers)
+			{
+				var change = (float)(scoreLose*r.Account.EloInvWeight);
+				r.Player.EloChange = change;
+				r.Account.Elo += change;
+
+				r.Account.XP += LoserTeamXpChange.Value;
+				r.Player.XpChange = LoserTeamXpChange.Value;
+
+				if (r.Account.EloWeight < GlobalConst.EloWeightMax)
+				{
+					r.Account.EloWeight = (float)(r.Account.EloWeight + ((sumW - r.Account.EloWeight)/(sumCount - 1))/GlobalConst.EloWeightLearnFactor);
+					if (r.Account.EloWeight > GlobalConst.EloWeightMax) r.Account.EloWeight = (float)GlobalConst.EloWeightMax;
+				}
+			}
+
+			IsEloProcessed = true;
 		}
-		
-		foreach (var a in SpringBattlePlayers.Where(x=>!x.IsSpectator))
-        {
-          
-          if (a.IsInVictoryTeam)
-          {
-            a.Account.XP += WinnerTeamXpChange.Value;
-            a.XpChange = WinnerTeamXpChange.Value;
-          }
-          else
-          {
-            a.Account.XP += LoserTeamXpChange.Value;
-            a.XpChange = LoserTeamXpChange.Value;
-          }
-        }
-        
-        IsEloProcessed = true;
-        return;
-      }
 
-      double winnerW = 0;
-      double loserW = 0;
-      double winnerInvW = 0;
-      double loserInvW = 0;
-
-      double winnerElo = 1;
-      double loserElo = 1;
-
-      var losers = SpringBattlePlayers.Where(x => !x.IsSpectator && !x.IsInVictoryTeam).Select(x => new { Player = x, x.Account }).ToList();
-      var winners = SpringBattlePlayers.Where(x => !x.IsSpectator && x.IsInVictoryTeam).Select(x => new { Player = x, x.Account }).ToList();
-
-      if (losers.Count == 0 || winners.Count == 0)
-      {
-        IsEloProcessed = true;
-        return;
-      }
-
-      foreach (var r in winners)
-      {
-        var elo = r.Account.Elo;
-        var w = r.Account.EloWeight;
-        winnerW += w;
-        winnerInvW += r.Account.EloInvWeight;
-        winnerElo += elo*w;
-      }
-      foreach (var r in losers)
-      {
-        var elo = r.Account.Elo;
-        var w = r.Account.EloWeight;
-        loserW += w;
-        loserInvW += r.Account.EloInvWeight;
-        loserElo += elo*w;
-      }
-
-      winnerElo = winnerElo/winnerW;
-      loserElo = loserElo/loserW;
-
-      var eWin = 1/(1 + Math.Pow(10, (loserElo - winnerElo)/400));
-      var eLose = 1/(1 + Math.Pow(10, (winnerElo - loserElo)/400));
-
-      var sumCount = losers.Count + winners.Count;
-      var scoreWin = Math.Sqrt(sumCount/2.0)*32*(1 - eWin)/winnerInvW;
-      var scoreLose = Math.Sqrt(sumCount/2.0)*32*(0 - eLose)/loserInvW;
-
-      var sumW = winnerW + loserW;
-
-      WinnerTeamXpChange = (int)(20 + (300 + 600 * (1 - eWin))/ (2.0+winners.Count));
-	  
-	  if (Duration < 480)
-	  {
-		WinnerTeamXpChange = 0;
-	  }
-	  else if (Duration < 720)
-	  {
-		WinnerTeamXpChange = (int)(WinnerTeamXpChange*(Duration-720)/240.0);
-	  }
-	  
-      foreach (var r in winners)
-      {
-        var change = (float)(scoreWin*r.Account.EloInvWeight);
-        r.Player.EloChange = change;
-        r.Account.Elo += change;
-
-        r.Account.XP += WinnerTeamXpChange.Value;
-        r.Player.XpChange = WinnerTeamXpChange;
-        
-        if (r.Account.EloWeight < GlobalConst.EloWeightMax)
-        {
-          r.Account.EloWeight = (float)(r.Account.EloWeight + ((sumW - r.Account.EloWeight)/(sumCount - 1))/GlobalConst.EloWeightLearnFactor);
-          if (r.Account.EloWeight > GlobalConst.EloWeightMax) r.Account.EloWeight = (float)GlobalConst.EloWeightMax;
-        }
-      }
-	  
-	   LoserTeamXpChange = (int)( 20 + (200 + 400 * (1 - eLose))/(2.0 + losers.Count));
-	  
-	  if (Duration < 480)
-	  {
-		LoserTeamXpChange = 0;
-	  }
-	  else if (Duration < 720)
-	  {
-		LoserTeamXpChange = (int)(LoserTeamXpChange*(Duration-720)/240.0);
-	  }
-
-      foreach (var r in losers)
-      {
-        var change = (float)(scoreLose * r.Account.EloInvWeight);
-        r.Player.EloChange = change;
-        r.Account.Elo += change;
-
-        r.Account.XP += LoserTeamXpChange.Value;
-        r.Player.XpChange = LoserTeamXpChange.Value;
-        
-        if (r.Account.EloWeight < GlobalConst.EloWeightMax)
-        {
-          r.Account.EloWeight = (float)(r.Account.EloWeight + ((sumW - r.Account.EloWeight)/(sumCount - 1))/GlobalConst.EloWeightLearnFactor);
-          if (r.Account.EloWeight > GlobalConst.EloWeightMax) r.Account.EloWeight = (float)GlobalConst.EloWeightMax;
-        }
-      }
-
-
-      IsEloProcessed = true;
-    }
-
-  }
+		static int FactorTime(int? xp, int duration)
+		{
+			xp = xp ?? 0;
+			if (duration < 480) return 0;
+			else if (duration < 720) return (int)(xp*(duration - 720)/240.0);
+			else return xp.Value;
+		}
+	}
 }
