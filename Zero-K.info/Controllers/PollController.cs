607383b27ac8cde564bda61890939b84ec5ec8c6
Licho <jalich@gmail.com>
2011-03-17 13:57:48 +0000

    added poll system for ZK site
    svn path=/trunk/other/; revision=2005

---
+++ Zero-K.info/Controllers/PollController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ZkData;
+
+namespace ZeroKWeb.Controllers
+{
+    public class PollController : Controller
+    {
+        //
+        // GET: /Poll/
+
+        public ActionResult Index(int pollID)
+        {
+        	var db = new ZkDataContext();
+        	var poll = db.Polls.Single(x => x.PollID == pollID);
+					
+					return PartialView("PollView", poll);
+        }
+			
+				public ActionResult PollVote(int pollID)
+				{
+					var key = Request.Form.AllKeys.Where(x => !string.IsNullOrEmpty(x)).First(x => x.StartsWith("option"));
+					int optionID = Convert.ToInt32(key.Substring(6));
+
+					if (Global.AccountID == 0) return Content("Not logged in");
+					var db = new ZkDataContext();
+					var poll = db.Polls.Single(x => x.PollID == pollID);
+
+					if (!poll.PollOptions.Any(x => x.OptionID == optionID)) return Content("Invalid option");
+
+					var entry = poll.PollVotes.SingleOrDefault(x => x.AccountID == Global.AccountID);
+					if (entry == null)
+					{
+						entry = new PollVote() { PollID = poll.PollID, AccountID = Global.AccountID};
+						poll.PollVotes.Add(entry);
+					}
+					entry.OptionID = optionID;
+					
+					db.SubmitChanges();
+					foreach (var opt in poll.PollOptions)
+					{
+						opt.Votes = opt.PollVotes.Count(x=>x.PollID == poll.PollID);
+					}
+					db.SubmitChanges();
+
+					return PartialView("PollView", poll);
+				}
+
+    }
+}
