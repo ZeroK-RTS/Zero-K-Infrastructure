05b8455059fa52cd8a8ef874a0de3a08e3a49b89
Licho <jalich@gmail.com>
2011-03-17 11:12:57 +0000

    added transactions around unlock and commander handling code Fixes issue 369
    svn path=/trunk/other/; revision=2000

--- Zero-K.info/Controllers/MyController.cs
+++ Zero-K.info/Controllers/MyController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
 using System.Text.RegularExpressions;
+using System.Transactions;
 using System.Web.Mvc;
 using ZkData;
 
@@ -16,106 +17,111 @@ namespace ZeroKWeb.Controllers
 			if (profileNumber < 1 || profileNumber > 4) return Content("WTF! get lost");
 
 			var db = new ZkDataContext();
-
-			var unlocks = db.AccountUnlocks.Where(x => x.AccountID == Global.AccountID);
-
-			var comm = db.Commanders.SingleOrDefault(x => x.ProfileNumber == profileNumber && x.AccountID == Global.AccountID);
-			if (comm != null)
+			using (var scope = new TransactionScope())
 			{
-				if (!string.IsNullOrEmpty(deleteCommander)) // delete commander
+
+				var unlocks = db.AccountUnlocks.Where(x => x.AccountID == Global.AccountID);
+
+				var comm = db.Commanders.SingleOrDefault(x => x.ProfileNumber == profileNumber && x.AccountID == Global.AccountID);
+				if (comm != null)
 				{
-					db.Commanders.DeleteOnSubmit(comm);
-					db.SubmitChanges();
-					return GetCommanderProfileView(db, profileNumber);
+					if (!string.IsNullOrEmpty(deleteCommander)) // delete commander
+					{
+						db.Commanders.DeleteOnSubmit(comm);
+						db.SubmitChanges();
+						scope.Complete();
+						return GetCommanderProfileView(db, profileNumber);
+					}
 				}
-			}
-			else
-			{
-				comm = new Commander() { AccountID = Global.AccountID, ProfileNumber = profileNumber };
-				db.Commanders.InsertOnSubmit(comm);
-			}
-
-			if (comm.Unlock == null)
-			{
-				var chassisUnlock = unlocks.FirstOrDefault(x => x.UnlockID == chassis);
-				if ((chassis == null || chassisUnlock == null)) return GetCommanderProfileView(db, profileNumber);
 				else
 				{
-					comm.ChassisUnlockID = chassis.Value;
-					comm.Unlock = chassisUnlock.Unlock;
+					comm = new Commander() { AccountID = Global.AccountID, ProfileNumber = profileNumber };
+					db.Commanders.InsertOnSubmit(comm);
 				}
-			}
 
-			if (!string.IsNullOrEmpty(name)) comm.Name = name;
-
-			foreach (var key in Request.Form.AllKeys.Where(x => !string.IsNullOrEmpty(x)))
-			{
-				var m = Regex.Match(key, "m([0-9]+)");
-				if (m.Success)
+				if (comm.Unlock == null)
 				{
-					var slotId = int.Parse(m.Groups[1].Value);
-					int unlockId;
-					int.TryParse(Request.Form[key], out unlockId);
-
-					if (unlockId > 0)
-					{
-						var slot = db.CommanderSlots.Single(x => x.CommanderSlotID == slotId);
-						var unlock = db.Unlocks.Single(x => x.UnlockID == unlockId);
-
-						if (!unlocks.Any(x => x.UnlockID == unlock.UnlockID)) return Content("WTF get lost!");
-						if (slot.MorphLevel < unlock.MorphLevel) return Content(string.Format("WTF cannot use {0} in slot {1}", unlock.Name, slot.CommanderSlotID));
-						if (!string.IsNullOrEmpty(unlock.LimitForChassis))
-						{
-							var validChassis = unlock.LimitForChassis.Split(',');
-							if (!validChassis.Contains(comm.Unlock.Code)) return Content(string.Format("{0} cannot be used in commander {1}", unlock.Name, comm.Unlock.Name));
-						}
-
-						var comSlot = comm.CommanderModules.SingleOrDefault(x => x.SlotID == slotId);
-						if (comSlot == null)
-						{
-							comSlot = new CommanderModule() { SlotID = slotId };
-							comm.CommanderModules.Add(comSlot);
-						}
-						comSlot.ModuleUnlockID = unlockId;
-					}
+					var chassisUnlock = unlocks.FirstOrDefault(x => x.UnlockID == chassis);
+					if ((chassis == null || chassisUnlock == null)) return GetCommanderProfileView(db, profileNumber);
 					else
 					{
-						var oldModule = comm.CommanderModules.FirstOrDefault(x => x.SlotID == slotId);
-						if (oldModule != null) comm.CommanderModules.Remove(oldModule);
+						comm.ChassisUnlockID = chassis.Value;
+						comm.Unlock = chassisUnlock.Unlock;
 					}
 				}
-			}
 
-			foreach (var toDel in Request.Form.AllKeys.Where(x => !string.IsNullOrEmpty(x)))
-			{
-				var m = Regex.Match(toDel, "deleteSlot([0-9]+)");
-				if (m.Success)
+				if (!string.IsNullOrEmpty(name)) comm.Name = name;
+
+				foreach (var key in Request.Form.AllKeys.Where(x => !string.IsNullOrEmpty(x)))
 				{
-					var slotId = int.Parse(m.Groups[1].Value);
-					comm.CommanderModules.Remove(comm.CommanderModules.SingleOrDefault(x => x.SlotID == slotId));
-				}
-			}
-
-			db.SubmitChanges();
-			foreach (var unlock in comm.CommanderModules.GroupBy(x => x.Unlock))
-			{
-				if (unlock.Key == null) continue;
-				var owned = unlocks.Where(x => x.UnlockID == unlock.Key.UnlockID).Sum(x => (int?)x.Count) ?? 0;
-				if (owned < unlock.Count())
-				{
-					var toRemove = unlock.Count() - owned;
-
-					foreach (var m in unlock.OrderByDescending(x => x.SlotID))
+					var m = Regex.Match(key, "m([0-9]+)");
+					if (m.Success)
 					{
-						db.CommanderModules.DeleteOnSubmit(m);
-						//comm.CommanderModules.Remove(m);
-						toRemove--;
-						if (toRemove <= 0) break;
+						var slotId = int.Parse(m.Groups[1].Value);
+						int unlockId;
+						int.TryParse(Request.Form[key], out unlockId);
+
+						if (unlockId > 0)
+						{
+							var slot = db.CommanderSlots.Single(x => x.CommanderSlotID == slotId);
+							var unlock = db.Unlocks.Single(x => x.UnlockID == unlockId);
+
+							if (!unlocks.Any(x => x.UnlockID == unlock.UnlockID)) return Content("WTF get lost!");
+							if (slot.MorphLevel < unlock.MorphLevel) return Content(string.Format("WTF cannot use {0} in slot {1}", unlock.Name, slot.CommanderSlotID));
+							if (!string.IsNullOrEmpty(unlock.LimitForChassis))
+							{
+								var validChassis = unlock.LimitForChassis.Split(',');
+								if (!validChassis.Contains(comm.Unlock.Code)) return Content(string.Format("{0} cannot be used in commander {1}", unlock.Name, comm.Unlock.Name));
+							}
+
+							var comSlot = comm.CommanderModules.SingleOrDefault(x => x.SlotID == slotId);
+							if (comSlot == null)
+							{
+								comSlot = new CommanderModule() { SlotID = slotId };
+								comm.CommanderModules.Add(comSlot);
+							}
+							comSlot.ModuleUnlockID = unlockId;
+						}
+						else
+						{
+							var oldModule = comm.CommanderModules.FirstOrDefault(x => x.SlotID == slotId);
+							if (oldModule != null) comm.CommanderModules.Remove(oldModule);
+						}
 					}
 				}
-			}
 
-			db.SubmitChanges();
+				foreach (var toDel in Request.Form.AllKeys.Where(x => !string.IsNullOrEmpty(x)))
+				{
+					var m = Regex.Match(toDel, "deleteSlot([0-9]+)");
+					if (m.Success)
+					{
+						var slotId = int.Parse(m.Groups[1].Value);
+						comm.CommanderModules.Remove(comm.CommanderModules.SingleOrDefault(x => x.SlotID == slotId));
+					}
+				}
+
+				db.SubmitChanges();
+				foreach (var unlock in comm.CommanderModules.GroupBy(x => x.Unlock))
+				{
+					if (unlock.Key == null) continue;
+					var owned = unlocks.Where(x => x.UnlockID == unlock.Key.UnlockID).Sum(x => (int?)x.Count) ?? 0;
+					if (owned < unlock.Count())
+					{
+						var toRemove = unlock.Count() - owned;
+
+						foreach (var m in unlock.OrderByDescending(x => x.SlotID))
+						{
+							db.CommanderModules.DeleteOnSubmit(m);
+							//comm.CommanderModules.Remove(m);
+							toRemove--;
+							if (toRemove <= 0) break;
+						}
+					}
+				}
+
+				db.SubmitChanges();
+				scope.Complete();
+			}
 
 			return GetCommanderProfileView(db, profileNumber);
 		}
@@ -151,22 +157,26 @@ namespace ZeroKWeb.Controllers
 		{
 			if (!Global.IsAccountAuthorized) return Content("Not logged in");
 			var db = new ZkDataContext();
-
-			List<Unlock> unlocks;
-			List<Unlock> future;
-
-			GetUnlockLists(out unlocks, out future);
-
-			if (unlocks.Any(x => x.UnlockID == id))
+			using (var scope = new TransactionScope())
 			{
-				var au = db.AccountUnlocks.SingleOrDefault(x => x.AccountID == Global.AccountID && x.UnlockID == id);
-				if (au == null)
+
+				List<Unlock> unlocks;
+				List<Unlock> future;
+
+				GetUnlockLists(db, out unlocks, out future);
+
+				if (unlocks.Any(x => x.UnlockID == id))
 				{
-					au = new AccountUnlock() { AccountID = Global.AccountID, UnlockID = id, Count = 1 };
-					db.AccountUnlocks.InsertOnSubmit(au);
+					var au = db.AccountUnlocks.SingleOrDefault(x => x.AccountID == Global.AccountID && x.UnlockID == id);
+					if (au == null)
+					{
+						au = new AccountUnlock() { AccountID = Global.AccountID, UnlockID = id, Count = 1 };
+						db.AccountUnlocks.InsertOnSubmit(au);
+					}
+					else au.Count++;
+					db.SubmitChanges();
 				}
-				else au.Count++;
-				db.SubmitChanges();
+				scope.Complete();
 			}
 			return RedirectToAction("UnlockList");
 		}
@@ -176,7 +186,8 @@ namespace ZeroKWeb.Controllers
 			if (!Global.IsAccountAuthorized) return Content("Not logged in");
 			List<Unlock> unlocks;
 			List<Unlock> future;
-			GetUnlockLists(out unlocks, out future);
+			var db = new ZkDataContext();
+			GetUnlockLists(db, out unlocks, out future);
 
 			return View("UnlockList", new UnlockListResult() { Account = Global.Account, Unlocks = unlocks, FutureUnlocks = future });
 		}
@@ -203,10 +214,8 @@ namespace ZeroKWeb.Controllers
 			                   });
 		}
 
-		void GetUnlockLists(out List<Unlock> unlocks, out List<Unlock> future)
+		void GetUnlockLists(ZkDataContext db, out List<Unlock> unlocks, out List<Unlock> future)
 		{
-			var db = new ZkDataContext();
-
 			var maxedUnlockList =
 				db.AccountUnlocks.Where(x => x.AccountID == Global.AccountID && x.Count >= x.Unlock.MaxModuleCount).Select(x => x.UnlockID).ToList();
 			var anyUnlockList = db.AccountUnlocks.Where(x => x.AccountID == Global.AccountID && x.Count > 0).Select(x => x.UnlockID).ToList();
