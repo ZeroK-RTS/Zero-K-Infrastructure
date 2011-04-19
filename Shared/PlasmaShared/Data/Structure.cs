08ec087df8de15d10ad9766ded93c728096ae1d0
Licho <jalich@gmail.com>
2011-04-19 22:54:12 +0000

    * added GetImageUrl  to structure (reflects destroyed state)  * fixed map detail - to show even on planets without  owner + added icon of structures
    svn path=/trunk/other/; revision=2482

---
+++ Shared/PlasmaShared/Data/Structure.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ZkData
+{
+	partial class PlanetStructure
+	{
+		public string GetImageUrl()
+		{
+			if (IsDestroyed) return string.Format("/img/structures/{0}", StructureType.DestroyedMapIcon); 
+			else return string.Format("/img/structures/{0}", StructureType.MapIcon);
+		}
+
+	}
+}
