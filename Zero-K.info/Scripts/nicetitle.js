a66e37ecc4be1fe68068d9ed08ae90b127832d4f
christopherreay@gmail.com <christopherreay@gmail.com@localhost>
2011-04-30 18:15:32 +0000

    TOPIC: Website Tooltips nicetitle Fixed: nictetitle.js incompatible with ( and only with ) lobby version of ie. Javascript engine baws at trailing commas in dictionary definitions
    There is stilla problems with:
    The toolstips are set to the wrong width in nicetitle.css
    color: inherit doesnt seem to work the same in the lobby version of ie as all the other browsers, so the text is colored grey ( this grea comes from default qtip format, so ill look for that)
    There seems to be a problem with assigning 0.9 em to text. I dont knwo what it is, as assigning something like 2em works perfectly
    
    svn path=/trunk/other/; revision=2695

--- Zero-K.info/Scripts/nicetitle.js
+++ Zero-K.info/Scripts/nicetitle.js
@@ -7,23 +7,12 @@
 //window.setInterval(makeNiceTitles, 1000);
 
 var niceTitlesJS = new Object();
-niceTitlesJS.CURRENT_NICE_TITLE = null;
-niceTitlesJS.cachedToolTips = new Array();
 
 niceTitlesJS.makeNiceTitles = function () {
-    var leafTitlenodes = new Array();
     $("[title]").each(
         function (index) {
 
-            var elem = this;
             var nicetitle = $(this).attr("title");
-
-            /*while ($(elem).parent() != $(document)
-            { if ($(elem).parent().attr("title") == nicetitle)
-                return;
-              else
-                elem = $(elem).parent()
-            }*/
             
             var qtipOptions = 
             { position: 
@@ -31,14 +20,14 @@ niceTitlesJS.makeNiceTitles = function () {
                   my: 'left top',
                   at: 'right top',
                   adjust: 
-                  { method: 'flip shift',
-                  },
+                  { method: 'flip shift'
+                  }
                 },
                 style:
-                { classes: 'nicetitle',
+                { classes: 'nicetitle'
                 },
                 show:
-                { solo: $(document),
+                { solo: $(document)
                 }
             }
 
@@ -49,7 +38,7 @@ niceTitlesJS.makeNiceTitles = function () {
                 ajax:
                 { url: '/Home/GetTooltip',
                   type: "GET",
-                  data: {key: nicetitle},
+                  data: {key: nicetitle}
                 }
               };
               
@@ -61,30 +50,6 @@ niceTitlesJS.makeNiceTitles = function () {
             $(this).qtip(qtipOptions);
         }
     );
-}
-
-niceTitlesJS.attachNiceTitleHandler = function(element) {
-    var nicetitle = $(element).attr("title");
-    var asyncMode = nicetitle.charAt(0) == '$';
-    var niceTitleDiv = $("<div />").addClass("nicetitle");
-    var niceTitleContent;
-    if (!asyncMode)
-        niceTitleContent = $("<span>" + nicetitle + "</span>");
-    else {
-        niceTitleContent = $("<span>Loading....<img src='/img/Loader.gif'></span>");
-        $.get('/Home/GetTooltip?key=' + nicetitle, function (ret) {
-            //cache returned data
-            niceTitlesJS.cachedTooltips[nicetitle] = ret;
-            // if tooltip still same, update it
-            if (niceTitleDiv == CURRENT_NICE_TITLE) {
-                niceTitlesJS.CURRENT_NICE_TITLE.html(ret);
-            }
-            //TODO: We MAY need to call update() method of jquery.tooltip to deal with the 
-            //  Tooltip changing size at this point
-        });
-    }
-    niceTitlesJS.CURRENT_NICE_TITLE = niceTitleDiv;
-    return niceTitleDiv.append(niceTitleContent);
 }
 
 $().ready(niceTitlesJS.makeNiceTitles);
