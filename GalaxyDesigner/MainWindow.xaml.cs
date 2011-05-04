6792abcc32fedfadf4533add87bb193deace22f6
lccquantum <lccquantum@localhost>
2011-05-04 21:25:54 +0000

    * Links in main map  * Fixed galaxy map lag  * Incomplete diplomacy map
    svn path=/trunk/other/; revision=2725

--- GalaxyDesigner/MainWindow.xaml.cs
+++ GalaxyDesigner/MainWindow.xaml.cs
@@ -7,6 +7,7 @@ using System.Windows.Controls;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Shapes;
+using Diagrams;
 
 namespace GalaxyDesigner
 {
--- Shared/PlasmaShared/Data/Structure.cs
+++ Shared/PlasmaShared/Data/Structure.cs
@@ -2,6 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.IO;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using PlasmaShared;
 
 namespace ZkData
 {
@@ -12,6 +16,39 @@ namespace ZkData
 			if (IsDestroyed) return string.Format("/img/structures/{0}", StructureType.DestroyedMapIcon); 
 			else return string.Format("/img/structures/{0}", StructureType.MapIcon);
 		}
+
+		public string GetPathResized(int size)
+		{
+			return "/img/structures/" + GetFileNameResized(size);
+		}
+
+		public string GetFileNameResized(int size)
+		{
+			var fileName = IsDestroyed ? StructureType.DestroyedMapIcon : StructureType.MapIcon;
+			var extension = Path.GetExtension(fileName);
+			return String.Format("{0}_r_{1}{2}", Path.GetFileNameWithoutExtension(fileName), size, extension);
+		}
+
+
+		public void GenerateResized(int size, string folder)
+		{
+			GenerateResized(size, folder, true);
+			GenerateResized(size, folder, false);
+		}
+
+
+
+		public void GenerateResized(int size, string folder, bool destroyed)
+		{
+			using (var image = Image.FromFile(folder + "/" + (IsDestroyed ? StructureType.DestroyedMapIcon : StructureType.MapIcon)))
+			{
+				using (var resized = image.GetResized(size, size, InterpolationMode.HighQualityBicubic))
+				{
+					resized.Save(folder + "/"  + GetFileNameResized(size));
+				}				
+			}
+		}
+
 
 	}
 }
