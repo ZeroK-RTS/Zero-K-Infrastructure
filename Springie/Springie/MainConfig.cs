0abc5fb0f3db90395a7942043f614ccb74dc9b45
Licho <jalich@gmail.com>
2011-05-07 01:02:22 +0000

    just sprignie config update
    svn path=/trunk/other/; revision=2758

--- Springie/Springie/MainConfig.cs
+++ Springie/Springie/MainConfig.cs
@@ -9,7 +9,7 @@ namespace Springie
 {
 	public class MainConfig: IPlasmaDownloaderConfig
 	{
-		public const string SpringieVersion = "Springie 1.49.2";
+		public const string SpringieVersion = "Springie 1.49.3";
 
 		string executableName = "spring-dedicated.exe";
 		bool gargamelMode = true;
