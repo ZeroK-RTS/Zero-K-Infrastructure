31259af23bdbf7c0dc970b0a26f022e01b487678
lccquantum <lccquantum@localhost>
2011-06-09 23:28:44 +0000

    Created the ChatToSpeech class
    svn path=/trunk/other/; revision=3002

---
+++ ZeroKLobby/ChatToSpeech.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Globalization;
+using System.Linq;
+using System.Speech.Synthesis;
+using System.Text;
+using LobbyClient;
+
+namespace ZeroKLobby
+{
+	class ChatToSpeech
+	{
+		bool isSpeechEnabled = false;
+		SpeechSynthesizer speechSynthesizer = new SpeechSynthesizer();
+		ReadOnlyCollection<InstalledVoice> voices;
+
+		public ChatToSpeech()
+		{
+			var spring = new Spring(Program.SpringPaths);
+			spring.LogLineAdded += spring_LogLineAdded;
+			voices = speechSynthesizer.GetInstalledVoices();
+		}
+
+		void spring_LogLineAdded(string text, bool isError)
+		{
+			if (text == "ENABLE TTS") isSpeechEnabled = true;
+			else if (text == "DISABLE TTS") isSpeechEnabled = false;
+			else
+			{
+				speechSynthesizer.SpeakAsync(text); 
+			}
+		}
+	}
+}
