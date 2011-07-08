b130c341b837e51042126e1b0ba10606370737b3
Licho <jalich@gmail.com>
2011-07-08 20:05:28 +0000

    * dont crash site if wiki fails
    svn path=/trunk/other/; revision=3330

--- Zero-K.info/AppCode/WikiHandler.cs
+++ Zero-K.info/AppCode/WikiHandler.cs
@@ -9,24 +9,28 @@ namespace ZeroKWeb
 	{
 		public static string LoadWiki(string node)
 		{
-			var entry = HttpContext.Current.Cache.Get("wiki_" + node) as string;
-			if (entry != null) return entry;
+			try {
+				var entry = HttpContext.Current.Cache.Get("wiki_" + node) as string;
+				if (entry != null) return entry;
 
-			var wc = new WebClient();
-			if (String.IsNullOrEmpty(node)) node = "Manual";
+				var wc = new WebClient();
+				if (String.IsNullOrEmpty(node)) node = "Manual";
 
-			var ret = wc.DownloadString("http://code.google.com/p/zero-k/wiki/" + node);
+				var ret = wc.DownloadString("http://code.google.com/p/zero-k/wiki/" + node);
 
-			var idx = ret.IndexOf("<div id=\"wikicontent\""); 
-			var idx2 = ret.LastIndexOf("</td>");
+				var idx = ret.IndexOf("<div id=\"wikicontent\"");
+				var idx2 = ret.LastIndexOf("</td>");
 
-			if (idx > -1 && idx2 > -1) ret = ret.Substring(idx, idx2 - idx);
+				if (idx > -1 && idx2 > -1) ret = ret.Substring(idx, idx2 - idx);
 
-			ret = ret.Replace("href=\"/p/zero-k/wiki/", "href =\"/Wiki/");
-			ret = ret.Replace("href=\"/", "href=\"http://code.google.com/");
+				ret = ret.Replace("href=\"/p/zero-k/wiki/", "href =\"/Wiki/");
+				ret = ret.Replace("href=\"/", "href=\"http://code.google.com/");
 
-			HttpContext.Current.Cache.Insert("wiki_" + node, ret, null, DateTime.UtcNow.AddMinutes(15), Cache.NoSlidingExpiration);
-			return ret;
+				HttpContext.Current.Cache.Insert("wiki_" + node, ret, null, DateTime.UtcNow.AddMinutes(15), Cache.NoSlidingExpiration);
+				return ret;
+			} catch (Exception ex) {
+				return string.Format("Error loading {0} : {1}", node, ex.Message);
+			}
 		}
 	}
 }
