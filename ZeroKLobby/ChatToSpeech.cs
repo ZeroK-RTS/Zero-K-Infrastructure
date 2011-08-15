fda1702e65f84a95d97a3d495b4b43b2f2439b6b
Licho <jalich@gmail.com>
2011-08-15 23:21:17 +0000

    dont need speech synthesizer to start
    svn path=/trunk/other/; revision=3576

--- ZeroKLobby/ChatToSpeech.cs
+++ ZeroKLobby/ChatToSpeech.cs
@@ -1,53 +1,61 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Diagnostics;
-using System.Globalization;
-using System.Linq;
 using System.Speech.Synthesis;
-using System.Text;
 using System.Text.RegularExpressions;
 using LobbyClient;
 
 namespace ZeroKLobby
 {
-	class ChatToSpeech
-	{
-		bool isSpeechEnabled = false;
-		SpeechSynthesizer speechSynthesizer = new SpeechSynthesizer();
-		ReadOnlyCollection<InstalledVoice> voices;
-		
-		public ChatToSpeech(Spring spring)
-		{
-			spring.LogLineAdded += spring_LogLineAdded;
-			voices = speechSynthesizer.GetInstalledVoices();
-		}
+    class ChatToSpeech
+    {
+        bool isSpeechEnabled = false;
+        readonly SpeechSynthesizer speechSynthesizer = new SpeechSynthesizer();
+        readonly ReadOnlyCollection<InstalledVoice> voices;
 
-		void spring_LogLineAdded(string text, bool isError)
-		{
-			if (string.IsNullOrEmpty(text) || isError) return;
-			try {
-				if (text.Contains(Program.Conf.LobbyPlayerName))
-				{
-					if (text.Contains("ENABLE TTS")) isSpeechEnabled = true;
-					else if (text.Contains("DISABLE TTS")) isSpeechEnabled = false;
-				}
-				if (isSpeechEnabled) {
-					var match = Regex.Match(text, "\\] <([^>]+)> Allies: (.+)");
-					if (!match.Success) match = Regex.Match(text, "\\] ([^ ]+) added point: (.+)");
-					if (match.Success) {
-						var name = match.Groups[1].Value;
-						var sayText = match.Groups[2].Value;
-						if (name != Program.Conf.LobbyPlayerName && !string.IsNullOrEmpty(text) && !Regex.IsMatch(sayText, "Start [0-9]+$"))
-						{
-							if (voices.Count > 1) speechSynthesizer.SelectVoice(voices[name.GetHashCode()%voices.Count].VoiceInfo.Name);
-							speechSynthesizer.SpeakAsync(sayText);
-						}
-					}
-				}
-			} catch (Exception ex) {
-				Trace.TraceError("Error in text to speech: {0}", ex);
-			}
-		}
-	}
+        public ChatToSpeech(Spring spring)
+        {
+            try
+            {
+                voices = speechSynthesizer.GetInstalledVoices();
+                spring.LogLineAdded += spring_LogLineAdded;
+            }
+            catch (Exception ex)
+            {
+                Trace.TraceError("voice synthetizer failed: {0}", ex);
+            }
+        }
+
+        void spring_LogLineAdded(string text, bool isError)
+        {
+            if (string.IsNullOrEmpty(text) || isError || voices == null) return;
+            try
+            {
+                if (text.Contains(Program.Conf.LobbyPlayerName))
+                {
+                    if (text.Contains("ENABLE TTS")) isSpeechEnabled = true;
+                    else if (text.Contains("DISABLE TTS")) isSpeechEnabled = false;
+                }
+                if (isSpeechEnabled)
+                {
+                    var match = Regex.Match(text, "\\] <([^>]+)> Allies: (.+)");
+                    if (!match.Success) match = Regex.Match(text, "\\] ([^ ]+) added point: (.+)");
+                    if (match.Success)
+                    {
+                        var name = match.Groups[1].Value;
+                        var sayText = match.Groups[2].Value;
+                        if (name != Program.Conf.LobbyPlayerName && !string.IsNullOrEmpty(text) && !Regex.IsMatch(sayText, "Start [0-9]+$"))
+                        {
+                            if (voices.Count > 1) speechSynthesizer.SelectVoice(voices[name.GetHashCode()%voices.Count].VoiceInfo.Name);
+                            speechSynthesizer.SpeakAsync(sayText);
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Trace.TraceError("Error in text to speech: {0}", ex);
+            }
+        }
+    }
 }
