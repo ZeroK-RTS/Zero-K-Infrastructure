b6f47d897659d308ebf98044a307599d83cbd82f
maackey <maackey@gmail.com>
2011-08-25 21:49:39 +0000

    news popups should have proper width
    svn path=/trunk/other/; revision=3616

--- Zero-K.info/Scripts/site.js
+++ Zero-K.info/Scripts/site.js
@@ -4,57 +4,57 @@ var ajaxScrollEnabled = true;
 
 /* confirm dialog when class is delete */
 $(document).ready(function () {
-  // delete confirm dialog
-  $('.delete').click(function () {
-    var answer = confirm('Are you sure?');
-    return answer;
-  });
-
-   $(".dialog").dialog();
-   
-   $(".datepicker").datepicker();
-
-  /* ajax form updater and scorll based loader
-  It updates form on submit using ajax - sending offset 0 to it when user clicks
-
-  It also updates form when user scrolls to bottom - sending current offset to it, this continues until controller returns data.
-
-  AjaxScrollProgress element is made visible to display loading progress
-  */
-  var frm = $("#ajaxScrollForm");
-  var prg = $("#ajaxScrollProgress");
-  var target = $("#ajaxScrollTarget");
-  if (frm && frm.attr("id") != undefined) {
-    window.onscroll = function () {
-      if (!ajaxScrollEnabled) return;
-      var el = document.documentElement;
-      if (el.scrollHeight - (document.documentElement.scrollTop + document.documentElement.clientHeight) < 50) {
-        ajaxScrollEnabled = false;
-        prg.show();
-        $.post(frm.attr("action"), frm.serialize() + "&offset=" + ajaxScrollOffset, function (ret) {
-          target.append(ret);
-          ajaxScrollOffset = ajaxScrollOffset + ajaxScrollCount;
-          if (ret == '') ajaxScrollEnabled = false;
-          else ajaxScrollEnabled = true;
-          prg.hide();
-        });
-      }
-    };
-
-
-    frm.submit(function () {
-      ajaxScrollEnabled = false;
-      ajaxScrollOffset = 0;
-      prg.show();
-      $.post(frm.attr("action"), frm.serialize() + "&offset=" + ajaxScrollOffset, function (ret) {
-        target.html(ret);
-        ajaxScrollOffset = ajaxScrollCount;
-        ajaxScrollEnabled = true;
-        prg.hide();
-      });
-      return false;
+    // delete confirm dialog
+    $('.delete').click(function () {
+        var answer = confirm('Are you sure?');
+        return answer;
     });
-  }
+
+    $(".dialog").dialog({width: 800 });
+
+    $(".datepicker").datepicker();
+
+    /* ajax form updater and scorll based loader
+    It updates form on submit using ajax - sending offset 0 to it when user clicks
+
+    It also updates form when user scrolls to bottom - sending current offset to it, this continues until controller returns data.
+
+    AjaxScrollProgress element is made visible to display loading progress
+    */
+    var frm = $("#ajaxScrollForm");
+    var prg = $("#ajaxScrollProgress");
+    var target = $("#ajaxScrollTarget");
+    if (frm && frm.attr("id") != undefined) {
+        window.onscroll = function () {
+            if (!ajaxScrollEnabled) return;
+            var el = document.documentElement;
+            if (el.scrollHeight - (document.documentElement.scrollTop + document.documentElement.clientHeight) < 50) {
+                ajaxScrollEnabled = false;
+                prg.show();
+                $.post(frm.attr("action"), frm.serialize() + "&offset=" + ajaxScrollOffset, function (ret) {
+                    target.append(ret);
+                    ajaxScrollOffset = ajaxScrollOffset + ajaxScrollCount;
+                    if (ret == '') ajaxScrollEnabled = false;
+                    else ajaxScrollEnabled = true;
+                    prg.hide();
+                });
+            }
+        };
+
+
+        frm.submit(function () {
+            ajaxScrollEnabled = false;
+            ajaxScrollOffset = 0;
+            prg.show();
+            $.post(frm.attr("action"), frm.serialize() + "&offset=" + ajaxScrollOffset, function (ret) {
+                target.html(ret);
+                ajaxScrollOffset = ajaxScrollCount;
+                ajaxScrollEnabled = true;
+                prg.hide();
+            });
+            return false;
+        });
+    }
 });
 
 
