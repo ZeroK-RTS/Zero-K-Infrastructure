9073bf6567cc9649ca17b3700a302250cae967f6
Licho <jalich@gmail.com>
2011-10-14 02:18:27 +0000

    * each faction gets its own fancy ship icon!
    svn path=/trunk/other/; revision=3973

--- Shared/PlasmaShared/Data/Faction.cs
+++ Shared/PlasmaShared/Data/Faction.cs
@@ -12,6 +12,10 @@ namespace ZkData
             return string.Format("/img/factions/{0}", ImageFile);
         }
 
+        public string GetShipImageUrl() {
+            return string.Format("/img/factions/{0}_ship.png", Shortcut);
+        }
+
         public static string FactionColor(Faction fac, int myFactionID) {
             if (fac == null) return "";
             return fac.Color;
