9e445ac148a4a8271e615766224203e2a57e752d
Licho <jalich@gmail.com>
2011-11-19 14:34:08 +0000

    dont deploy configs with 1920x1200 resolution..
    svn path=/trunk/other/; revision=4352

--- NightWatch/Nightwatch.cs
+++ NightWatch/Nightwatch.cs
@@ -184,46 +184,4 @@ namespace CaTracker
 			recon.Start();
 		}
 	}
-
-    public class PlayerMover
-    {
-        TasClient tas;
-
-        public PlayerMover(TasClient tas)
-        {
-            this.tas = tas;
-            tas.Said += new EventHandler<TasSayEventArgs>(tas_Said);
-        }
-
-        void tas_Said(object sender, TasSayEventArgs e)
-        {
-            if (e.Place == TasSayEventArgs.Places.Normal && e.Text.StartsWith("!move")) {
-                var db = new ZkDataContext();
-                var acc = db.Accounts.Single(x => x.Name == e.UserName);
-                if (acc.IsZeroKAdmin || acc.IsLobbyAdministrator) {
-                    var parts = e.Text.Split(' ');
-                    if (parts.Length != 3)
-                    {
-                        tas.Say(TasClient.SayPlace.User, e.UserName, "!move [from] [to]", false);
-                    }
-                    else
-                    {
-                        var from = tas.ExistingBattles.Values.FirstOrDefault(x => x.Founder.Name == parts[1]);
-                        var to = tas.ExistingBattles.Values.FirstOrDefault(x => x.Founder.Name == parts[2]);
-                        if (from != null && to != null)
-                        {
-                            foreach (var b in from.Users)
-                            {
-                                if (!b.LobbyUser.IsInGame) tas.Say(TasClient.SayPlace.User, b.Name, "!join " + parts[2], false);
-                            }
-                        }
-                        else {
-                            tas.Say(TasClient.SayPlace.User, e.UserName, "Not a valid battle host name",false);
-                        }
-                    }
-                }
-            }
-
-        }
-    }
 }
