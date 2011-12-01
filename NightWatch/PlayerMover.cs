0c311a8c34638b14f6cf2067e3169554d254ef39
KingRaptor <kingraptorowns@gmail.com>
2011-12-01 12:50:23 +0000

    Test functionality for splitting players into two rooms by median Elo
    svn path=/trunk/other/; revision=4489

--- NightWatch/PlayerMover.cs
+++ NightWatch/PlayerMover.cs
@@ -16,23 +16,55 @@ namespace CaTracker
 
         void tas_Said(object sender, TasSayEventArgs e)
         {
-            if (e.Place == TasSayEventArgs.Places.Normal && e.Text.StartsWith("!move"))
+            if (e.Place == TasSayEventArgs.Places.Normal)
             {
-                var db = new ZkDataContext();
-                var acc = db.Accounts.Single(x => x.Name == e.UserName);
-                if (acc.IsZeroKAdmin || acc.IsLobbyAdministrator)
+                if (e.Text.StartsWith("!move"))
                 {
-                    var parts = e.Text.Split(' ');
-                    if (parts.Length != 3) tas.Say(TasClient.SayPlace.User, e.UserName, "!move [from] [to]", false);
-                    else
+                    var db = new ZkDataContext();
+                    var acc = db.Accounts.Single(x => x.Name == e.UserName);
+                    if (acc.IsZeroKAdmin || acc.IsLobbyAdministrator)
                     {
-                        var from = tas.ExistingBattles.Values.FirstOrDefault(x => x.Founder.Name == parts[1]);
-                        var to = tas.ExistingBattles.Values.FirstOrDefault(x => x.Founder.Name == parts[2]);
-                        if (from != null && to != null)
+                        var parts = e.Text.Split(' ');
+                        if (parts.Length != 3) tas.Say(TasClient.SayPlace.User, e.UserName, "!move [from] [to]", false);
+                        else
                         {
-                            foreach (var b in from.Users) if (!b.LobbyUser.IsInGame) tas.Say(TasClient.SayPlace.User, b.Name, "!join " + parts[2], false);
+                            var from = tas.ExistingBattles.Values.FirstOrDefault(x => x.Founder.Name == parts[1]);
+                            var to = tas.ExistingBattles.Values.FirstOrDefault(x => x.Founder.Name == parts[2]);
+                            if (from != null && to != null)
+                            {
+                                foreach (var b in from.Users) if (!b.LobbyUser.IsInGame) tas.Say(TasClient.SayPlace.User, b.Name, "!join " + parts[2], false);
+                            }
+                            else tas.Say(TasClient.SayPlace.User, e.UserName, "Not a valid battle host name", false);
                         }
-                        else tas.Say(TasClient.SayPlace.User, e.UserName, "Not a valid battle host name", false);
+                    }
+                }
+                // split players evenly into two games by median elo -> expand to specify proportion to shunt?
+                else if (e.Text.StartsWith("!splitplayers"))
+                {
+                    var db = new ZkDataContext();
+                    var acc = db.Accounts.Single(x => x.Name == e.UserName);
+                    if (acc.IsZeroKAdmin || acc.IsLobbyAdministrator)
+                    {
+                        var parts = e.Text.Split(' ');
+                        if (parts.Length != 3) tas.Say(TasClient.SayPlace.User, e.UserName, "!splitplayers [from] [to]", false);
+                        else
+                        {
+                            var from = tas.ExistingBattles.Values.FirstOrDefault(x => x.Founder.Name == parts[1]);
+                            var to = tas.ExistingBattles.Values.FirstOrDefault(x => x.Founder.Name == parts[2]);
+                            if (from != null && to != null)
+                            {
+                                double medianElo = 1500;
+                                double numPlayers = 0;
+                                var list = from.Users.OrderBy(x => x.LobbyUser.EffectiveElo);
+                                medianElo = list.Take(from.Users.Count / 2).LobbyUser.EffectiveElo; 
+                                for (int i = 0; i < players.Length; i++)
+                                {
+                                    var b = players[i];
+                                    if (!b.LobbyUser.IsInGame && b.LobbyUser.EffectiveElo > medianElo) tas.Say(TasClient.SayPlace.User, b.Name, "!join " + parts[2], false);
+                                }
+                            }
+                            else tas.Say(TasClient.SayPlace.User, e.UserName, "Not a valid battle host name", false);
+                        }
                     }
                 }
             }
