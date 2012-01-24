da1a44c13ea4a0fb993fbf1d1cadfd0a375bfed3
KingRaptor <kingraptorowns@gmail.com>
2012-01-24 14:04:02 +0000

    Commit missing Objective.cs in MissionEditor
    svn path=/trunk/other/; revision=5149

---
+++ MissionEditor/CMissionLib/Objective.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace CMissionLib
+{
+	[DataContract]
+	public class Objective : PropertyChanged
+	{
+        [DataMember]
+        public string ID { get; set; }
+	}
+}
