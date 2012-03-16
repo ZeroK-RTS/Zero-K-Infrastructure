aa5bf42ff25c8291a902fc12e99c6d46b5f68541
KingRaptor <kingraptorowns@gmail.com>
2012-03-16 14:51:47 +0000

    Commit missing CustomAction2.cs
    svn path=/trunk/other/; revision=5711

---
+++ MissionEditor/CMissionLib/Actions/CustomAction2.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+
+namespace CMissionLib.Actions
+{
+	[DataContract]
+	public class CustomAction2 : Action
+	{
+        string codeStr;
+
+        public CustomAction2()
+		{
+            CodeStr = "";
+		}
+
+        [DataMember]
+        public string CodeStr
+        {
+            get { return codeStr; }
+            set
+            {
+                codeStr = value;
+                RaisePropertyChanged("CodeStr");
+            }
+        }
+
+		public override LuaTable GetLuaTable(Mission mission)
+		{
+            var map = new Dictionary<object, object>
+				{
+					{"codeStr", CodeStr},
+				};
+            return new LuaTable(map);
+		}
+
+		public override string GetDefaultName()
+		{
+			return "Custom Action (Alternate)";
+		}
+	}
+}
