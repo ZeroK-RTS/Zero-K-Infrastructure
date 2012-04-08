5de86b4ca9254cd0dff70a000c906449d071fd7b
Licho <jalich@gmail.com>
2012-04-08 19:13:47 +0000

    ZKL fixed missing error messages in login form
    svn path=/trunk/other/; revision=5923

--- ZeroKLobby/MicroLobby/LoginForm.Designer.cs
+++ ZeroKLobby/MicroLobby/LoginForm.Designer.cs
@@ -40,11 +40,11 @@
             this.tabControl1 = new System.Windows.Forms.TabControl();
             this.tabPage1 = new System.Windows.Forms.TabPage();
             this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.rgName = new System.Windows.Forms.TextBox();
             this.rgPassword = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
-            this.rgName = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
             this.tabControl1.SuspendLayout();
             this.tabPage1.SuspendLayout();
             this.tabPage2.SuspendLayout();
@@ -99,7 +99,7 @@
             this.lbInfo.AutoSize = true;
             this.lbInfo.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
             this.lbInfo.ForeColor = System.Drawing.Color.Red;
-            this.lbInfo.Location = new System.Drawing.Point(15, 13);
+            this.lbInfo.Location = new System.Drawing.Point(15, 178);
             this.lbInfo.Name = "lbInfo";
             this.lbInfo.Size = new System.Drawing.Size(0, 13);
             this.lbInfo.TabIndex = 5;
@@ -130,11 +130,10 @@
             // 
             this.tabControl1.Controls.Add(this.tabPage1);
             this.tabControl1.Controls.Add(this.tabPage2);
-            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tabControl1.Location = new System.Drawing.Point(0, 0);
             this.tabControl1.Name = "tabControl1";
             this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(278, 164);
+            this.tabControl1.Size = new System.Drawing.Size(290, 166);
             this.tabControl1.TabIndex = 8;
             // 
             // tabPage1
@@ -148,7 +147,7 @@
             this.tabPage1.Location = new System.Drawing.Point(4, 22);
             this.tabPage1.Name = "tabPage1";
             this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage1.Size = new System.Drawing.Size(270, 138);
+            this.tabPage1.Size = new System.Drawing.Size(282, 140);
             this.tabPage1.TabIndex = 0;
             this.tabPage1.Text = "Login";
             this.tabPage1.UseVisualStyleBackColor = true;
@@ -169,6 +168,13 @@
             this.tabPage2.Text = "Register";
             this.tabPage2.UseVisualStyleBackColor = true;
             // 
+            // rgName
+            // 
+            this.rgName.Location = new System.Drawing.Point(99, 18);
+            this.rgName.Name = "rgName";
+            this.rgName.Size = new System.Drawing.Size(146, 20);
+            this.rgName.TabIndex = 3;
+            // 
             // rgPassword
             // 
             this.rgPassword.Location = new System.Drawing.Point(99, 59);
@@ -186,15 +192,6 @@
             this.label3.TabIndex = 2;
             this.label3.Text = "Password:";
             // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(11, 21);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(68, 13);
-            this.label4.TabIndex = 1;
-            this.label4.Text = "Player name:";
-            // 
             // button1
             // 
             this.button1.DialogResult = System.Windows.Forms.DialogResult.Cancel;
@@ -206,12 +203,14 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.btnCancel_Click);
             // 
-            // rgName
+            // label4
             // 
-            this.rgName.Location = new System.Drawing.Point(99, 18);
-            this.rgName.Name = "rgName";
-            this.rgName.Size = new System.Drawing.Size(146, 20);
-            this.rgName.TabIndex = 3;
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(11, 21);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(68, 13);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "Player name:";
             // 
             // LoginForm
             // 
@@ -219,7 +218,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.CancelButton = this.btnCancel;
-            this.ClientSize = new System.Drawing.Size(278, 164);
+            this.ClientSize = new System.Drawing.Size(287, 201);
             this.Controls.Add(this.tabControl1);
             this.Controls.Add(this.lbInfo);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
