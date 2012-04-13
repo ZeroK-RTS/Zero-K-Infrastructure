65ca2931003e77363de6395c4eb3b57ac0d06585
Licho <jalich@gmail.com>
2012-04-13 16:16:47 +0000

    fixed ILMerge for springie
    svn path=/trunk/other/; revision=6029

--- Springie/merge.cmd
+++ Springie/merge.cmd
@@ -1,4 +1,4 @@
 cd bin
 del *.pdb
-ilmerge /out:../Springie.exe Springie.exe LobbyClient.dll MonoTorrent.dll PlasmaShared.dll PlasmaDownloader.dll Ionic.Zip.Reduced.dll
-
+ilmerge /v4 /out:../Springie.exe Springie.exe LobbyClient.dll MonoTorrent.dll PlasmaShared.dll PlasmaDownloader.dll Ionic.Zip.Reduced.dll
+cd ..
