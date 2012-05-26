ace9dbde1f5dbc8186eb65305826bde81285cecf
Licho <jalich@gmail.com>
2012-05-26 23:31:11 +0000

    DEPLOY_SITE another try
    svn path=/trunk/other/; revision=6627

--- Shared/PlasmaShared/Utils.cs
+++ Shared/PlasmaShared/Utils.cs
@@ -87,6 +87,26 @@ namespace PlasmaShared
 
         public static double StdDev(this IEnumerable<double> values)
         {
+            double ret = 0;
+            int count = values.Count();
+            if (count > 1)
+            {
+                //Compute the Average
+                double avg = values.Average();
+
+                //Perform the Sum of (value-avg)^2
+                double sum = values.Sum(d => (d - avg) * (d - avg));
+
+                //Put it all together
+                ret = Math.Sqrt(sum / count);
+            }
+            return ret;
+        }
+
+
+        /*
+        public static double StdDev(this IEnumerable<double> values)
+        {
             // ref: http://warrenseen.com/blog/2006/03/13/how-to-calculate-standard-deviation/
             double mean = 0.0;
             double sum = 0.0;
@@ -104,6 +124,8 @@ namespace PlasmaShared
 
             return stdDev;
         }
+        */
+
 
 		public static bool CanRead(string filename)
 		{
