c6bac63ff3b4b59dde54759e2835a57d0d0a2033
Licho <jalich@gmail.com>
2012-05-26 19:50:03 +0000

    DEPLOY_SITE fix split on start
    svn path=/trunk/other/; revision=6619

--- Zero-K.info/SpringieInterface/Balancer.cs
+++ Zero-K.info/SpringieInterface/Balancer.cs
@@ -518,7 +518,7 @@ namespace ZeroKWeb.SpringieInterface
                 var splitTo =
                     tas.ExistingBattles.Values.FirstOrDefault(
                         x =>
-                        !x.Founder.IsInGame && x.NonSpectatorCount == 0 && x.Founder.Name != context.AutohostName &&
+                        !x.Founder.IsInGame && x.NonSpectatorCount == 0 && x.Founder.Name != context.AutohostName && !x.IsPassworded && 
                         x.Founder.Name.TrimEnd('0', '1', '2', '3', '4', '5', '6', '7', '8', '9') == context.AutohostName.TrimEnd('0', '1', '2', '3', '4', '5', '6', '7', '8', '9'));
 
                 if (splitTo != null)
