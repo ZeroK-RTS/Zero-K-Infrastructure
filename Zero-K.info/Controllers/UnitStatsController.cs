6b2d6b95d3f3e012bf520a03c13c82d1a5525ad7
Licho <jalich@gmail.com>
2012-05-30 11:23:49 +0000

    DEPLOY_SITE fix
    svn path=/trunk/other/; revision=6722

--- Zero-K.info/Controllers/UnitStatsController.cs
+++ Zero-K.info/Controllers/UnitStatsController.cs
@@ -12,11 +12,13 @@ namespace ZeroKWeb.Controllers
         //
         // GET: /UnitStats/
 
-        public ActionResult Index(string id = "tremor") {
+        public ActionResult Index(string id = "trem") {
             var db = new ModStatsDb();
             var unit = id;
-            var gameFilter = db.Games.OrderByDescending(x=>x.GameID).Take(500);
+            var gameFilter = db.Games.OrderByDescending(x=>x.GameID);
             var results = new List<MonthEntry>();
+
+            if (!db.Units.Any(x => x.Unit1 == unit)) return Content("Unit not found");
 
             foreach (var m in gameFilter.GroupBy(x => x.Created.Year * 13 + x.Created.Month)) {
                 var gameIds = m.Select(x => x.GameID).ToList();
@@ -29,21 +31,22 @@ namespace ZeroKWeb.Controllers
 
                 if (!unitCostHealth.ContainsKey(unit)) continue;
                 var unitDamageRecieved = damages.Where(x => x.VictimUnit == unit).Sum(x => (double?)x.Damage1);
-                
-                var costDamageRecieved = unitDamageRecieved*unitCostHealth[unit];
-                var costDamageDone = damages.Where(x => x.AttackerUnit == unit).Sum(x => x.Damage1*unitCostHealth[x.VictimUnit]);
+                if (unitDamageRecieved == null) continue;
 
-                results.Add(new MonthEntry() { Month = new DateTime(m.Key/13, m.Key%13, 1), CostDamagedLost = costDamageDone/costDamageRecieved, CostDamagedInvest = costDamageDone/totalSpending });
+                var costDamageRecieved = unitDamageRecieved*unitCostHealth[unit];
+                double costDamageDone = 0;
+                foreach (var dd in damages.Where(x => x.AttackerUnit == unit).GroupBy(x=>x.VictimUnit).Select(x=>new {unit = x.Key, sum =x.Sum(y=>y.Damage1)})) costDamageDone +=dd.sum * (unitCostHealth[dd.unit]??0);
+                
+
+                results.Add(new MonthEntry() { Month = new DateTime(m.Key/13, m.Key%13, 1), CostDamagedLost = (costDamageDone/costDamageRecieved)??0, CostDamagedInvest = (costDamageDone/totalSpending)??0 });
             }
 
-            var data = results.OrderBy(x => x.Month);
+            var data = results.OrderBy(x => x.Month).ToList();
             var chart = new Chart(1500, 700, ChartTheme.Blue);
-
+            
             chart.AddTitle(string.Format("{0} effectivity", unit));
-            chart.AddLegend("Monthly averages", "ma");
-
-            chart.AddSeries("Cost damaged/lost", "Line", xValue: data.Select(x => x.Month), yValues: data.Select(x => x.CostDamagedLost), legend: "ma");
-            chart.AddSeries("Cost damaged/invest", "Line", xValue: data.Select(x => x.Month), yValues: data.Select(x => x.CostDamagedInvest), legend: "ma");
+            chart.AddSeries("Cost damaged/lost", "Line", xValue: data.Select(x => x.Month), yValues: data.Select(x => x.CostDamagedLost));
+            chart.AddSeries("Cost damaged/invest", "Line", xValue: data.Select(x => x.Month), yValues: data.Select(x => x.CostDamagedInvest ?? 0), legend: "ma");
 
             return File(chart.GetBytes("png"), "image/png");
         }
