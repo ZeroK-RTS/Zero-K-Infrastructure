e6487e64bd5eaf41b12111fd4284d533868c77b5
xponen <msafwan86msafwan86_nnss@hotmail.com>
2012-06-06 22:23:08 +0000

    "NotifyBarContainer.cs": fixed bar getting bigger and bigger when user has High-DPI setting. It happen because Window scaled the bar again and again (from its original height) until it grow soo big, and so the fix scaled it down first. --only tested to work on 1 computer... yet
    svn path=/trunk/other/; revision=6798

--- ZeroKLobby/Notifications/NotifyBarContainer.cs
+++ ZeroKLobby/Notifications/NotifyBarContainer.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Windows.Forms;
+using System.Drawing;
 
 namespace ZeroKLobby.Notifications
 {
@@ -16,7 +17,16 @@ namespace ZeroKLobby.Notifications
             btnStop.Click += stop_Click;
             btnStop.Image = Resources.Remove;
             var control = barContent.GetControl();
-            Height = control.Height + 8; // margin
+            
+            //-- code for countering the 'DPI scaling cause bar to grow infinitely big'. The bar grew larger because each time the bar was created the bar is scaled to high-DPI, but since old height is used again for new bar: the bar get bigger & bigger after each scaling.
+            Graphics formGraphics = this.CreateGraphics(); //Reference: http://msdn.microsoft.com/en-us/library/system.drawing.graphics.dpix.aspx
+            float formDPIvertical = formGraphics.DpiY; //get current DPI
+            float scaleDownRatio = 96 / formDPIvertical; //get scaleDown ratio (to counter-act DPI virtualization/scaling)
+            //--
+            float newHeight = control.Height + 8; //get old height + margin
+            newHeight = newHeight*scaleDownRatio; //multiply the scaledown to old height
+
+            Height = (int) newHeight; // apply height (the margin will again be automatically be scaled up according to Window's DPI) 
             tableLayoutPanel1.Controls.Add(control, 1, 0);
             control.Dock = DockStyle.Fill;
             Dock = DockStyle.Top;
