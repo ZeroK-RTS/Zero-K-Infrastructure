67c685b5a6c6b6b2b07a128ad7f938e92c5a6709
m_ritoniemi <m_ritoniemi@windowslive.com>
2012-06-12 09:43:44 +0000

    Since the map can't be changed while a game is in progress, a vote can't be started about mapchange either.
    svn path=/trunk/other/; revision=6887

--- Springie/Springie/autohost/Polls/VoteMap.cs
+++ Springie/Springie/autohost/Polls/VoteMap.cs
@@ -13,31 +13,39 @@ namespace Springie.autohost.Polls
         {
             winCount = 0;
             question = null;
-            if (words.Length > 0)
+			if (spring.IsRunning)
             {
-                string[] vals;
-                int[] indexes;
-                ah.FilterMaps(words, out vals, out indexes);
-                if (vals.Length > 0)
-                {
-                    map = vals[0];
-                    var resource = ah.cache.FindResourceData(new string[]{map}, ResourceType.Map);
-                    question = string.Format("Change map to {0} http://zero-k.info/Maps/Detail/{1} ?", map, resource[0].ResourceID);
-                    return true;
-                }
-                else
-                {
-                    AutoHost.Respond(tas, spring, e, "Cannot find such map");
-                    return false;
-                }
+                AutoHost.Respond(tas, spring, e, "Cannot change map while the game is running");
+                return false;
             }
             else
             {
-                question = "Do you want to change to a suitable random map?";
-                return true;
+                if (words.Length > 0)
+                {
+                    string[] vals;
+                    int[] indexes;
+                    ah.FilterMaps(words, out vals, out indexes);
+                    if (vals.Length > 0)
+                    {
+                        map = vals[0];
+                        var resource = ah.cache.FindResourceData(new string[]{map}, ResourceType.Map);
+                        question = string.Format("Change map to {0} http://zero-k.info/Maps/Detail/{1} ?", map, resource[0].ResourceID);
+                        return true;
+                    }
+                    else
+                    {
+                        AutoHost.Respond(tas, spring, e, "Cannot find such map");
+                        return false;
+                    }
+                }
+                else
+                {
+                    question = "Do you want to change to a suitable random map?";
+                    return true;
+                }
             }
         }
-
+		
         protected override void SuccessAction()
         {
             if (string.IsNullOrEmpty(map))
