d9d50bdfd7d77690237be46b8d0da6e1fef10d74
xponen <msafwan86msafwan86_nnss@hotmail.com>
2012-06-16 00:32:54 +0000

    'BattleBar.cs': set the default button to "No" for pop-up dialog:"Spring Engine has crash.." because if you use 'Snap-to' (a windows mouse setting) you'll sometimes accidentally click on "Yes" (and all your springsetting.cfg will reverted to default). ie: using touchpad with snap-to to easily click "Yes" but unfortunately you press "Yes" to default your setting :(
    svn path=/trunk/other/; revision=6913

--- ZeroKLobby/Notifications/BattleBar.cs
+++ ZeroKLobby/Notifications/BattleBar.cs
@@ -64,7 +64,10 @@ namespace ZeroKLobby.Notifications
 
 					if (e.Data) {
 						Program.MainWindow.InvokeFunc(() => {
-                            if (MessageBox.Show("Do you want me to set Low details?", "Spring engine has crashed, update your video and audio drivers please!", MessageBoxButtons.YesNo) == DialogResult.Yes)
+
+                            MessageBoxDefaultButton defaultButton = MessageBoxDefaultButton.Button2;
+                            MessageBoxIcon icon = MessageBoxIcon.None;
+                            if (MessageBox.Show("Do you want me to set Low details?", "Spring engine has crashed, update your video and audio drivers please!", MessageBoxButtons.YesNo, icon, defaultButton) == DialogResult.Yes)
                             {
                                 Program.Conf.UseSafeMode = true;
                                 Program.EngineConfigurator.Configure(true, 0);
