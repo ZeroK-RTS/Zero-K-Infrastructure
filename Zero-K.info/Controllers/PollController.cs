90c13398c09a42e8e4b20250a4221349b088ac63
KingRaptor <kingraptorowns@gmail.com>
2012-07-23 04:08:22 +0000

    DEPLOY_SITE Impeachment votes do nothing until they can be fixed
    svn path=/trunk/other/; revision=7396

--- Zero-K.info/Controllers/PollController.cs
+++ Zero-K.info/Controllers/PollController.cs
@@ -42,10 +42,11 @@ namespace ZeroKWeb.Controllers
                         var entry = acc.AccountRolesByAccountID.FirstOrDefault(x => x.RoleTypeID == p.RoleTypeID);
                         if (entry != null)
                         {
-                            acc.AccountRolesByAccountID.Remove(entry);
-                            db.Events.InsertOnSubmit(Global.CreateEvent("{0} was removed from the {1} role of {2} by a vote - {3} for, {4} against", acc, (object)p.Clan ?? p.Faction, p.RoleType, yes, no));
+                            // FIXME
+                            //acc.AccountRolesByAccountID.Remove(entry);
+                            //db.Events.InsertOnSubmit(Global.CreateEvent("{0} was removed from the {1} role of {2} by a vote - {3} for, {4} against", acc, (object)p.Clan ?? p.Faction, p.RoleType, yes, no));
                             
-                            AuthServiceClient.SendLobbyMessage(acc,string.Format("You were recalled from the function of {0} by a vote", p.RoleType.Name));
+                            //AuthServiceClient.SendLobbyMessage(acc,string.Format("You were recalled from the function of {0} by a vote", p.RoleType.Name));
                         }
                     }
                     else
