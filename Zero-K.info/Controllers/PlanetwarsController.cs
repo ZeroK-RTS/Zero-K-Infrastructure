66756ce11637dc37fe634612a902fb53b43af9f7
KingRaptor <kingraptorowns@gmail.com>
2012-08-04 05:28:12 +0000

    Proper handling for attacking a defended planet with insufficient bombers.
    svn path=/trunk/other/; revision=7672

--- Zero-K.info/Controllers/PlanetwarsController.cs
+++ Zero-K.info/Controllers/PlanetwarsController.cs
@@ -31,7 +31,7 @@ namespace ZeroKWeb.Controllers
             if (avail > 0) {
                 double defense = planet.PlanetStructures.Where(x => x.IsActive).Sum(x => x.StructureType.EffectDropshipDefense) ?? 0;
                 double effective = avail - defense;
-                if (avail < 0) return Content("Enemy defenses completely block your ships");
+                if (effective < 0) return Content("Enemy defenses completely block your ships");
 
                 acc.SpendBombers(avail);
                 if (useWarp == true) acc.SpendWarps(avail);
