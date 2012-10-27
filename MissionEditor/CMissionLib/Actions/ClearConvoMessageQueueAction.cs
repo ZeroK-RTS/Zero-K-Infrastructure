6bad7c205a4d380204b21f8524e8aec24cdad4e2
KingRaptor <kingraptorowns@gmail.com>
2012-10-27 10:48:32 +0000

    ClearConvoMessageQueueAction.cs
    svn path=/trunk/other/; revision=8297

---
+++ MissionEditor/CMissionLib/Actions/ClearConvoMessageQueueAction.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace CMissionLib.Actions
+{
+	[DataContract]
+	public class ClearConvoMessageQueueAction : Action
+	{
+
+		public override LuaTable GetLuaTable(Mission mission)
+		{
+			return new LuaTable();
+		}
+
+		public override string GetDefaultName()
+		{
+            return "Clear Convo Message Queue";
+		}
+	}
+}
