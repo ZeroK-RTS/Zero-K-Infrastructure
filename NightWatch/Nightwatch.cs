ff2ed772a42357c8f1cfcea49315b575e98c35f3
Licho <jalich@gmail.com>
2013-02-18 20:30:03 +0000

    DEPLOY_SITE formatting message fix
    svn path=/trunk/other/; revision=9460

--- NightWatch/Nightwatch.cs
+++ NightWatch/Nightwatch.cs
@@ -87,7 +87,7 @@ namespace CaTracker
 		        {
 		            tas.Say(TasClient.SayPlace.Channel,
 		                    "zkdev",
-		                    string.Format("WOHOO! {0:d} New contribution of {1}€ by {2}  {3}", c.Time, c.Contributor, c.Euros, c.Message),
+		                    string.Format("WOHOO! {0:d} New contribution of {1:F2}€ by {2}  {3}", c.Time, c.Euros, c.Contributor, c.Message),
 		                    true);
 		            if (c.Account == null)
 		                tas.Say(TasClient.SayPlace.Channel,
