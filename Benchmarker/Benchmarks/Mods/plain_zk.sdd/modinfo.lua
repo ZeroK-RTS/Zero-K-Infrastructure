0923fa9ca58345d4391ef832032852d0046c054c
Licho <jalich@gmail.com>
2013-05-11 14:47:33 +0000

    testing tweaks
    svn path=/trunk/other/; revision=10091

--- Benchmarker/Benchmarks/Mods/plain_zk.sdd/modinfo.lua
+++ Benchmarker/Benchmarks/Mods/plain_zk.sdd/modinfo.lua
@@ -3,6 +3,6 @@ local modinfo = {
   description   =	[[Mission Mutator]],
   modtype       =	[[1]],
   shortname     =	[[Tutorial 2 - A Day Out]],
-  depend = {[[Zero-K v1.0.7.7]]},
+  depend = {[[Zero-K v1.1.3.6]]},
 }
 return modinfo
