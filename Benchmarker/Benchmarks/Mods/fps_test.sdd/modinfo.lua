c03b04f64c97278e1153a6aab06800784f9b3c69
Licho <jalich@gmail.com>
2013-05-11 20:27:19 +0000

    rename for clarity
    svn path=/trunk/other/; revision=10108

--- Benchmarker/Benchmarks/Mods/fps_test.sdd/modinfo.lua
+++ Benchmarker/Benchmarks/Mods/fps_test.sdd/modinfo.lua
@@ -2,7 +2,6 @@ local modinfo = {
   name          =	[[Tutorial 2 - A Day Out r14]],
   description   =	[[Mission Mutator]],
   modtype       =	[[1]],
-  shortname     =	[[Tutorial 2 - A Day Out]],
   depend = {'Zero-K v1.1.3.6'},
 }
 return modinfo
