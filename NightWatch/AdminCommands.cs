50e9a4677001bd903baa748ddcdd375bac086466
KingRaptor <kingraptorowns@gmail.com>
2013-05-18 03:05:40 +0000

    Nightwatch support for kicking | DEPLOY_SITE
    svn path=/trunk/other/; revision=10168

---
+++ NightWatch/AdminCommands.cs
@@ -0,0 +1,44 @@
+using System.Linq;
+using LobbyClient;
+using ZkData;
+
+namespace CaTracker
+{
+    public class AdminCommands
+    {
+        readonly TasClient tas;
+
+        public AdminCommands(TasClient tas)
+        {
+            this.tas = tas;
+            tas.Said += tas_Said;
+        }
+
+        void tas_Said(object sender, TasSayEventArgs e)
+        {
+            if (e.Place == TasSayEventArgs.Places.Normal)
+            {
+                if (e.Text.StartsWith("!kick"))
+                {
+                    var db = new ZkDataContext();
+                    var acc = Account.AccountByLobbyID(db, tas.ExistingUsers[e.UserName].LobbyID);
+                    if (acc.IsZeroKAdmin || acc.IsLobbyAdministrator)
+                    {
+                        var parts = e.Text.Split(' ');
+                        if (parts.Length != 2) tas.Say(TasClient.SayPlace.User, e.UserName, "!kick [player] [reason]", false);
+                        else
+                        {
+                            var player = tas.ExistingUsers.FirstOrDefault(x => x.Key == parts[1]).Key;
+                            var reason = parts[2] ?? "";
+                            if (player != null)
+                            {
+                                tas.AdminKickFromLobby(player, reason);
+                            }
+                            else tas.Say(TasClient.SayPlace.User, e.UserName, "Not a valid player name", false);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
