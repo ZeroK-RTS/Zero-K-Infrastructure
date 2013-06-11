6ab847bcf99baa13a2db48b8a944c802008ba674
Licho <jalich@gmail.com>
2013-06-11 19:42:06 +0000

    also show w_drawscr_dt == 1/FPS
    svn path=/trunk/other/; revision=10430

--- Benchmarker/Benchmarks/games/fps_war.sdd/LuaUI/Widgets/dbg_fpslog_direct.lua
+++ Benchmarker/Benchmarks/games/fps_war.sdd/LuaUI/Widgets/dbg_fpslog_direct.lua
@@ -11,11 +11,24 @@ end
 
 local START_GRACE = 64
 
+local GetTimer = Spring.GetTimer 
+local DiffTimers = Spring.DiffTimers 
+local frameTimer= GetTimer()
+
 function widget:Update(dt)
 	if not gameStart then return end
 	if (Spring.GetGameFrame() > START_GRACE) then
 		Spring.Echo("!transmitlobby w_update_dt: "..dt)
 	end
+end
+
+function widget:DrawScreen()
+	if not gameStart then return end
+	local newTimer = GetTimer()
+	if (Spring.GetGameFrame() > START_GRACE) then
+		Spring.Echo("!transmitlobby w_drawscr_dt: "..DiffTimers(newTimer, frameTimer))
+	end
+	frameTimer = newTimer
 end
 
 
