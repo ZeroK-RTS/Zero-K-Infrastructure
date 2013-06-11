9bf0a45441a2189ab5881ab5bc5607db6c202a49
Licho <jalich@gmail.com>
2013-06-11 19:47:09 +0000

    dump fps instead
    svn path=/trunk/other/; revision=10431

--- Benchmarker/Benchmarks/games/fps_war.sdd/LuaUI/Widgets/dbg_fpslog_direct.lua
+++ Benchmarker/Benchmarks/games/fps_war.sdd/LuaUI/Widgets/dbg_fpslog_direct.lua
@@ -11,26 +11,13 @@ end
 
 local START_GRACE = 64
 
-local GetTimer = Spring.GetTimer 
-local DiffTimers = Spring.DiffTimers 
-local frameTimer= GetTimer()
-
 function widget:Update(dt)
 	if not gameStart then return end
 	if (Spring.GetGameFrame() > START_GRACE) then
 		Spring.Echo("!transmitlobby w_update_dt: "..dt)
+		Spring.Echo("!transmitlobby w_fps: "..Spring.GetFPS())
 	end
 end
-
-function widget:DrawScreen()
-	if not gameStart then return end
-	local newTimer = GetTimer()
-	if (Spring.GetGameFrame() > START_GRACE) then
-		Spring.Echo("!transmitlobby w_drawscr_dt: "..DiffTimers(newTimer, frameTimer))
-	end
-	frameTimer = newTimer
-end
-
 
 
 function widget:GameStart()
