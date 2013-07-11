ZeroK-RTS/Zero-K-Infrastructure

commit 6d19105c1f8a76653cb05797a3640be952c0d089

Internal browser changes: - turn-off IE7 compatibility tweak, use IE9 instead (this fix hang and error when visiting non-ZK website) - fix address bar for non-ZK website (now address bar show correct address & Back/Forward will work) - "open-in-new-window" right click option now work, and non-ZK page no longer inconsistently open in new window if link is clicked in forum.
Navigation bar button arrangement (barely noticeable!):
- if "use external browser" then all hyperlink button is arranged to right with slight/very-slight color difference (while Multiplayer, Chat, Setting is to the left) 

svn path=/trunk/other/; revision=10702

ZeroKLobby/MicroLobby/BrowserTab.cs
@@ -22,36 +22,39 @@ namespace ZeroKLobby
                 navigatingTo = head;
                 Navigate(head);
             };
+            base.DocumentCompleted += new WebBrowserDocumentCompletedEventHandler(UpdateURL); //This will call "UpdateURL()" when page finish loading
         }
 
-        protected override void OnNewWindow(System.ComponentModel.CancelEventArgs e) {
-            e.Cancel = true;
-            Navigate(StatusText);
-        }
+        //protected override void OnNewWindow(System.ComponentModel.CancelEventArgs e) //This block "Open In New Window" button.
+        //{
+        //    e.Cancel = true;
+        //    Navigate(StatusText);
+        //}
 
 
-        protected override void OnNavigated(WebBrowserNavigatedEventArgs e)
-        {
-            base.OnNavigated(e);
-            if (navigatingTo == e.Url.ToString())
-            {
-                if (!initLoad) {
-                    var final = navigatingTo;
-                    if (navigatedIndex == navigatedPlaces.Count) navigatedPlaces.Add(final);
-                    else navigatedPlaces[navigatedIndex] = final;
-                    navigatedIndex++;
-                    Program.MainWindow.navigationControl.Path = Uri.UnescapeDataString(final);
-                }
-                initLoad = false;
-            }
-        }
-
-        protected override void OnNavigating(WebBrowserNavigatingEventArgs e)
+        //protected override void OnNavigated(WebBrowserNavigatedEventArgs e) //This intercept which URL finish loading (including Advertisement)
+        //{
+        //    base.OnNavigated(e);
+        //    if (navigatingTo == e.Url.ToString()) //this store visited URL in a list:
+        //    {
+        //        if (!initLoad)
+        //        {
+        //            var final = navigatingTo;
+        //            if (navigatedIndex == navigatedPlaces.Count) navigatedPlaces.Add(final);
+        //            else navigatedPlaces[navigatedIndex] = final;
+        //            navigatedIndex++;
+        //            Program.MainWindow.navigationControl.Path = Uri.UnescapeDataString(final);
+        //        }
+        //        initLoad = false;
+        //    }
+        //}
+        
+        protected override void OnNavigating(WebBrowserNavigatingEventArgs e) //this intercept URL navigation induced when user click on link or during page loading
         {
             var url = e.Url.ToString();
-            if (string.IsNullOrEmpty(e.TargetFrameName) && url.Contains("zero-k") && !url.StartsWith("javascript:"))
+            if (string.IsNullOrEmpty(e.TargetFrameName) && url.Contains("zero-k") && !url.StartsWith("javascript:")) //if navigation is within Zero-K
             {
-                var nav = Program.MainWindow.navigationControl.GetInavigatableByPath(url);
+                var nav = Program.MainWindow.navigationControl.GetInavigatableByPath(url); //check which TAB this URL represent
                 if (nav == null || nav == this) {
                     navigatingTo = url;
                 }
@@ -67,18 +70,34 @@ namespace ZeroKLobby
 
         public string PathHead { get { return pathHead; } }
 
-        public bool TryNavigate(params string[] path)
+        public bool TryNavigate(params string[] path) //navigation induced by call from "NavigationControl.cs"
         {
             var pathString = String.Join("/", path);
-            if (!pathString.StartsWith(PathHead)) return false;
-            var url = Url != null ? Url.ToString() : "";
-            if (navigatingTo == pathString || pathString == url) return true; // already navigating there
-
-            navigatingTo = pathString;
-            Navigate(pathString);
-            return true;
+            if (navigatingTo == pathString) { return true; }  //already navigating there, just return TRUE
+            if (pathString.StartsWith(PathHead)) 
+            {
+                navigatingTo = pathString;
+                Navigate(pathString);
+                return true; //the URL contain intended header, return TRUE and Navigate() to URL
+            }
+            if (Url != null && !string.IsNullOrEmpty(Url.ToString()))
+            {
+                var url = Url.ToString();
+                if (pathString == url) { return true; } //already there, just return TRUE
+            }
+            int count = 0;
+            foreach (String previousURL in navigatedPlaces)
+            {
+                if (previousURL == pathString) 
+                {
+                    navigatingTo = pathString;
+                    Navigate(pathString);
+                    return true; //the URL is from history, return TRUE and Navigate() to URL
+                }
+                count++;
+            }
+            return false; //URL has nothing to do with this WebBrowser instance,  just return FALSE
         }
-
 
         public bool Hilite(HiliteLevel level, params string[] path)
         {
@@ -95,5 +114,29 @@ namespace ZeroKLobby
         }
 
         public bool CanReload { get { return true; }}
+
+        private void UpdateURL(object sender, WebBrowserDocumentCompletedEventArgs e) //is called when webpage finish loading
+        {   //Reference: http://msdn.microsoft.com/en-us/library/system.windows.forms.webbrowser.aspx
+            
+            //This update URL textbox & add the page to NavigationBar's history (so that Back&Forward button can be used):
+            navigatingTo = ((WebBrowser)sender).Url.ToString();
+            Program.MainWindow.navigationControl.Path = ((WebBrowser)sender).Url.ToString();
+            //NOTE on operation: 
+            //setting the "path" to URL will cause "GoToPage(path)" to be called, 
+            //"GoToPage(path)" then will loop over all tab control (including this instance of "BrowserTab"), which will then call "TryToNavigate(path)".
+            //SINCE the "navigatingTo" is already set to this URL here, so when "TryToNavigate(path)" is called it matches to the current URL and return TRUE.
+            //when "TryToNavigate(path)" return TRUE it will cause the page to be pushed to stack & the Back button will work.
+            
+            if (!initLoad) //skip the first URL (?)
+            {      
+                //This store previously visited URL for checking later. This list is unordered (it will have duplicate entry & not related at all to sequence in Forward/Backward button)
+                var final = navigatingTo;
+                if (navigatedIndex == navigatedPlaces.Count) { navigatedPlaces.Add(final); }//if at end of table then use "Add"
+                else navigatedPlaces[navigatedIndex] = final;
+                navigatedIndex++;
+            }
+            initLoad = false;
+        }
+         
     }
 }

ZeroKLobby/NavigationControl.cs
@@ -13,7 +13,6 @@ namespace ZeroKLobby
         bool CanGoBack { get { return backStack.Any(); } }
         bool CanGoForward { get { return forwardStack.Any(); } }
 
-
         NavigationStep CurrentPage {
             get { return _currentPage; }
             set {
@@ -58,43 +57,75 @@ namespace ZeroKLobby
 
                 if (CurrentPage != null && CurrentPage.ToString() == value) return; // we are already there, no navigation needed
 
-                var step = GoToPage(value.Split('/')); //go to page and is not reload
+                var step = GoToPage(value.Split('/')); //go to page
                 if (step != null) {
                     if (CurrentPage != null && CurrentPage.ToString() != value) backStack.Push(CurrentPage);
                     CurrentPage = step;
                 }
-                else if (value.StartsWith("http://") || value.StartsWith("https://")) Program.BrowserInterop.OpenUrl(value);
+                else if (value.StartsWith("http://") || value.StartsWith("https://") || value.StartsWith("www.")) { Program.BrowserInterop.OpenUrl(value); } //this open external browser
+
             }
         }
 
         public NavigationControl() {
             InitializeComponent();
 
-            ButtonList = new List<ButtonInfo>()
+            if (Environment.OSVersion.Platform != PlatformID.Unix && !Program.Conf.UseExternalBrowser)
             {
-                new ButtonInfo() { Label = "HOME", TargetPath = "http://zero-k.info/", Icon= Buttons.home, Height = 32,},
-                new ButtonInfo()
+                ButtonList = new List<ButtonInfo>() //normal arrangement
                 {
-                    Label = "SINGLEPLAYER",
-                    TargetPath = "http://zero-k.info/Missions",
-                    Icon = Buttons.spherebot,
-                    Width = 128,
-                    Height = 32,
-                },
-                new ButtonInfo()
+                    new ButtonInfo() { Label = "HOME", TargetPath = "http://zero-k.info/", Icon= Buttons.home, Height = 32,},
+                    new ButtonInfo()
+                    {
+                        Label = "SINGLEPLAYER",
+                        TargetPath = "http://zero-k.info/Missions",
+                        Icon = Buttons.spherebot,
+                        Width = 128,
+                        Height = 32,
+                    },
+                    new ButtonInfo()
+                    {
+                        Label = "MULTIPLAYER",
+                        TargetPath = "battles", Icon =  ZklResources.battle,
+                        Width = 128,
+                        Height = 32,
+                    },
+                    new ButtonInfo() { Label = "CHAT", TargetPath = "chat", Icon= ZklResources.chat, Height = 32, },
+                    new ButtonInfo() { Label = "PLANETWARS", TargetPath = "http://zero-k.info/PlanetWars", Height = 32,  },
+                    new ButtonInfo() { Label = "MAPS", TargetPath = "http://zero-k.info/Maps", Icon = Buttons.map, Height = 32,  },
+                    new ButtonInfo() { Label = "REPLAYS", TargetPath = "http://zero-k.info/Battles", Icon = Buttons.video_icon, Height = 32, },
+                    new ButtonInfo() { Label = "FORUM", TargetPath = "http://zero-k.info/Forum", Height = 32, },
+                    new ButtonInfo() { Label = "SETTINGS", TargetPath = "settings", Icon = Buttons.settings, Height = 32, },
+                };
+            }
+            else //((Environment.OSVersion.Platform == PlatformID.Unix || Program.Conf.UseExternalBrowser)
+            {
+                ButtonList = new List<ButtonInfo>() //website button on the right
                 {
-                    Label = "MULTIPLAYER",
-                    TargetPath = "battles", Icon =  ZklResources.battle,
-                    Width = 128,
-                    Height = 32,
-                },
-                new ButtonInfo() { Label = "CHAT", TargetPath = "chat", Icon= ZklResources.chat, Height = 32, },
-                new ButtonInfo() { Label = "PLANETWARS", TargetPath = "http://zero-k.info/PlanetWars", Height = 32,  },
-                new ButtonInfo() { Label = "MAPS", TargetPath = "http://zero-k.info/Maps", Icon = Buttons.map, Height = 32,  },
-                new ButtonInfo() { Label = "REPLAYS", TargetPath = "http://zero-k.info/Battles", Icon = Buttons.video_icon, Height = 32, },
-                new ButtonInfo() { Label = "FORUM", TargetPath = "http://zero-k.info/Forum", Height = 32, },
-                new ButtonInfo() { Label = "SETTINGS", TargetPath = "settings", Icon = Buttons.settings, Height = 32, },
-            };
+                    new ButtonInfo()
+                    {
+                        Label = "MULTIPLAYER",
+                        TargetPath = "battles", Icon =  ZklResources.battle,
+                        Width = 128,
+                        Height = 32,
+                    },
+                    new ButtonInfo() { Label = "CHAT", TargetPath = "chat", Icon= ZklResources.chat, Height = 32, },
+                    new ButtonInfo() { Label = "SETTINGS", TargetPath = "settings", Icon = Buttons.settings, Height = 32, },
+                    new ButtonInfo() { Label = "HOME", TargetPath = "http://zero-k.info/", Icon= Buttons.home, Height = 32,},
+                    new ButtonInfo()
+                    {
+                        Label = "SINGLEPLAYER",
+                        TargetPath = "http://zero-k.info/Missions",
+                        Icon = Buttons.spherebot,
+                        Width = 128,
+                        Height = 32,
+                    },
+                    new ButtonInfo() { Label = "PLANETWARS", TargetPath = "http://zero-k.info/PlanetWars", Height = 32,  },
+                    new ButtonInfo() { Label = "MAPS", TargetPath = "http://zero-k.info/Maps", Icon = Buttons.map, Height = 32,  },
+                    new ButtonInfo() { Label = "REPLAYS", TargetPath = "http://zero-k.info/Battles", Icon = Buttons.video_icon, Height = 32, },
+                    new ButtonInfo() { Label = "FORUM", TargetPath = "http://zero-k.info/Forum", Height = 32, },
+               };
+            }
 
             Instance = this;
 
@@ -120,8 +151,23 @@ namespace ZeroKLobby
             AddTabPage(new AdvertiserWindow(), "Advertiser");
             AddTabPage(new DownloaderTab(), "Rapid");
             
-            foreach (var but in ButtonList) flowLayoutPanel1.Controls.Add(but.GetButton());
+            foreach (var but in ButtonList) flowLayoutPanel1.Controls.Add(but.GetButton());     
             flowLayoutPanel1.BringToFront();
+
+            if (Environment.OSVersion.Platform == PlatformID.Unix || Program.Conf.UseExternalBrowser)//make these button more distinct when using external browser
+            {
+                foreach (Control button in flowLayoutPanel1.Controls)
+                {
+                    if (button.Text == "HOME" || button.Text == "MAPS" ||
+                        button.Text == "PLANETWARS" || button.Text == "REPLAYS" ||
+                        button.Text == "SINGLEPLAYER" || button.Text == "FORUM")
+                    {
+                        button.BackColor = System.Drawing.Color.FromArgb(255, 60, 75, 60);//NOTE: this color won't reset if using external browser because button will not be selectable
+                        button.Cursor = Cursors.Arrow;
+                    }
+                    if (button.Text == "HOME") { button.Margin = new Padding(15, 0, 0, 3); }
+                }
+            }
         }
 
         public INavigatable GetInavigatableByPath(string path) {
@@ -204,7 +250,7 @@ namespace ZeroKLobby
         {
             foreach (TabPage tabPage in tabControl.Controls)
             {
-                var navigatable = GetINavigatableFromControl(tabPage);
+                var navigatable = GetINavigatableFromControl(tabPage); //translate tab button into the page it represent
                 if (navigatable != null && navigatable.TryNavigate(path))
                 {
                     tabControl.SelectTab(tabPage);
@@ -260,6 +306,7 @@ namespace ZeroKLobby
             btnForward.Location = new System.Drawing.Point(btnForward.Location.X, height);
             urlBox.Location = new System.Drawing.Point(urlBox.Location.X, height);
             reloadButton1.Location = new System.Drawing.Point(reloadButton1.Location.X, height);
+            goButton1.Location = new System.Drawing.Point(goButton1.Location.X, height);
 
             //resize the "browser" (that show chat & internal browser) according to Nav bar auto resize (dynamic resizing)
             int windowHeight = this.Size.Height;
@@ -274,6 +321,11 @@ namespace ZeroKLobby
         {
             var navig = CurrentNavigatable;
             if (navig != null && navig.CanReload) navig.Reload();
+        }
+
+        private void goButton1_Click(object sender, EventArgs e)
+        {
+            Path = urlBox.Text;
         }
 
     }

ZeroKLobby/NavigationControl.Designer.cs
@@ -31,6 +31,7 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(NavigationControl));
             this.urlBox = new System.Windows.Forms.TextBox();
             this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.goButton1 = new ZeroKLobby.BitmapButton();
             this.reloadButton1 = new ZeroKLobby.BitmapButton();
             this.btnForward = new ZeroKLobby.BitmapButton();
             this.btnBack = new ZeroKLobby.BitmapButton();
@@ -39,7 +40,7 @@
             // 
             // urlBox
             // 
-            this.urlBox.Location = new System.Drawing.Point(166, 38);
+            this.urlBox.Location = new System.Drawing.Point(166, 34);
             this.urlBox.Name = "urlBox";
             this.urlBox.Size = new System.Drawing.Size(190, 20);
             this.urlBox.TabIndex = 2;
@@ -59,6 +60,23 @@
             this.flowLayoutPanel1.Size = new System.Drawing.Size(703, 28);
             this.flowLayoutPanel1.TabIndex = 5;
             // 
+            // goButton1
+            // 
+            this.goButton1.BackColor = System.Drawing.Color.Transparent;
+            this.goButton1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("goButton1.BackgroundImage")));
+            this.goButton1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.goButton1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.goButton1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.goButton1.ForeColor = System.Drawing.Color.White;
+            this.goButton1.Location = new System.Drawing.Point(362, 34);
+            this.goButton1.Name = "goButton1";
+            this.goButton1.Size = new System.Drawing.Size(35, 23);
+            this.goButton1.TabIndex = 6;
+            this.goButton1.Text = "Go";
+            this.goButton1.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.goButton1.UseVisualStyleBackColor = true;
+            this.goButton1.Click += new System.EventHandler(this.goButton1_Click);
+            // 
             // reloadButton1
             // 
             this.reloadButton1.BackColor = System.Drawing.Color.Transparent;
@@ -67,13 +85,14 @@
             this.reloadButton1.Cursor = System.Windows.Forms.Cursors.Hand;
             this.reloadButton1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.reloadButton1.ForeColor = System.Drawing.Color.White;
-            this.reloadButton1.Location = new System.Drawing.Point(362, 38);
+            this.reloadButton1.Location = new System.Drawing.Point(403, 34);
             this.reloadButton1.Name = "reloadButton1";
-            this.reloadButton1.Size = new System.Drawing.Size(75, 23);
-            this.reloadButton1.TabIndex = 6;
-            this.reloadButton1.Text = "Reload";
+            this.reloadButton1.Size = new System.Drawing.Size(58, 23);
+            this.reloadButton1.TabIndex = 7;
+            this.reloadButton1.Text = "Refresh";
             this.reloadButton1.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
             this.reloadButton1.UseVisualStyleBackColor = true;
+            this.reloadButton1.Visible = false;
             this.reloadButton1.Click += new System.EventHandler(this.reloadButton1_Click);
             // 
             // btnForward
@@ -84,7 +103,7 @@
             this.btnForward.Cursor = System.Windows.Forms.Cursors.Hand;
             this.btnForward.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnForward.ForeColor = System.Drawing.Color.White;
-            this.btnForward.Location = new System.Drawing.Point(85, 38);
+            this.btnForward.Location = new System.Drawing.Point(85, 34);
             this.btnForward.Name = "btnForward";
             this.btnForward.Size = new System.Drawing.Size(75, 23);
             this.btnForward.TabIndex = 4;
@@ -101,7 +120,7 @@
             this.btnBack.Cursor = System.Windows.Forms.Cursors.Hand;
             this.btnBack.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnBack.ForeColor = System.Drawing.Color.White;
-            this.btnBack.Location = new System.Drawing.Point(4, 38);
+            this.btnBack.Location = new System.Drawing.Point(4, 34);
             this.btnBack.Name = "btnBack";
             this.btnBack.Size = new System.Drawing.Size(75, 23);
             this.btnBack.TabIndex = 3;
@@ -126,6 +145,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
             this.BackColor = System.Drawing.Color.DimGray;
+            this.Controls.Add(this.goButton1);
             this.Controls.Add(this.reloadButton1);
             this.Controls.Add(this.flowLayoutPanel1);
             this.Controls.Add(this.btnForward);
@@ -148,5 +168,6 @@
         private BitmapButton btnBack;
         private BitmapButton btnForward;
         private BitmapButton reloadButton1;
+        private BitmapButton goButton1;
     }
 }

commit 636450a5957ed02aa2382d732349e7363d634d4b

zkl tweak: NavigationControl.cs: -fix case of ChatTab's width is larger than the size of the window (in Linux)
BrowserTab.cs: 
-fix internal browser's TAB hoping caused by r10702

Plus,
added some SuspendLayout() and ResumeLayout() pairs in various places since it was said to increase performance.

svn path=/trunk/other/; revision=10746

ZeroKLobby/Config.cs
@@ -254,7 +254,7 @@ namespace ZeroKLobby
 
         [Category("Chat")]
         [DisplayName("Color: Default text")]
-        [Description("Color for the text on chat window and on playerlist")] //added safwan [tweak]
+        [Description("Color for the text on chat window and on playerlist")]
         [XmlIgnore]
         public Color TextColor {
             get { return Color.FromArgb(TextColorInt); }

ZeroKLobby/MicroLobby/BattleListTab.cs
@@ -10,6 +10,7 @@ namespace ZeroKLobby.MicroLobby
         BattleListControl battleListControl;
 
         public BattleListTab() {
+            SuspendLayout(); //pause
             InitializeComponent();
 
             if (DesignMode) return;
@@ -32,7 +33,7 @@ namespace ZeroKLobby.MicroLobby
             // battle list
             battleListControl = new BattleListControl() { Dock = DockStyle.Fill };
             battlePanel.Controls.Add(battleListControl);
-
+            ResumeLayout();
         }
 
         public bool TryNavigate(params string[] path) {

ZeroKLobby/MicroLobby/BrowserTab.cs
@@ -65,6 +65,7 @@ namespace ZeroKLobby
             if (navigatingTo == pathString) { return true; }  //already navigating there, just return TRUE
             if (pathString.StartsWith(PathHead)) 
             {
+                SuspendLayout(); //pause layout until page loaded. //Reference: http://msdn.microsoft.com/en-us/library/system.windows.forms.control.suspendlayout.aspx
                 bool canNavigate = TryToGoBackForward(pathString);
                 if (canNavigate) { return true; }
                 navigatingTo = pathString;
@@ -81,6 +82,7 @@ namespace ZeroKLobby
             {
                 if (navigatedPlaces[i] == pathString) 
                 {
+                    SuspendLayout();
                     bool canNavigate = TryToGoBackForward(pathString);
                     if (canNavigate) {return true;}
                     navigatingTo = pathString;
@@ -112,7 +114,8 @@ namespace ZeroKLobby
             
             //This update URL textbox & add the page to NavigationBar's history (so that Back&Forward button can be used):
             navigatingTo = ((WebBrowser)sender).Url.ToString();
-            Program.MainWindow.navigationControl.Path = ((WebBrowser)sender).Url.ToString();
+            Program.MainWindow.navigationControl.AddToHistoryStack(navigatingTo);
+            ResumeLayout();
             //NOTE on operation: 
             //setting the path to this URL will cause "GoToPage(path)" to be called, 
             //"GoToPage(path)" then will loop over all tab control (including this instance of "BrowserTab"), which will then call "TryToNavigate(path)".

ZeroKLobby/MicroLobby/ChatTab.cs
@@ -25,6 +25,7 @@ namespace ZeroKLobby.MicroLobby
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Reliability", "CA2000:Dispose objects before losing scope")]
         public ChatTab()
         {
+            SuspendLayout();
             InitializeComponent();
             if (Process.GetCurrentProcess().ProcessName == "devenv") return; // detect design mode, workaround for non-working this.DesignMode 
             Controls.Add(toolTabs);
@@ -47,6 +48,7 @@ namespace ZeroKLobby.MicroLobby
             foreach (var channel in Program.TasClient.JoinedChannels.Values.Where(c => !IsIgnoredChannel(c.Name))) CreateChannelControl(channel.Name);
             toolTabs.SelectTab("Battle");
             toolTabs.ForeColor = Program.Conf.OtherTextColor;
+            ResumeLayout();
         }
 
         public void CloseTab(string key)

ZeroKLobby/MicroLobby/DownloaderTab.cs
@@ -48,10 +48,12 @@ namespace ZeroKLobby.MapDownloader
 
     void UpdateAvailablePackages()
     {
+        SuspendLayout(); //pause
       tvAvailable.BeginUpdate();
       tvAvailable.Nodes.Clear();
       foreach (var repo in Program.Downloader.PackageDownloader.Repositories) foreach (var key in repo.VersionsByTag.Keys) AddNode(key);
       tvAvailable.EndUpdate();
+        ResumeLayout();
     }
 
     void UpdateSelectedPackages()

ZeroKLobby/MicroLobby/SpringsettingForm.cs
@@ -63,6 +63,7 @@ namespace ZeroKLobby.MicroLobby
 
         private void SpringsettingForm_Load(object sender, EventArgs e)
         {
+            SuspendLayout(); //pause layout until all element is set
             try
             {
                 Program.ToolTip.SetText(highlighttextBox, "Highlight any options that contain this term as name");
@@ -154,6 +155,7 @@ namespace ZeroKLobby.MicroLobby
             catch (Exception ex) {
                 ErrorHandling.HandleException(ex, false);
             }
+            ResumeLayout();
         }
 
         private void button1_Click(object sender, EventArgs e)

ZeroKLobby/NavigationControl.cs
@@ -68,6 +68,7 @@ namespace ZeroKLobby
         }
 
         public NavigationControl() {
+            SuspendLayout();//(Increase performance), Reference: http://msdn.microsoft.com/en-us/library/system.windows.forms.control.suspendlayout.aspx
             InitializeComponent();
 
             if (Environment.OSVersion.Platform != PlatformID.Unix && !Program.Conf.UseExternalBrowser)
@@ -168,6 +169,7 @@ namespace ZeroKLobby
                     if (button.Text == "HOME") { button.Margin = new Padding(15, 0, 0, 3); }
                 }
             }
+            ResumeLayout();
         }
 
         public INavigatable GetInavigatableByPath(string path) {
@@ -311,8 +313,10 @@ namespace ZeroKLobby
             //resize the "browser" (that show chat & internal browser) according to Nav bar auto resize (dynamic resizing)
             int windowHeight = this.Size.Height;
             int freeHeight = windowHeight - height;
+            int windowWidth = this.Size.Width;
             tabControl.Location = new System.Drawing.Point(tabControl.Location.X, height);
             tabControl.Height = freeHeight;
+            tabControl.Width = windowWidth;
         }
 
         public INavigatable CurrentNavigatable { get { return tabControl.SelectedTab.Controls.OfType<INavigatable>().FirstOrDefault(); } }
@@ -326,6 +330,13 @@ namespace ZeroKLobby
         private void goButton1_Click(object sender, EventArgs e)
         {
             Path = urlBox.Text;
+        }
+
+        //add path to history stack skipping all checks (attempt to check pathstring for valid TAB will make the ZKL jump/hop to the TAB)
+        public void AddToHistoryStack(String pathString)
+        {
+            if (CurrentPage != null && CurrentPage.ToString() != pathString) backStack.Push(CurrentPage);
+            CurrentPage = new NavigationStep { Path = pathString.Split('/') };
         }
 
     }

ZeroKLobby/Notifications/NotifyBarContainer.cs
@@ -10,6 +10,7 @@ namespace ZeroKLobby.Notifications
 
         public NotifyBarContainer(INotifyBar barContent)
         {
+            SuspendLayout(); //suspend layout until all bar element is finish set up
             BarContent = barContent;
             InitializeComponent();
             btnDetail.Click += detail_Click;
@@ -23,6 +24,7 @@ namespace ZeroKLobby.Notifications
             control.Dock = DockStyle.Fill;
             Dock = DockStyle.Top;
             BarContent.AddedToContainer(this);
+            ResumeLayout();
         }
 
 