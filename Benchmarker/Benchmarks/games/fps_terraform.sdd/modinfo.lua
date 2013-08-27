cd7c70810310baea89c30b4059b1c411a1333a6c
GoogleFrog <googl3frog@gmail.com>
2013-08-27 08:39:14 +0000

    Remove echo from benchmark.
    svn path=/trunk/other/; revision=11246

--- Benchmarker/Benchmarks/games/fps_terraform.sdd/modinfo.lua
+++ Benchmarker/Benchmarks/games/fps_terraform.sdd/modinfo.lua
@@ -2,6 +2,6 @@ local modinfo = {
   name          =	[[FPS Terraform]],
   description   =	[[Mission Mutator]],
   modtype       =	[[1]],
-  depend = {'Zero-K test-11244'},
+  depend = {'Zero-K v1.1.3.6', },
 }
 return modinfo
