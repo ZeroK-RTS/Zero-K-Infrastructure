6d7ca536aee774f5751ee244994b17f1711a0d51
Licho <jalich@gmail.com>
2013-09-05 14:48:52 +0000

    zkl support for nightwatch originating channel polls
    svn path=/trunk/other/; revision=11362

--- ZeroKLobby/Notifications/VoteBar.cs
+++ ZeroKLobby/Notifications/VoteBar.cs
@@ -1,78 +1,65 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Drawing;
-using System.Data;
 using System.Linq;
-using System.Text;
 using System.Text.RegularExpressions;
 using System.Windows.Forms;
 using LobbyClient;
+using ZkData;
 
 namespace ZeroKLobby.Notifications
 {
-    public partial class VoteBar : UserControl, INotifyBar
+    public partial class VoteBar: UserControl, INotifyBar
     {
         NotifyBarContainer container;
-        private TasClient tas;
+        readonly TasClient tas;
 
-        public VoteBar()
-        {
+        public VoteBar() {
             InitializeComponent();
             tas = Program.TasClient;
             tas.PreviewSaid += (sender, e) =>
                 {
-                var args = e.Data;
-                if (tas.MyBattle != null && args.Place == TasSayEventArgs.Places.Battle && args.UserName == tas.MyBattle.Founder.Name && args.Text.StartsWith("Poll:")) {
-                    var lid = args.Text.LastIndexOf("[");
-                    
-                    if (lid != -1) {
-                        var question = args.Text.Substring(6, lid - 6);
-                        var data = args.Text.Substring(lid + 1);
-                        if (data.Contains("END:")) {
-                            Program.NotifySection.RemoveBar(this);
-                        } else
-                        {
-                            if (!Program.NotifySection.Bars.Contains(this))
-                            {
-                                Program.NotifySection.AddBar(this);
-                                if (!tas.MyUser.IsInGame && !tas.MyBattleStatus.IsSpectator) Program.MainWindow.NotifyUser("chat/battle", string.Format("Poll: {0}", question), true, true);
-                            } else {
-                                e.Cancel = true; // vote bar already visible, dont spam vote text again
-                            }
+                    var args = e.Data;
+                    if (((tas.MyBattle != null && args.Place == TasSayEventArgs.Places.Battle && args.UserName == tas.MyBattle.Founder.Name) ||
+                         (args.Place == TasSayEventArgs.Places.Channel && args.UserName == GlobalConst.NightwatchName)) &&
+                        args.Text.StartsWith("Poll:")) {
+                        var lid = args.Text.LastIndexOf("[");
 
-                            lbQuestion.Text = question; //add vote text into linklabel
-                            lbQuestion.Links.Clear(); //remove all link (convert to normal text?)
-                            foreach (Match match in Regex.Matches(question, @"((mailto|spring|http|https|ftp|ftps)\://(\S+))")) //find map link
-                            {
-                                lbQuestion.Links.Add(match.Groups[1].Index, match.Groups[1].Length); //activate link for map link
-                            }
-                            
-                            var m2 = Regex.Match(data, "!y=([0-9]+)/([0-9]+), !n=([0-9]+)/([0-9]+)");
-                            if (m2.Success) {
-                                var yes = int.Parse(m2.Groups[1].Value);
-                                var yesMax = int.Parse(m2.Groups[2].Value);
-                                var no = int.Parse(m2.Groups[3].Value);
-                                var noMax = int.Parse(m2.Groups[4].Value);
-                                lbYes.Text = string.Format("{0}/{1}",  yes, yesMax);
-                                lbNo.Text = string.Format("{0}/{1}", no, noMax);
-                                pbYes.Maximum = yesMax;
-                                pbYes.Value = yes;
-                                pbNo.Maximum = noMax;
-                                pbNo.Value = no;
+                        if (lid != -1) {
+                            var question = args.Text.Substring(6, lid - 6);
+                            var data = args.Text.Substring(lid + 1);
+                            if (data.Contains("END:")) Program.NotifySection.RemoveBar(this);
+                            else {
+                                if (!Program.NotifySection.Bars.Contains(this)) {
+                                    Program.NotifySection.AddBar(this);
+                                    if (tas.MyBattleStatus == null || (!tas.MyUser.IsInGame && !tas.MyBattleStatus.IsSpectator)) Program.MainWindow.NotifyUser("chat/battle", string.Format("Poll: {0}", question), true, true);
+                                }
+                                else e.Cancel = true; // vote bar already visible, dont spam vote text again
+
+                                lbQuestion.Text = question; //add vote text into linklabel
+                                lbQuestion.Links.Clear(); //remove all link (convert to normal text?)
+                                foreach (Match match in Regex.Matches(question, @"((mailto|spring|http|https|ftp|ftps)\://(\S+))")) //find map link
+                                    lbQuestion.Links.Add(match.Groups[1].Index, match.Groups[1].Length); //activate link for map link
+
+                                var m2 = Regex.Match(data, "!y=([0-9]+)/([0-9]+), !n=([0-9]+)/([0-9]+)");
+                                if (m2.Success) {
+                                    var yes = int.Parse(m2.Groups[1].Value);
+                                    var yesMax = int.Parse(m2.Groups[2].Value);
+                                    var no = int.Parse(m2.Groups[3].Value);
+                                    var noMax = int.Parse(m2.Groups[4].Value);
+                                    lbYes.Text = string.Format("{0}/{1}", yes, yesMax);
+                                    lbNo.Text = string.Format("{0}/{1}", no, noMax);
+                                    pbYes.Maximum = yesMax;
+                                    pbYes.Value = yes;
+                                    pbNo.Maximum = noMax;
+                                    pbNo.Value = no;
+                                }
                             }
                         }
                     }
-                }
-            };
-            tas.BattleClosed += (sender, args) =>
-            {
-                Program.NotifySection.RemoveBar(this);
-            };
+                };
+            tas.BattleClosed += (sender, args) => { Program.NotifySection.RemoveBar(this); };
         }
 
         public void AddedToContainer(NotifyBarContainer container) {
-            
             this.container = container;
             container.btnDetail.Enabled = false;
             container.btnDetail.Text = "Vote";
@@ -82,27 +69,21 @@ namespace ZeroKLobby.Notifications
             Program.NotifySection.RemoveBar(this);
         }
 
-        public void DetailClicked(NotifyBarContainer container) {
-            
-            
-        }
+        public void DetailClicked(NotifyBarContainer container) {}
 
         public Control GetControl() {
             return this;
         }
 
-        private void btnYes_Click(object sender, EventArgs e)
-        {
-            tas.Say(TasClient.SayPlace.Battle, "","!y", false);
-        }
-
-        private void btnNo_Click(object sender, EventArgs e)
-        {
+        void btnNo_Click(object sender, EventArgs e) {
             tas.Say(TasClient.SayPlace.Battle, "", "!n", false);
         }
 
-        private void lbQuestion_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
-        {
+        void btnYes_Click(object sender, EventArgs e) {
+            tas.Say(TasClient.SayPlace.Battle, "", "!y", false);
+        }
+
+        void lbQuestion_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e) {
             Utils.OpenWeb(lbQuestion.Text.Substring(e.Link.Start, e.Link.Length), true); //open map's webpage
         }
     }
