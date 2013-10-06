2d4e6e2afc8dfa0b490ce1f78c61717277e3287d
KingRaptor <kingraptorowns@gmail.com>
2013-10-06 03:25:08 +0000

    PlayerJoinHandler:  * null check for account  * replace clanless tip with factionless tip
    svn path=/trunk/other/; revision=11648

--- Zero-K.info/SpringieInterface/PlayerJoinHandler.cs
+++ Zero-K.info/SpringieInterface/PlayerJoinHandler.cs
@@ -29,40 +29,45 @@ namespace ZeroKWeb.SpringieInterface
                 }
                 Account account = Account.AccountByLobbyID(db, accountID); // accountID is in fact lobbyID
 
-                if (account.Level < context.GetConfig().MinLevel) {
-                    AuthServiceClient.SendLobbyMessage(account,
-                                                       string.Format(
-                                                           "Sorry, PlanetWars is competive online campaign for experienced players. You need to be at least level {0} to play here. To increase your level, play more games on other hosts or open multiplayer game and play against computer AI bots.  You can observe this game however.",
-                                                           context.GetConfig().MinLevel));
-                }
+                if (account != null)
+                {
+                    if (account.Level < context.GetConfig().MinLevel)
+                    {
+                        AuthServiceClient.SendLobbyMessage(account,
+                                                           string.Format(
+                                                               "Sorry, PlanetWars is competive online campaign for experienced players. You need to be at least level {0} to play here. To increase your level, play more games on other hosts or open multiplayer game and play against computer AI bots.  You can observe this game however.",
+                                                               context.GetConfig().MinLevel));
+                    }
 
-                if (account.Clan == null) {
-                    res.PrivateMessage =
-                        string.Format(
-                            "{0} this is competetive PlanetWars campaign server. Join a clan to conquer the galaxy http://zero-k.info/Clans",
-                            account.Name);
+                    if (account.Faction == null)
+                    {
+                        res.PrivateMessage =
+                            string.Format(
+                                "{0} this is competetive PlanetWars campaign server. Join a clan to conquer the galaxy http://zero-k.info/Factions",
+                                account.Name);
+                        return res;
+                    }
+
+                    string owner = "";
+                    if (planet.Account != null) owner = planet.Account.Name;
+                    string facRoles = string.Join(",",
+                                                  account.AccountRolesByAccountID.Where(x => !x.RoleType.IsClanOnly).Select(x => x.RoleType.Name).ToList());
+                    if (!string.IsNullOrEmpty(facRoles)) facRoles += " of " + account.Faction.Name + ", ";
+
+                    string clanRoles = string.Join(",",
+                                                   account.AccountRolesByAccountID.Where(x => x.RoleType.IsClanOnly).Select(x => x.RoleType.Name).ToList());
+                    if (!string.IsNullOrEmpty(clanRoles)) clanRoles += " of " + account.Clan.ClanName;
+
+                    res.PublicMessage = string.Format("Greetings {0} {1}{2}, welcome to {3} planet {4} http://zero-k.info/PlanetWars/Planet/{5}",
+                                                      account.Name,
+                                                      facRoles,
+                                                      clanRoles,
+                                                      owner,
+                                                      planet.Name,
+                                                      planet.PlanetID);
+
                     return res;
                 }
-
-                string owner = "";
-                if (planet.Account != null) owner = planet.Account.Name;
-                string facRoles = string.Join(",",
-                                              account.AccountRolesByAccountID.Where(x => !x.RoleType.IsClanOnly).Select(x => x.RoleType.Name).ToList());
-                if (!string.IsNullOrEmpty(facRoles)) facRoles += " of " + account.Faction.Name + ", ";
-
-                string clanRoles = string.Join(",",
-                                               account.AccountRolesByAccountID.Where(x => x.RoleType.IsClanOnly).Select(x => x.RoleType.Name).ToList());
-                if (!string.IsNullOrEmpty(clanRoles)) clanRoles += " of " + account.Clan.ClanName;
-
-                res.PublicMessage = string.Format("Greetings {0} {1}{2}, welcome to {3} planet {4} http://zero-k.info/PlanetWars/Planet/{5}",
-                                                  account.Name,
-                                                  facRoles,
-                                                  clanRoles,
-                                                  owner,
-                                                  planet.Name,
-                                                  planet.PlanetID);
-
-                return res;
             }
             return null;
         }
