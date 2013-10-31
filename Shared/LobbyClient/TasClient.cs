c7ec73892d95c0c4e4155d957df6f18af7da1298
KingRaptor <kingraptorowns@gmail.com>
2013-10-31 08:16:59 +0000

    DEPLOY_SITE again
    svn path=/trunk/other/; revision=11862

--- Shared/LobbyClient/TasClient.cs
+++ Shared/LobbyClient/TasClient.cs
@@ -648,9 +648,9 @@ namespace LobbyClient
                     Say(SayPlace.User, user.Name, "!join " + battle.Founder.Name, false);
                 }
                 else {*/
-                    //con.SendCommand(string.Format("KICKFROMBATTLE {0}", name));
-                    con.SendCommand("FORCEJOINBATTLE", name, battleID, password);
-                    con.SendCommand(string.Format("FORGEREVERSEMSG {0} LEAVEBATTLE", name));
+                    con.SendCommand(string.Format("KICKFROMBATTLE {0}", name));
+                    con.SendCommand(string.Format("FORCEJOINBATTLE", name, battleID, password));
+                    //con.SendCommand(string.Format("FORGEREVERSEMSG {0} LEAVEBATTLE", name));
                     //con.SendCommand(string.Format("FORGEREVERSEMSG {0} JOINBATTLE {1}", name, battleID));
                 //}
             }
