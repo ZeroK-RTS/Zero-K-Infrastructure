58ed6668192c59d0485820e4d278ea3394c3cd19
KingRaptor <kingraptorowns@gmail.com>
2013-11-22 07:37:06 +0000

    Fix issue 2025
    svn path=/trunk/other/; revision=12136

--- Springie/Springie/autohost/AutoHost.cs
+++ Springie/Springie/autohost/AutoHost.cs
@@ -984,7 +984,8 @@ namespace Springie.autohost
         }
 
         void tas_LoginDenied(object sender, TasEventArgs e) {
-            if (e.ServerParams[0] == "Bad username/password") tas.Register(GetAccountName(), config.Password);
+            //System.Console.WriteLine("Failed login for {0}: {1}", GetAccountName(), e.ServerParams[0]);
+            if (e.ServerParams[0] == "Invalid username or password") tas.Register(GetAccountName(), config.Password);
             else {
                 CloneNumber++;
                 tas.Login(GetAccountName(), config.Password);
