66347624f935f1c8412c72ebc712ae155b00d9a3
KingRaptor <kingraptorowns@gmail.com>
2014-01-03 15:14:12 +0000

    DEPLOY_SITE moar block
    svn path=/trunk/other/; revision=12360

--- NightWatch/AuthService.cs
+++ NightWatch/AuthService.cs
@@ -24,7 +24,8 @@ namespace NightWatch
         int messageId;
         readonly ConcurrentDictionary<int, RequestInfo> requests = new ConcurrentDictionary<int, RequestInfo>();
         readonly TopPlayers topPlayers = new TopPlayers();
-        public static string[] blockedCompanies = new string[] { "PRIVAX-LTD", "NetcoSolution-BLK-IP", "ServeTheWorld", "AnchorFree", "AltusHost", "Altushost" };
+        // this stuff should really be in DB
+        public static string[] blockedCompanies = new string[] { "PRIVAX-LTD", "NetcoSolution-BLK-IP", "ServeTheWorld", "AnchorFree", "AltusHost", "Altushost", "IWeb", "iWeb" };
         public static string[] blockedHosts = new string[] { "anchorfree.com", "leaseweb.com", "uk2net.com", "privax.com", "hidemyass.com", "hotspotshield.com", "ipvanish.com",
             "alvotech.net", "unknown.puregig.net", "edis.at", "fastweb.ro", "ubiquityservers.com"};
 
