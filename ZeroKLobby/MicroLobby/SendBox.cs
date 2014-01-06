dcf60d0446a46ee7ad6be0fee97ff968f70512e5
xponen <msafwan86msafwan86_nnss@hotmail.com>
2014-01-06 13:43:35 +0000

    ZKL SendBox.cs : whitespace fix by visual studio
    svn path=/trunk/other/; revision=12391

--- ZeroKLobby/MicroLobby/SendBox.cs
+++ ZeroKLobby/MicroLobby/SendBox.cs
@@ -7,56 +7,56 @@ using PlasmaShared;
 
 namespace ZeroKLobby.MicroLobby
 {
-	public class SendBox: TextBox
-	{
-		readonly List<string> history = new List<string>();
-		int historyIndex = 0;
-		string ncFirstChunk;
-		IEnumerator ncMatchesEn;
-		string ncSecondChunk;
-		string ncWordToComplete = "";
-		bool nickCompleteMode;
-		public event Func<string, IEnumerable<string>> CompleteWord;
-		public event EventHandler<EventArgs<string>> LineEntered = delegate { };
+    public class SendBox : TextBox
+    {
+        readonly List<string> history = new List<string>();
+        int historyIndex = 0;
+        string ncFirstChunk;
+        IEnumerator ncMatchesEn;
+        string ncSecondChunk;
+        string ncWordToComplete = "";
+        bool nickCompleteMode;
+        public event Func<string, IEnumerable<string>> CompleteWord;
+        public event EventHandler<EventArgs<string>> LineEntered = delegate { };
 
-		public SendBox()
-		{
-			Multiline = true;
+        public SendBox()
+        {
+            Multiline = true;
             this.Font = Program.Conf.ChatFont;
-		}
+        }
 
-		protected override void OnKeyPress(KeyPressEventArgs e)
-		{
-			if (e.KeyChar == '\t')
-			{
-				CompleteNick();
-				e.Handled = true;
-			}
-			base.OnKeyPress(e);
-		}
+        protected override void OnKeyPress(KeyPressEventArgs e)
+        {
+            if (e.KeyChar == '\t')
+            {
+                CompleteNick();
+                e.Handled = true;
+            }
+            base.OnKeyPress(e);
+        }
 
-		protected override void OnKeyUp(KeyEventArgs e)
-		{
-			if (Lines.Length > 1)
-			{
-				var line = Text.Replace("\t", "  ").TrimEnd(new[] { '\r', '\n' });
+        protected override void OnKeyUp(KeyEventArgs e)
+        {
+            if (Lines.Length > 1)
+            {
+                var line = Text.Replace("\t", "  ").TrimEnd(new[] { '\r', '\n' });
 
-				if (!string.IsNullOrEmpty(line))
-				{
-					history.Add(line);
-					historyIndex = history.Count;
-				}
+                if (!string.IsNullOrEmpty(line))
+                {
+                    history.Add(line);
+                    historyIndex = history.Count;
+                }
 
-				Text = String.Empty;
-				LineEntered(this, new EventArgs<string>(line));
-			}
-			base.OnKeyUp(e);
-		}
+                Text = String.Empty;
+                LineEntered(this, new EventArgs<string>(line));
+            }
+            base.OnKeyUp(e);
+        }
 
-		protected override void OnPreviewKeyDown(PreviewKeyDownEventArgs e)
-		{
-			if (e.KeyCode == Keys.Up)
-			{
+        protected override void OnPreviewKeyDown(PreviewKeyDownEventArgs e)
+        {
+            if (e.KeyCode == Keys.Up)
+            {
                 if (e.Control)
                 {
                     ActionHandler.PrevButton();
@@ -72,9 +72,9 @@ namespace ZeroKLobby.MicroLobby
                         SelectionStart = Text.Length;
                     }
                 }
-			}
-			else if (e.KeyCode == Keys.Down)
-			{
+            }
+            else if (e.KeyCode == Keys.Down)
+            {
                 if (e.Control)
                 {
                     ActionHandler.NextButton();
@@ -91,64 +91,64 @@ namespace ZeroKLobby.MicroLobby
                     }
                     SelectionStart = Text.Length;
                 }
-			}
-			else historyIndex = history.Count;
+            }
+            else historyIndex = history.Count;
 
-			//Prevent cutting line in half when sending
-			if (e.KeyCode == Keys.Return) SelectionStart = Text.Length;
-			if (e.KeyCode != Keys.Tab) nickCompleteMode = false;
+            //Prevent cutting line in half when sending
+            if (e.KeyCode == Keys.Return) SelectionStart = Text.Length;
+            if (e.KeyCode != Keys.Tab) nickCompleteMode = false;
 
-			base.OnPreviewKeyDown(e);
-		}
+            base.OnPreviewKeyDown(e);
+        }
 
 
-		void CompleteNick()
-		{
-			if (CompleteWord == null) return;
+        void CompleteNick()
+        {
+            if (CompleteWord == null) return;
 
-			var ss = SelectionStart;
+            var ss = SelectionStart;
 
-			//don't bother nick complete if caret is at start of box or after a space
-			if (ss == 0) return;
-			var test = Text.Substring(ss - 1, 1);
-			if (test == " ") return;
+            //don't bother nick complete if caret is at start of box or after a space
+            if (ss == 0) return;
+            var test = Text.Substring(ss - 1, 1);
+            if (test == " ") return;
 
-			if (!nickCompleteMode)
-			{
-				//split chatbox text chunks
-				var ncFirstChunkTemp = Text.Substring(0, ss).Split(' ');
-				ncFirstChunk = "";
-				for (var i = 0; i < ncFirstChunkTemp.Length - 1; i++) ncFirstChunk += ncFirstChunkTemp[i] + " ";
-				ncSecondChunk = Text.Substring(ss);
+            if (!nickCompleteMode)
+            {
+                //split chatbox text chunks
+                var ncFirstChunkTemp = Text.Substring(0, ss).Split(' ');
+                ncFirstChunk = "";
+                for (var i = 0; i < ncFirstChunkTemp.Length - 1; i++) ncFirstChunk += ncFirstChunkTemp[i] + " ";
+                ncSecondChunk = Text.Substring(ss);
 
-				//word entered by user
-				ncWordToComplete = ncFirstChunkTemp[ncFirstChunkTemp.Length - 1];
+                //word entered by user
+                ncWordToComplete = ncFirstChunkTemp[ncFirstChunkTemp.Length - 1];
 
-				//match up entered word and nick list, store in enumerator
+                //match up entered word and nick list, store in enumerator
 
-				var ncMatches = CompleteWord(ncWordToComplete).ToList();
+                var ncMatches = CompleteWord(ncWordToComplete).ToList();
 
-				if (ncMatches.Any())
-				{
-					ncMatchesEn = ncMatches.GetEnumerator();
-					nickCompleteMode = true;
-				}
-			}
+                if (ncMatches.Any())
+                {
+                    ncMatchesEn = ncMatches.GetEnumerator();
+                    nickCompleteMode = true;
+                }
+            }
 
-			if (nickCompleteMode)
-			{
-				//get next matched nickname
-				if (!ncMatchesEn.MoveNext())
-				{
-					ncMatchesEn.Reset();
-					ncMatchesEn.MoveNext();
-				}
-				var nick = ncMatchesEn.Current.ToString();
+            if (nickCompleteMode)
+            {
+                //get next matched nickname
+                if (!ncMatchesEn.MoveNext())
+                {
+                    ncMatchesEn.Reset();
+                    ncMatchesEn.MoveNext();
+                }
+                var nick = ncMatchesEn.Current.ToString();
 
-				//remake chatbox text
-				Text = ncFirstChunk + nick + ncSecondChunk;
-				SelectionStart = ncFirstChunk.Length + nick.Length;
-			}
-		}
-	}
+                //remake chatbox text
+                Text = ncFirstChunk + nick + ncSecondChunk;
+                SelectionStart = ncFirstChunk.Length + nick.Length;
+            }
+        }
+    }
 }
