38a04a9e933c0d09c52e41fc85450d35e9a70e2d
GoogleFrog <googl3frog@gmail.com>
2014-01-30 15:25:43 +0000

    Updated FPS War2 benchmark. Use it on DSD!
    svn path=/trunk/other/; revision=12595

--- Benchmarker/Benchmarks/games/fps_war2.sdd/modinfo.lua
+++ Benchmarker/Benchmarks/games/fps_war2.sdd/modinfo.lua
@@ -2,6 +2,6 @@ local modinfo = {
   name          =	[[fps_war2]],
   description   =	[[War Mission Mutator]],
   modtype       =	[[1]],
-  depend = {'Zero-K v1.1.3.6'},
+  depend = {'Zero-K v1.2.1.7'},
 }
 return modinfo
