03a0a5d60e653e1c06fb6d116c24eab519623425
xponen <msafwan86_nnss@hotmail.com>
2014-06-13 18:50:43 +0800

    AutoRegistrator/Program.cs: set to download Spring 97.0.1-18
    so it use updated unitsync and not Spring 80s version.

--- AutoRegistrator/Program.cs
+++ AutoRegistrator/Program.cs
@@ -48,7 +48,7 @@ namespace AutoRegistrator
             Scanner.Start();
             Downloader = new PlasmaDownloader.PlasmaDownloader(new Config(), Scanner, Paths);
             Downloader.DownloadAdded += (s, e) => Trace.TraceInformation("Download started: {0}", e.Data.Name);
-            Downloader.GetAndSwitchEngine("85.0");
+            Downloader.GetAndSwitchEngine("97.0.1-18-gf1dd749"); //for ZKL equivalent, see PlasmaShared/GlobalConst.cs
             Downloader.PackagesChanged += Downloader_PackagesChanged;
 
             while (true) {Thread.Sleep(10000);}
