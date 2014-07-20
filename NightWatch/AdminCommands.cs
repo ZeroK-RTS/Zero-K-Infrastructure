55a3077c6a1f5d9aeb5bfe4c358b5fdb6b4afca3
L.J. Lim <kingraptorowns@gmail.com>
2014-07-20 13:30:38 +0800

    NightWatch/AdminCommands.cs: allow proxy ChanServ commands with !chanserv; small refactor DEPLOY_SITE

--- NightWatch/AdminCommands.cs
+++ NightWatch/AdminCommands.cs
@@ -8,6 +8,8 @@ namespace CaTracker
     {
         readonly TasClient tas;
 
+        public static string[] adminCommands = { "kick", "op", "changeaccountpass", "chanserv" };
+
         public AdminCommands(TasClient tas)
         {
             this.tas = tas;
@@ -20,64 +22,65 @@ namespace CaTracker
 
             if (e.Place == TasSayEventArgs.Places.Normal)
             {
+                foreach (string command in adminCommands)
+                {
+                    if (e.Text.StartsWith("!" + command))
+                    {
+                        var db = new ZkDataContext();
+                        var acc = Account.AccountByLobbyID(db, tas.ExistingUsers[e.UserName].LobbyID);
+                        if (!(acc.IsZeroKAdmin || acc.IsLobbyAdministrator)) return;
+                        break;
+                    }
+                }
+
                 if (e.Text.StartsWith("!kick"))
                 {
-                    var db = new ZkDataContext();
-                    var acc = Account.AccountByLobbyID(db, tas.ExistingUsers[e.UserName].LobbyID);
-                    if (acc.IsZeroKAdmin || acc.IsLobbyAdministrator)
+                    var parts = e.Text.Split(' ');
+                    if (!(parts.Length >= 2)) tas.Say(TasClient.SayPlace.User, e.UserName, "!kick [player] [reason]", false);
+                    else
                     {
-                        var parts = e.Text.Split(' ');
-                        if (!(parts.Length >= 2)) tas.Say(TasClient.SayPlace.User, e.UserName, "!kick [player] [reason]", false);
-                        else
+                        var player = tas.ExistingUsers.FirstOrDefault(x => x.Key == parts[1]).Key;
+                        var reasonParts = ((string[])parts.Clone()).ToList();
+                        reasonParts.RemoveRange(0, 2);
+                        string reason = string.Join(" ", reasonParts) ?? "";
+                        if (player != null)
                         {
-                            var player = tas.ExistingUsers.FirstOrDefault(x => x.Key == parts[1]).Key;
-                            var reasonParts = ((string[])parts.Clone()).ToList();
-                            reasonParts.RemoveRange(0, 2);
-                            string reason = string.Join(" ", reasonParts) ?? "";
-                            if (player != null)
-                            {
-                                tas.AdminKickFromLobby(player, reason);
-                            }
-                            else tas.Say(TasClient.SayPlace.User, e.UserName, "Not a valid player name", false);
+                            tas.AdminKickFromLobby(player, reason);
                         }
+                        else tas.Say(TasClient.SayPlace.User, e.UserName, "Not a valid player name", false);
                     }
                 }
                 else if (e.Text.StartsWith("!op"))
                 {
-                    var db = new ZkDataContext();
-                    var acc = Account.AccountByLobbyID(db, tas.ExistingUsers[e.UserName].LobbyID);
-                    if (acc.IsZeroKAdmin || acc.IsLobbyAdministrator)
+                    var parts = e.Text.Split(' ');
+                    if (parts.Length != 3) tas.Say(TasClient.SayPlace.User, e.UserName, "!op [player] [channel]", false);
+                    else
                     {
-                        var parts = e.Text.Split(' ');
-                        if (parts.Length != 3) tas.Say(TasClient.SayPlace.User, e.UserName, "!op [player] [channel]", false);
-                        else
+                        var player = tas.ExistingUsers.FirstOrDefault(x => x.Key == parts[1]).Key;
+                        var channel = parts[2];
+                        if (player != null)
                         {
-                            var player = tas.ExistingUsers.FirstOrDefault(x => x.Key == parts[1]).Key;
-                            var channel = parts[2];
-                            if (player != null)
-                            {
-                                tas.Say(TasClient.SayPlace.User, "ChanServ", string.Format("!op {0} {1}", channel, player), false);
-                                tas.Say(TasClient.SayPlace.User, e.UserName, string.Format("DEBUG: !op {0} {1}", channel, player), false);
-                            }
-                            else tas.Say(TasClient.SayPlace.User, e.UserName, "Not a valid player name", false);
+                            tas.Say(TasClient.SayPlace.User, "ChanServ", string.Format("!op {0} {1}", channel, player), false);
+                            tas.Say(TasClient.SayPlace.User, e.UserName, string.Format("DEBUG: !op {0} {1}", channel, player), false);
                         }
+                        else tas.Say(TasClient.SayPlace.User, e.UserName, "Not a valid player name", false);
                     }
                 }
                 else if (e.Text.StartsWith("!changeaccountpass"))
                 {
-                    var db = new ZkDataContext();
-                    var acc = Account.AccountByLobbyID(db, tas.ExistingUsers[e.UserName].LobbyID);
-                    if (acc.IsZeroKAdmin || acc.IsLobbyAdministrator)
+                    var parts = e.Text.Split(' ');
+                    if (parts.Length != 3) tas.Say(TasClient.SayPlace.User, e.UserName, "!changeaccountpass [player] [password (plaintext)]", false);
+                    else
                     {
-                        var parts = e.Text.Split(' ');
-                        if (parts.Length != 3) tas.Say(TasClient.SayPlace.User, e.UserName, "!changeaccountpass [player] [password (plaintext)]", false);
-                        else
-                        {
-                            var password = PlasmaShared.Utils.HashLobbyPassword(parts[2]);
-                            tas.SendRaw(string.Format("CHANGEACCOUNTPASS {0} {1}", parts[1], password));
-                            tas.Say(TasClient.SayPlace.User, e.UserName, string.Format("DEBUG: CHANGEACCOUNTPASS {0} {1}", parts[1], password), false);
-                        }
+                        var password = PlasmaShared.Utils.HashLobbyPassword(parts[2]);
+                        tas.SendRaw(string.Format("CHANGEACCOUNTPASS {0} {1}", parts[1], password));
+                        tas.Say(TasClient.SayPlace.User, e.UserName, string.Format("DEBUG: CHANGEACCOUNTPASS {0} {1}", parts[1], password), false);
                     }
+                }
+                else if (e.Text.StartsWith("!chanserv"))
+                {
+                    var command = e.Text.Substring(9).TrimStart();
+                    tas.Say(TasClient.SayPlace.User, "ChanServ", "!" + command, false);
                 }
             }
         }
