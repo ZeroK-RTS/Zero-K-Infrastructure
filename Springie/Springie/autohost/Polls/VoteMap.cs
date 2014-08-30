13708fec1aad0e569590371611cae0085f2d6a78
L.J. Lim <kingraptorowns@gmail.com>
2014-08-30 22:27:08 +0800

    fix #89

--- Springie/Springie/autohost/Polls/VoteMap.cs
+++ Springie/Springie/autohost/Polls/VoteMap.cs
@@ -40,13 +40,39 @@ namespace Springie.autohost.Polls
                 }
                 else
                 {
-                    //question = "Do you want to change to a suitable random map?";
-                    ah.FilterMaps(words, out vals, out indexes);
-                    var rand = new System.Random();
-                    map = vals[rand.Next(vals.Length)];
-                    var resource = ah.cache.FindResourceData(new string[] { map }, ResourceType.Map);
-                    question = string.Format("Change map to {0} http://zero-k.info/Maps/Detail/{1} ?", map, resource[0].ResourceID);
-                    return true;
+                    try
+                    {
+                        if (tas.MyBattle != null && !spring.IsRunning)
+                        {
+                            var serv = new PlasmaShared.SpringieInterfaceReference.SpringieService();
+                            serv.Timeout = 15000;
+                            serv.GetRecommendedMapCompleted += (sender, args) =>
+                            {
+                                if (!args.Cancelled && args.Error == null)
+                                {
+                                    var foundMap = args.Result;
+                                    if (foundMap != null && foundMap.MapName != null && tas.MyBattle != null)
+                                    {
+                                        if (tas.MyBattle.MapName != foundMap.MapName)
+                                        {
+                                            map = foundMap.MapName;
+                                        }
+                                    }
+
+                                }
+                            };
+                            serv.GetRecommendedMapAsync(tas.MyBattle.GetContext(), true);
+                            var resource = ah.cache.FindResourceData(new string[] { map }, ResourceType.Map);
+                            question = string.Format("Change map to {0} http://zero-k.info/Maps/Detail/{1} ?", map, resource[0].ResourceID);
+                            return true;
+                        }
+                    }
+                    catch (System.Exception ex)
+                    {
+                        AutoHost.Respond(tas, spring, e, ex.ToString());
+                        //System.Diagnostics.Trace.TraceError(ex.ToString());
+                    }
+                    return false;
                 }
             }
         }
