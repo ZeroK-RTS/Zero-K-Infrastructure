fc11e994cec3e5a6736755725b750046f8b42515
sprunk <gnurps@o2.pl>
2014-09-07 20:41:51 +0200

    Update ForumController.cs

--- Zero-K.info/Controllers/ForumController.cs
+++ Zero-K.info/Controllers/ForumController.cs
@@ -232,7 +232,7 @@ namespace ZeroKWeb.Controllers
 				{
                     if (forumPostID != null) {
                         var post = thread.ForumPosts.Single(x => x.ForumPostID == forumPostID);
-                        if (post.AuthorAccountID != Global.AccountID && !Global.Account.IsZeroKAdmin && !Global.IsLobbyAdministrator) throw new ApplicationException("Not authorized to edit the post");
+                        if (post.AuthorAccountID != Global.AccountID && !Global.Account.IsZeroKAdmin && !Global.Account.IsLobbyAdministrator) throw new ApplicationException("Not authorized to edit the post");
                         post.ForumPostEdits.Add(new ForumPostEdit() { 
                                 EditorAccountID = Global.AccountID,
                                 EditTime = DateTime.UtcNow,
