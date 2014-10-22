2dbacc607a6b64e709517b3f718d6bdedf8bb797
xponen <msafwan86_nnss@hotmail.com>
2014-10-22 19:09:32 +0800

    MinimapFuncBox.cs: Add Spring AI to regular Multiplayer AddAI button. Note: the AI is indeed unstable. Haven't able to spectate a reliable game. the Spring AI is not shown in main list, but at bottom of LUAUI AI, in subdirectory( hopefully doesn't look like something default)

--- ZeroKLobby/Controls/MinimapFuncBox.cs
+++ ZeroKLobby/Controls/MinimapFuncBox.cs
@@ -6,6 +6,8 @@ using System.Linq;
 using System.Windows.Forms;
 using ZeroKLobby;
 using ZeroKLobby.Lines;
+using PlasmaShared.UnitSyncLib;
+using ZeroKLobby.MicroLobby.ExtrasTab;
 
 namespace ZeroKLobby.Controls
 {
@@ -19,6 +21,21 @@ namespace ZeroKLobby.Controls
             Program.ToolTip.SetText(btnChangeTeam, "Create or move to new team");
             Program.ToolTip.SetText(btnAddAI, "Add AI to other team");
         }
+
+        private bool refreshSpringAi = true;
+        private List<Ai> springAi;
+        private void AddAIToTeam(string botShortName)
+        {
+            var botNumber = Enumerable.Range(1, int.MaxValue).First(i => !Program.TasClient.MyBattle.Bots.Any(bt => bt.Name == "Bot_" + i));
+            var botStatus = Program.TasClient.MyBattleStatus.Clone();
+            // new team        	
+            botStatus.TeamNumber = Enumerable.Range(0, TasClient.MaxTeams - 1).FirstOrDefault(
+                                    x => !Program.TasClient.MyBattle.Users.Any(y => y.TeamNumber == x));
+            //different alliance than player
+            botStatus.AllyNumber = Enumerable.Range(0, TasClient.MaxAlliances - 1).FirstOrDefault(x => x != botStatus.AllyNumber);
+            Program.TasClient.AddBot("Bot_" + botNumber, botStatus, (int)(ZeroKLobby.MicroLobby.MyCol)Color.White, botShortName);
+        } 
+
 
         private void addAIButton_Click(object sender, EventArgs e)
         {
@@ -35,23 +52,39 @@ namespace ZeroKLobby.Controls
                 {
                     var item = new System.Windows.Forms.MenuItem(string.Format("{0} ({1})", bot.ShortName, bot.Description));
                     var b = bot;
-                    item.Click += (s, e2) =>
-                    {
-                        var botNumber =
-                            Enumerable.Range(1, int.MaxValue).First(i => !Program.TasClient.MyBattle.Bots.Any(bt => bt.Name == "Bot_" + i));
-                        var botStatus = Program.TasClient.MyBattleStatus.Clone();
-                        // new team        	
-                        botStatus.TeamNumber =
-                            Enumerable.Range(0, TasClient.MaxTeams - 1).FirstOrDefault(
-                                x => !Program.TasClient.MyBattle.Users.Any(y => y.TeamNumber == x));
-                        //different alliance than player
-                        botStatus.AllyNumber = Enumerable.Range(0, TasClient.MaxAlliances - 1).FirstOrDefault(x => x != botStatus.AllyNumber);
-
-                        Program.TasClient.AddBot("Bot_" + botNumber, botStatus, (int)(ZeroKLobby.MicroLobby.MyCol)Color.White, b.ShortName);
-                    };
+                    item.Click += (s, e2) => { AddAIToTeam(bot.ShortName); };
                     menu.MenuItems.Add(item);
                 }
             }
+            menu.MenuItems.Add("-");
+
+            MenuItem item3 = new System.Windows.Forms.MenuItem("Spring AI");
+            item3.Select += (s, e2) =>
+                {
+                    if (item3.MenuItems.Count == 0)
+                    {
+                        if (refreshSpringAi)
+                            springAi = SkirmishControlTool.GetSpringAIs(Program.SpringPaths.UnitSyncDirectory); //note: used UnitSyncDirectory because its just same as Engine folder
+                        refreshSpringAi = false;
+                        MenuItem springAIitem;
+                        for (int i = 0; i < springAi.Count; i++)
+                        {
+                            string version = springAi[i].Version; //for keeping reference of independent value inside item.Click event;
+                            string shortname = springAi[i].ShortName;
+                            springAIitem = new System.Windows.Forms.MenuItem(string.Format("{0} ({1})", shortname, version));
+                            springAIitem.Click += (s1, e3) => { AddAIToTeam(shortname + "|" + version); };
+                            item3.MenuItems.Add(springAIitem);
+                        }
+                        item3.MenuItems.Add("-");
+                        springAIitem = new System.Windows.Forms.MenuItem("Refresh List");
+                        springAIitem.Click += (s1, e3) => {
+                            refreshSpringAi = true;
+                        };
+                        item3.MenuItems.Add(springAIitem);
+                    }
+                };
+            menu.MenuItems.Add(item3);
+
             menu.Show(btnAddAI, new Point(0, 0));
         }
 
