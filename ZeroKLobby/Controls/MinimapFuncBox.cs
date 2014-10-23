bc21b37820191c1db9caa3516fcec54fceefec2d
xponen <msafwan86_nnss@hotmail.com>
2014-10-24 02:08:52 +0800

    MinimapFuncBox & ContextMenus.cs: move duplicate "Add Spring AI" code from MinimapFuncBox to ContextMenus.cs to increase maintenance capability

--- ZeroKLobby/Controls/MinimapFuncBox.cs
+++ ZeroKLobby/Controls/MinimapFuncBox.cs
@@ -22,69 +22,18 @@ namespace ZeroKLobby.Controls
             Program.ToolTip.SetText(btnAddAI, "Add AI to other team");
         }
 
-        private bool refreshSpringAi = true;
-        private List<Ai> springAi;
-        private void AddAIToTeam(string botShortName)
-        {
-            var botNumber = Enumerable.Range(1, int.MaxValue).First(i => !Program.TasClient.MyBattle.Bots.Any(bt => bt.Name == "Bot_" + i));
-            var botStatus = Program.TasClient.MyBattleStatus.Clone();
-            // new team        	
-            botStatus.TeamNumber = Enumerable.Range(0, TasClient.MaxTeams - 1).FirstOrDefault(
-                                    x => !Program.TasClient.MyBattle.Users.Any(y => y.TeamNumber == x));
-            //different alliance than player
-            botStatus.AllyNumber = Enumerable.Range(0, TasClient.MaxAlliances - 1).FirstOrDefault(x => x != botStatus.AllyNumber);
-            Program.TasClient.AddBot("Bot_" + botNumber, botStatus, (int)(ZeroKLobby.MicroLobby.MyCol)Color.White, botShortName);
-        } 
-
-
         private void addAIButton_Click(object sender, EventArgs e)
         {
             var enabled = Program.TasClient.MyBattle != null && Program.ModStore.Ais != null && Program.ModStore.Ais.Any();
             ContextMenu menu = new ContextMenu();
+
             if (!enabled)
             {
                 // TODO
                 return;
             }
-            if (Program.ModStore.Ais != null)
-            {
-                foreach (var bot in Program.ModStore.Ais)
-                {
-                    var item = new System.Windows.Forms.MenuItem(string.Format("{0} ({1})", bot.ShortName, bot.Description));
-                    var b = bot;
-                    item.Click += (s, e2) => { AddAIToTeam(bot.ShortName); };
-                    menu.MenuItems.Add(item);
-                }
-            }
-            menu.MenuItems.Add("-");
 
-            MenuItem item3 = new System.Windows.Forms.MenuItem("Spring AI");
-            item3.Select += (s, e2) =>
-                {
-                    if (item3.MenuItems.Count == 0)
-                    {
-                        if (refreshSpringAi)
-                            springAi = SkirmishControlTool.GetSpringAIs(Program.SpringPaths.UnitSyncDirectory); //note: used UnitSyncDirectory because its just same as Engine folder
-                        refreshSpringAi = false;
-                        MenuItem springAIitem;
-                        for (int i = 0; i < springAi.Count; i++)
-                        {
-                            string version = springAi[i].Version; //for keeping reference of independent value inside item.Click event;
-                            string shortname = springAi[i].ShortName;
-                            springAIitem = new System.Windows.Forms.MenuItem(string.Format("{0} ({1})", shortname, version));
-                            springAIitem.Click += (s1, e3) => { AddAIToTeam(shortname + "|" + version); };
-                            item3.MenuItems.Add(springAIitem);
-                        }
-                        item3.MenuItems.Add("-");
-                        springAIitem = new System.Windows.Forms.MenuItem("Refresh List");
-                        springAIitem.Click += (s1, e3) => {
-                            refreshSpringAi = true;
-                        };
-                        item3.MenuItems.Add(springAIitem);
-                    }
-                };
-            menu.MenuItems.Add(item3);
-
+            menu.MenuItems.AddRange(ZeroKLobby.MicroLobby.ContextMenus.GetAddBotItems());
             menu.Show(btnAddAI, new Point(0, 0));
         }
 
@@ -94,30 +43,7 @@ namespace ZeroKLobby.Controls
 
             if (Program.TasClient.MyBattle != null)
             {
-                int freeAllyTeam;
-                bool iAmSpectator = Program.TasClient.MyBattleStatus.IsSpectator;
-                foreach (var allyTeam in ZeroKLobby.MicroLobby.ContextMenus.GetExistingTeams(out freeAllyTeam).Distinct())
-                {
-                    if (iAmSpectator || allyTeam != Program.TasClient.MyBattleStatus.AllyNumber)
-                    {
-                        var item = new System.Windows.Forms.MenuItem("Join Team " + (allyTeam + 1));
-                        item.Click += (s, e2) => ActionHandler.JoinAllyTeam(allyTeam);
-                        menu.MenuItems.Add(item);
-                    }
-                }
-
-                menu.MenuItems.Add("-");
-
-                var newTeamItem = new System.Windows.Forms.MenuItem("Start New Team");
-                newTeamItem.Click += (s, e2) => ActionHandler.JoinAllyTeam(freeAllyTeam);
-                menu.MenuItems.Add(newTeamItem);
-
-                if (!Program.TasClient.MyBattleStatus.IsSpectator)
-                {
-                    var specItem = new System.Windows.Forms.MenuItem("Spectate");
-                    specItem.Click += (s, e2) => ActionHandler.Spectate();
-                    menu.MenuItems.Add(specItem);
-                }
+                menu.MenuItems.AddRange(ZeroKLobby.MicroLobby.ContextMenus.GetSetAllyItems());
             }
 
             menu.Show(btnChangeTeam, new Point(0, 0));
@@ -135,16 +61,8 @@ namespace ZeroKLobby.Controls
                 // TODO
                 return;
             }
-            var form = new Form { Width = 1000, Height = 300, Icon = ZklResources.ZkIcon, Text = "Game options" };
-            var optionsControl = new ZeroKLobby.MicroLobby.ModOptionsControl { Dock = DockStyle.Fill };
-            form.Controls.Add(optionsControl);
-            Program.TasClient.BattleClosed += (s2, e2) =>
-            {
-                form.Close();
-                form.Dispose();
-                optionsControl.Dispose();
-            };
-            form.Show(); //hack show Program.FormMain
+
+            ZeroKLobby.MicroLobby.ContextMenus.ShowGameOptions();
         }
     }
 }
