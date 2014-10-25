4e565fa7b652f248380c32c51584c3984c06596e
xponen <msafwan86_nnss@hotmail.com>
2014-10-25 17:53:28 +0800

    SkirmishControlTool.cs: implement code readability improvement for sorting function as suggested by Licho

--- ZeroKLobby/MicroLobby/ExtrasTab/SkirmishControlTool.cs
+++ ZeroKLobby/MicroLobby/ExtrasTab/SkirmishControlTool.cs
@@ -35,13 +35,13 @@ namespace ZeroKLobby.MicroLobby.ExtrasTab
 
         /// <summary>
         /// This function sort the content of a List<object[]> table
-        /// using the first entry of that object array (index 0) as
-        /// a string and uses the character (a-z) of the first 3 word
-        /// (of that string) and the 4 first numbers (0-50000) in that
-        /// string as versions.
+        /// using the string on the first index of that object array
         /// 
-        /// Content of the object array will not be altered but
-        /// will be sorted together with the string at first index.
+        /// Content of the object array is not be altered but will 
+        /// be sorted together with the string on first index.
+        /// 
+        /// It sort based on the first 2 character of every word, and
+        /// numbers as version.
         /// 
         /// The words (and numbers) are assumed to be separated by
         /// character such as ".", "-", "_", and " "
@@ -62,133 +62,89 @@ namespace ZeroKLobby.MicroLobby.ExtrasTab
             //initialize score table
             List<object[]> scoredItem = new List<object[]>(nameList.Count);
             for (int i = 0; i < nameList.Count; i++)
-                scoredItem.Add(new object[2] { 0, nameList[i] });
-
-
-            //score each entry
-            for (int i = 0; i < scoredItem.Count; i++)
             {
                 string[] textComponent = ((string)nameList[i][0]).ToLower().Split(new char[4] { ' ', '.', '-', '_' }, StringSplitOptions.RemoveEmptyEntries);
-                double scoreMultiplier = 0.01;
-                double score = 0;
-                // score for 1st,2nd,3rd & 4th integer mapped this way: 0.11223344
-                int maxOperation = Math.Min(textComponent.Length, 4);
-                int j = 0;
-                int count = 0;
-                while (j < textComponent.Length && count < maxOperation)
+                List<object> charList = new List<object>();
+                List<bool> numberList = new List<bool>(); //remember which one is char and which one is integer in "charList"
+                for (int j = 0; j < textComponent.Length; j++)
                 {
                     int isInteger = -1;
                     int.TryParse(textComponent[j], out isInteger);
-                    if (isInteger != 0 || textComponent[j] == "0") //score integer
+                    if (isInteger != 0 || textComponent[j].Length == 1)
                     {
-                        score = score + (1 - 1 * Math.Exp(-isInteger / 130.0) + 0.00002 * isInteger) * 99 * scoreMultiplier;
-                        scoreMultiplier = scoreMultiplier / 100; //go to next lower significant integer
-                        count = count + 1;
+                        charList.Add(isInteger);
+                        numberList.Add(true);
                     }
-                    else if (textComponent[j].Length >= 2) //text, but probably versioning char (like 'v' or 'r')
+                    else //text, but probably versioning char (like 'v' or 'r')
                     {
                         int.TryParse(textComponent[j].Substring(1), out isInteger);
-                        if (isInteger != 0 || textComponent[j].Substring(1) == "0")  //trailing integer is probably version number
+                        if (isInteger != 0 || textComponent[j][1]=='0')  //trailing integer is probably version number
                         {
-                            score = score + (1 - 1 * Math.Exp(-isInteger / 130.0) + 0.00002 * isInteger) * 99 * scoreMultiplier;
-                            scoreMultiplier = scoreMultiplier / 100; //go to next lower significant integer
-                            count = count + 1;
-
-                            // For reference, equation:
-                            // (1 - 1 * Math.Exp(-isInteger / 140.0) + 0.00002 * isInteger) 
-                            // behave as following:
-                            //   ^
-                            //  2|                                     x 
-                            //   |                                x  
-                            //   |                          x  
-                            //   |                    x   
-                            //   |              x  
-                            //  1|        x  
-                            //   |    x
-                            //   |   x                                    
-                            //   |  x                                      
-                            //   | x                                       
-                            //   |x                                       
-                            // 0 ----------------------------------------> isInteger
-                            //   0   600                               50000
-                            //
-                            //  Targeted properties:
-                            //  1) have exaggerated sensitivity for isInteger <600 (to avoid small value like 1 or 2 approaching 0)
-                            //  2) low sensitivity for higher isInteger (to avoid big value like 50000 from filling up all the score's digit)
-                            //
-                            // "Score Digit":
-                            // If we have "Zero-K v1.10.0.500"
-                            // then we will use the following digit to score the numbers:
-                            // v1 --> 0.xx
-                            // .10 --> 0.00xx
-                            // .0 ---> 0.0000xx
-                            // .500 ---> 0.000000xx
-                            // If we literally translate versioning number digit by digit, then:
-                            // v1.10.0.500 --> 0.01100500
-                            // v1.10.1.0 --> 0.01100100 (notice that previous version yield a bigger score-digit than current version)
-                            //
-                            // So we have to scale down the "500" to fit its score digit, 
-                            // but can't scale down "1" using the same amount because it will reach 0.
-                            // So we use this equation: "(1 - 1 * Math.Exp(-isInteger / 140.0) + 0.00002 * isInteger)"
-                        }
-                    }
-                    j = j + 1;
-                }
-
-                scoreMultiplier = 01000000;
-                // score for 1st,2nd & 3rd word mapped this way: 11112233
-                maxOperation = Math.Min(textComponent.Length, 3);
-                j = 0;
-                while (j < maxOperation)
-                {
-                    int isInteger = -1;
-                    int.TryParse(textComponent[j], out isInteger);
-                    if (isInteger == 0 && textComponent[j] != "0") //score text
-                    {
-                        int letterToCheck = Math.Min(textComponent[j].Length, 2);
-                        char[] letters = textComponent[j].Substring(0, letterToCheck).ToCharArray();
-
-                        if (j >= 1)
-                        { //for next 2 word, score only first character
-                            for (int k = 0; k < charIndex.Length; k++)
-                                if (charIndex[k] == letters[0])
-                                {
-                                    score = score + k * scoreMultiplier;
-                                    break;
-                                }
-                            scoreMultiplier = scoreMultiplier / 100; //go to next lower significant digit
+                            charList.Add(isInteger);
+                            numberList.Add(true);
                         }
                         else
-                        { //for first word, score 2 character at once
-                            double tempScoreMultiplier = scoreMultiplier;
-                            for (int L = 0; L < letters.Length; L++)
-                            {
-                                for (int k = 0; k < charIndex.Length; k++)
-                                    if (charIndex[k] == letters[L])
-                                    {
-                                        score = score + k * tempScoreMultiplier;
-                                        tempScoreMultiplier = tempScoreMultiplier / 100;
-                                        break;
-                                    }
-                            }
-                            scoreMultiplier = scoreMultiplier / 10000; //go to next lower significant digit
+                        {  // score for 1st,2nd char in a word
+                            charList.Add(textComponent[j][0]);
+                            charList.Add(textComponent[j][1]);
+                            numberList.Add(false);
+                            numberList.Add(false);
                         }
                     }
-                    j = j + 1;
                 }
-                scoredItem[i][0] = score;
+                scoredItem.Add(new object[3] { 
+                    charList.ToArray(),
+                    numberList.ToArray(),
+                    nameList[i] });
             }
+
 
             scoredItem.Sort(delegate(object[] x, object[] y) //Reference: http://msdn.microsoft.com/en-us/library/b0zbh7b6(v=vs.110).aspx
             {
-                if ((double)x[0] < (double)y[0]) return -1;
-                else if ((double)x[0] == (double)y[0]) return 0;
-                else return 1;
+                object[] tableX = (object[])x[0];
+                bool[] tableXnum = (bool[])x[1];
+                object[] tableY = (object[])y[0];
+                bool[] tableYnum = (bool[])y[1];
+                int loopCount = Math.Max(tableX.Length, tableY.Length);
+                for (int i = 0; i < loopCount; i++)
+                {
+                    int scoreX = -2;
+                    int scoreY = -2;
+                    if (i < tableX.Length)
+                    {
+                        if (tableXnum[i]) //a number
+                            scoreX = (int)tableX[i];
+                        else //a char 
+                            scoreX = GetMatchingCharIndex(charIndex, (char)tableX[i]);
+                    }
+
+                    if (i < tableY.Length)
+                    {
+                        if (tableYnum[i])
+                            scoreY = (int)tableY[i];
+                        else
+                            scoreY = GetMatchingCharIndex(charIndex, (char)tableY[i]);
+                    }
+
+                    if (scoreX > scoreY || scoreY==-2) return 1; //is "1" when X is bigger than Y or when Y is empty
+                    if (scoreX == -2 || scoreX < scoreY) return -1; //is "-1" when X is smaller than Y or when X is empty
+                }
+                return 0; //same chars or numbers
             });
+
             for (int i = 0; i < nameList.Count; i++)
-                nameList[i] = (object[])scoredItem[i][1];// +" " + ((double)scoredItem[i][0]).ToString();
+                nameList[i] = (object[])scoredItem[i][2];// +" " + ((double)scoredItem[i][0]).ToString();
 
             return nameList;
+        }
+
+        private static int GetMatchingCharIndex(char[] charIndex, char aChar)
+        {
+            for (int k = 0; k < charIndex.Length; k++)
+                if (charIndex[k] == aChar)
+                    return k;
+
+            return -1;
         }
 
         /// <summary>
@@ -269,15 +225,15 @@ namespace ZeroKLobby.MicroLobby.ExtrasTab
                     aiLibFolder = PlasmaShared.Utils.MakePath(aiFolder, aiVerFolder); //eg: Spring/engine/98.0/AI/Skirmish/AAI/0.9
                     aiInfoFile = PlasmaShared.Utils.MakePath(aiLibFolder, "AIInfo.lua"); //eg: Spring/engine/98.0/AI/Skirmish/AAI/0.9/AIInfo.lua
                     var info = GetAIInfo(aiInfoFile);
-                    if (info[0] != null && info[1] != null)
+                    if (info.ContainsKey("shortName") && info.ContainsKey("version"))
                     {
                         var bot = new Ai()
                         {
                             Info = new AiInfoPair[3] 
                         { 
-                            new AiInfoPair { Key = "shortName", Value = info[0] } , //usually equivalent to AI folder name (aiName[i])
-                            new AiInfoPair { Key = "version", Value = info[1] } , //usually equivalent to sub- AI folder name (aiVerFolder)
-                            new AiInfoPair { Key = "description", Value = info[2] } ,
+                            new AiInfoPair { Key = "shortName", Value = info.ContainsKey("shortName")?info["shortName"]:"" } , //usually equivalent to AI folder name (aiName[i])
+                            new AiInfoPair { Key = "version", Value = info.ContainsKey("version")?info["version"]:"" } , //usually equivalent to sub- AI folder name (aiVerFolder)
+                            new AiInfoPair { Key = "description", Value = info.ContainsKey("description")?info["description"]:"" } ,
                         }
                         };
                         springAis.Add(bot);
@@ -301,65 +257,51 @@ namespace ZeroKLobby.MicroLobby.ExtrasTab
         /// <summary>
         /// Get AI description from AIInfo.lua
         /// </summary>
-        private static string[] GetAIInfo(string filePath)
+        private static Dictionary<string, string> GetAIInfo(string filePath)
         {
-            string[] info = new string[3];
+            Dictionary<string, string> info = new Dictionary<string, string>();
             if (!File.Exists(filePath)) return info;
 
-            bool descriptionFound = false;
-            bool versionFound = false;
-            bool shortNameFound = false;
             //Open the stream and read
             using (FileStream fileStream = File.OpenRead(filePath))
             {
                 string currentLine;
-                string description;
-                //int open;
-                int close;
+                string longValue;
+                string shortValue;
+                string keyName;
                 using (var stream = new StreamReader(fileStream))
                     while (!stream.EndOfStream)
                     {
                         currentLine = stream.ReadLine();
-                        if (!shortNameFound && currentLine.Contains("key    = 'shortName',"))
+                        if (currentLine.Contains("key    = "))
                         {
-                            shortNameFound = true;
+                            keyName = GetValue(currentLine);
+                            System.Diagnostics.Trace.TraceError(keyName);
                             currentLine = stream.ReadLine();
-                            //open = currentLine.IndexOf("value  = ", 0, currentLine.Length, StringComparison.InvariantCultureIgnoreCase) + 9;
-                            //close = currentLine.IndexOf(',', open);
-                            //currentLine = currentLine.Substring(open, close - open);
-                            //info[0] = currentLine.Trim(new char[1] { ' ' });
-                            info[0] = currentLine.Substring(12, currentLine.Length - 12 - 2); // eg: "		value  = 'KAIK',"
-                            currentLine = stream.ReadLine();
-                        }
-                        if (!versionFound && currentLine.Contains("key    = 'version',"))
-                        {
-                            versionFound = true;
-                            currentLine = stream.ReadLine();
-                            close = currentLine.IndexOf(',', 12); //eg: "		value  = '0.13', -- AI version - !This comment is used for parsing!"
-                            info[1] = currentLine.Substring(12, close - 12 - 1);
-                            currentLine = stream.ReadLine();
-                        }
-                        if (!descriptionFound && currentLine.Contains("key    = 'description',"))
-                        {
-                            descriptionFound = true;
-                            currentLine = stream.ReadLine();
-                            description = currentLine;
+                            longValue = currentLine;
                             currentLine = stream.ReadLine();
                             while (!currentLine.Contains("desc   = "))
                             {
-                                description = description + currentLine; //multiple line, such as RAI's description.
+                                longValue = longValue + currentLine; //can contain multiple line, such as RAI's description, so we append line until some sign that tell us to stop
                                 currentLine = stream.ReadLine();
                             }
-                            //open = description.IndexOf("value  = ", 0, description.Length, StringComparison.InvariantCultureIgnoreCase) + 9;
-                            //close = description.IndexOf(',', open);
-                            //description = description.Substring(open, close - open);
-                            description = description.Substring(12, description.Length - 12 - 2);
-                            info[2]= description.Trim(new char[5] { ' ', '\t', '[', ']', '\'' });
+                            shortValue = GetValue(longValue).Trim(new char[5] { ' ', '\t', '[', ']', '\'' });
+                            info.Add(keyName, shortValue);
                         }
-                        if (shortNameFound && versionFound && descriptionFound) return info;
                     }
             }
             return info;
+        }
+
+        private static string GetValue(string line)
+        {
+            //eg1: "		value  = 'KAIK',"
+            //eg2: "		value  = '0.13', -- AI version - !This comment is used for parsing!"
+            //eg3: "		value  = [[Plays a nice, slow game, concentrating on base building.\nUses ground, air, hovercrafts and water well.\nDoes some smart moves and handles T2+ well.\n
+            //                       Realtive CPU usage: 1.0\nScales well with growing unit numbers.\nKnown to Support: BA, SA, XTA, NOTA]],"
+            int open = line.IndexOf("= ", 0, line.Length) + 2 + 1;
+            int close = line.IndexOf(',', open) - 1;
+            return line.Substring(open, close - open); 
         }
     }
 }
