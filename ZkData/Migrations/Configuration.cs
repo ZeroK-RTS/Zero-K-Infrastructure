b94611223301e6899bb5a4e49b297ac69d10e74f
Licho <licho@licho.eu>
2015-01-06 14:34:52 +0100

    also create forum categories for local testing of news posting

--- ZkData/Migrations/Configuration.cs
+++ ZkData/Migrations/Configuration.cs
@@ -1,39 +1,47 @@
 using System.Data.Entity.Migrations;
-using System.Linq;
-using ZkData;
 
 namespace ZkData.Migrations
 {
-    internal sealed class Configuration : DbMigrationsConfiguration<ZkDataContext>
+    sealed class Configuration: DbMigrationsConfiguration<ZkDataContext>
     {
         public Configuration()
         {
             ContextKey = "PlasmaShared.Migrations.Configuration"; // if you change this, you also ahve to change content of __MigrationHistory table in DB
+               
             AutomaticMigrationsEnabled = false;
         }
 
         protected override void Seed(ZkDataContext db)
         {
             //  This method will be called after migrating to the latest version.
-           
+
             if (GlobalConst.Mode == ModeType.Local) {
                 // fill local DB with some basic test data
 
-                db.MiscVars.AddOrUpdate(x => x.VarName, new MiscVar() { VarName = "NightwatchPassword", VarValue = "dummy" });
+                db.MiscVars.AddOrUpdate(x => x.VarName, new MiscVar { VarName = "NightwatchPassword", VarValue = "dummy" });
 
-                db.Accounts.AddOrUpdate(x=>x.Name, new Account() {
-                    Name = "test",
-                    Password = Utils.HashLobbyPassword("test"),
-                    IsZeroKAdmin = true,
-                    Kudos = 200,
-                    Elo = 1700,
-                    EloWeight = 2,
-                    SpringieLevel = 4,
-                    Country = "cz",
-                    LobbyID = 2
-                });
+                db.Accounts.AddOrUpdate(x => x.Name,
+                    new Account {
+                        Name = "test",
+                        Password = Utils.HashLobbyPassword("test"),
+                        IsZeroKAdmin = true,
+                        Kudos = 200,
+                        Elo = 1700,
+                        EloWeight = 2,
+                        SpringieLevel = 4,
+                        Country = "cz",
+                        LobbyID = 2
+                    });
+
+                db.ForumCategories.AddOrUpdate(x => x.Title, 
+                    new ForumCategory { Title = "General discussion", },
+                    new ForumCategory { Title = "News", IsNews = true }, 
+                    new ForumCategory { Title = "Maps", IsMaps = true },
+                    new ForumCategory { Title = "Battles", IsSpringBattles = true }, 
+                    new ForumCategory { Title = "Missions", IsMissions = true },
+                    new ForumCategory { Title = "Clans", IsClans = true }, 
+                    new ForumCategory { Title = "Planets", IsPlanets = true });
             }
-
         }
     }
 }
