255ce74b64b61706c9ffa970bbf24ef1305e1c91
Licho <licho@licho.eu>
2015-01-25 01:43:08 +0100

    stop recording data for zero-k.info/stats

--- NightWatch/Nightwatch.cs
+++ NightWatch/Nightwatch.cs
@@ -22,15 +22,9 @@ namespace CaTracker
 
     public class Nightwatch
     {
-        DateTime lastStatsSave;
-
         Timer recon;
-        readonly Timer statsTimer = new Timer(60000);
         TasClient tas;
-
-        public Dictionary<string, int> MapPlayerMinutes = new Dictionary<string, int>();
-        public Dictionary<string, int> ModPlayerMinutes = new Dictionary<string, int>();
-
+        
         public TasClient Tas { get { return tas; } }
         public static Config config;
         string webRoot;
@@ -59,9 +53,6 @@ namespace CaTracker
             tas = new TasClient(null, "NightWatch", GlobalConst.ZkLobbyUserCpu);
             this.webRoot = webRoot;
 			config = new Config();
-			statsTimer.Elapsed += statsTimer_Elapsed;
-			statsTimer.AutoReset = true;
-			statsTimer.Start();
             Trace.Listeners.Add(new NightwatchTraceListener(tas));
         }
 
@@ -125,57 +116,7 @@ namespace CaTracker
 			return true;
 		}
 
-        
-        
-
-
-		void TrackPlayerMinutes()
-		{
-            Directory.SetCurrentDirectory(webRoot);
-			try
-			{
-				var now = DateTime.Now;
-				if (now.Minute != lastStatsSave.Minute)
-				{
-					foreach (var pair in tas.ExistingBattles)
-					{
-						var mod = pair.Value.ModName;
-						var map = pair.Value.MapName;
-						var cnt = pair.Value.Users.Count;
-						if (ModPlayerMinutes.ContainsKey(mod)) ModPlayerMinutes[mod] = ModPlayerMinutes[mod] + cnt;
-						else ModPlayerMinutes.Add(mod, cnt);
-
-						if (MapPlayerMinutes.ContainsKey(map)) MapPlayerMinutes[map] = MapPlayerMinutes[map] + cnt;
-						else MapPlayerMinutes.Add(map, cnt);
-					}
-
-					if (!Directory.Exists("stats")) Directory.CreateDirectory("stats");
-
-					var fname = now.ToString("yyyy-MM-dd-HH") + ".mods";
-					var res = "";
-					foreach (var st in ModPlayerMinutes) res += st.Key + "|" + st.Value + "\n";
-					File.WriteAllText(Directory.GetCurrentDirectory() + "/stats/" + fname, res);
-
-					fname = now.ToString("yyyy-MM-dd-HH") + ".maps";
-					res = "";
-					foreach (var st in MapPlayerMinutes) res += st.Key + "|" + st.Value + "\n";
-					File.WriteAllText(Directory.GetCurrentDirectory() + "/stats/" + fname, res);
-
-					if (now.Hour != lastStatsSave.Hour)
-					{
-						ModPlayerMinutes.Clear();
-						MapPlayerMinutes.Clear();
-					}
-
-					lastStatsSave = now;
-				}
-			}
-			catch (Exception e)
-			{
-				Trace.TraceError("Error while tracking stats " + e.ToString());
-			}
-		}
-
+       
 		void recon_Elapsed(object sender, ElapsedEventArgs e)
 		{
 			if (tas.IsConnected && tas.IsLoggedIn) return;
@@ -188,11 +129,6 @@ namespace CaTracker
 			{
 				recon.Start();
 			}
-		}
-
-		void statsTimer_Elapsed(object sender, ElapsedEventArgs e)
-		{
-			TrackPlayerMinutes();
 		}
 
 		void tas_Connected(object sender, TasEventArgs e)
