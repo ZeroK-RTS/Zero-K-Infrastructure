c6d98d074b1c7ff1cc056fac9094b7dd92e914b5
Licho <licho@licho.eu>
2015-01-28 01:21:47 +0100

    minor refactoring

--- Shared/PlasmaShared/LobbyMessages/CommandJsonSerializer.cs
+++ Shared/PlasmaShared/LobbyMessages/CommandJsonSerializer.cs
@@ -2,14 +2,13 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using Newtonsoft.Json;
-using ZkData;
 
 namespace PlasmaShared.LobbyMessages
 {
     public class CommandJsonSerializer
     {
-        Dictionary<string, Type> knownTypes = new Dictionary<string, Type>();
-        JsonSerializerSettings settings = new JsonSerializerSettings();
+        readonly Dictionary<string, Type> knownTypes = new Dictionary<string, Type>();
+        readonly JsonSerializerSettings settings = new JsonSerializerSettings();
 
         public CommandJsonSerializer()
         {
@@ -19,110 +18,39 @@ namespace PlasmaShared.LobbyMessages
             RegisterType<Register>();
             RegisterType<RegisterResponse>();
 
-            settings.Formatting =Formatting.None;
+            settings.Formatting = Formatting.None;
             settings.NullValueHandling = NullValueHandling.Ignore;
         }
 
 
-        public void RegisterTypes(params Type[] types)
-        {
-            foreach (var t in types) knownTypes[t.Name] = t;
-        }
-
-        public void RegisterType<T>()
-        {
-            var t = typeof(T);
-            knownTypes[t.Name] = t;
-        }
-        
         public object DeserializeLine(string line)
         {
             if (!string.IsNullOrEmpty(line)) {
-                var parts = line.Split(new[] { " " }, 2, StringSplitOptions.RemoveEmptyEntries);
+                string[] parts = line.Split(new[] { " " }, 2, StringSplitOptions.RemoveEmptyEntries);
                 if (parts.Length != 2) throw new Exception(string.Format("Invalid json data {0} : {1}", this, line));
-                else {
-                    Type type;
-                    if (!knownTypes.TryGetValue(parts[0], out type)) {
-                        throw  new Exception(string.Format("Invalid json type {0} : {1}", this, parts[0]));
-                    } else {
-                        return JsonConvert.DeserializeObject(parts[1], type);
-                    }
-                }
+                Type type;
+                if (!knownTypes.TryGetValue(parts[0], out type)) throw new Exception(string.Format("Invalid json type {0} : {1}", this, parts[0]));
+                return JsonConvert.DeserializeObject(parts[1], type);
             }
             return null;
         }
 
+        public void RegisterType<T>()
+        {
+            Type t = typeof(T);
+            knownTypes[t.Name] = t;
+        }
+
+        public void RegisterTypes(params Type[] types)
+        {
+            foreach (Type t in types) knownTypes[t.Name] = t;
+        }
+
         public string SerializeToLine<T>(T value)
         {
-            return string.Format("{0} {1}\n",typeof(T).Name, JsonConvert.SerializeObject(value, settings));
+            return string.Format("{0} {1}\n", typeof(T).Name, JsonConvert.SerializeObject(value, settings));
         }
     }
 
-    public class Welcome
-    {
-        public string Version;
-        public string Game;
-        public string Engine;
-    }
-
-    public class Login
-    {
-        public string Name;
-        public string PasswordHash;
-    }
-
-    public class Register
-    {
-        public string Name;
-        public string PasswordHash;
-    }
-
-    public class RegisterResponse
-    {
-        public enum Code
-        {
-            Ok = 0,
-
-            [Description("already connected")]
-            AlreadyConnected = 1,
-
-            [Description("name already exists")]
-            InvalidName = 2,
-
-            [Description("invalid password")]
-            InvalidPassword = 3,
-
-            [Description("banned")]
-            Banned = 4
-        }
-
-        public Code ResultCode;
-        public string Reason;
-    }
-
-
-
-    public class LoginResponse
-    {
-        public enum Code
-        {
-            Ok = 0,
-            
-            [Description("already connected")]
-            AlreadyConnected = 1,
-            
-            [Description("invalid name")]
-            InvalidName = 2,
-            
-            [Description("invalid password")]
-            InvalidPassword = 3,
-
-            [Description("banned")]
-            Banned = 4
-        }
-
-        public Code ResultCode;
-        public string Reason;
-    }
-
+   
 }
---
+++ Shared/PlasmaShared/LobbyMessages/Messages.cs
@@ -0,0 +1,109 @@
+﻿using System.ComponentModel;
+
+namespace PlasmaShared.LobbyMessages
+{
+    /// <summary>
+    /// Initial message sent by server to client on connect
+    /// </summary>
+    public class Welcome
+    {
+        /// <summary>
+        /// Default suggested engine
+        /// </summary>
+        public string Engine;
+        /// <summary>
+        /// Default suggested game version
+        /// </summary>
+        public string Game;
+        /// <summary>
+        /// Lobby server version
+        /// </summary>
+        public string Version;
+    }
+
+
+    /// <summary>
+    /// Login request
+    /// </summary>
+    public class Login
+    {
+        /// <summary>
+        /// User name
+        /// </summary>
+        public string Name;
+        /// <summary>
+        /// base64(md5(password))
+        /// </summary>
+        public string PasswordHash;
+    }
+
+    /// <summary>
+    /// Registration request
+    /// </summary>
+    public class Register
+    {
+        /// <summary>
+        /// User name
+        /// </summary>
+        public string Name;
+        /// <summary>
+        /// base64(md5(password))
+        /// </summary>
+        public string PasswordHash;
+    }
+
+    public class RegisterResponse
+    {
+        public enum Code
+        {
+            Ok = 0,
+
+            [Description("already connected")]
+            AlreadyConnected = 1,
+
+            [Description("name already exists")]
+            InvalidName = 2,
+
+            [Description("invalid password")]
+            InvalidPassword = 3,
+
+            [Description("banned")]
+            Banned = 4
+        }
+        
+        public Code ResultCode;
+
+        /// <summary>
+        /// Additional text (ban reason)
+        /// </summary>
+        public string Reason;
+    }
+
+
+    public class LoginResponse
+    {
+        public enum Code
+        {
+            Ok = 0,
+
+            [Description("already connected")]
+            AlreadyConnected = 1,
+
+            [Description("invalid name")]
+            InvalidName = 2,
+
+            [Description("invalid password")]
+            InvalidPassword = 3,
+
+            [Description("banned")]
+            Banned = 4
+        }
+
+        public Code ResultCode;
+
+        /// <summary>
+        /// Additional text (ban reason)
+        /// </summary>
+        public string Reason;
+    }
+}
