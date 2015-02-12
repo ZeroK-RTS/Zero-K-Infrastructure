9308ddf0d9251ee3401718d3301b4bddc25ee894
Licho <licho@licho.eu>
2015-02-12 14:49:07 +0100

    removed unused using

--- Zero-K.info/Controllers/GithubController.cs
+++ Zero-K.info/Controllers/GithubController.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Net.Mime;
-using System.Web;
+﻿using System.IO;
 using System.Web.Mvc;
 using LobbyClient;
 using Newtonsoft.Json.Linq;
