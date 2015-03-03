8efa3258da0e8726d985404a2d536f75a47d4a47
sprunk <gnurps@o2.pl>
2015-03-03 21:37:35 +0100

    fix

--- Springie/Springie/autohost/Polls/VoteMove.cs
+++ Springie/Springie/autohost/Polls/VoteMove.cs
@@ -26,7 +26,7 @@ namespace Springie.autohost.Polls
             host = words[0];
 
             var target_battle = tas.ExistingBattles.Values.FirstOrDefault(x => x.Founder.Name == host);
-            if (!target_battle)
+            if (target_battle == NULL)
             {
                 ah.Respond(e, string.Format("Host {0} not found", words[0]));
                 return false;
