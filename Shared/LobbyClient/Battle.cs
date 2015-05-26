f5ad5f33a852003ead90ea2ac2e5b5efa63b1e5e
sprunk <gnurps@o2.pl>
2015-05-26 10:38:30 +0200

    Springie fills in fake teams
    Prevents engine from messing up the order

--- Shared/LobbyClient/Battle.cs
+++ Shared/LobbyClient/Battle.cs
@@ -299,24 +299,20 @@ namespace LobbyClient
             var startboxes = new StringBuilder();
             startboxes.Append("return { ");
             script.AppendLine();
-            foreach (var allyNumber in
-                users.Where(x => !x.IsSpectator).Select(x => x.AllyNumber).Union(bots.Select(x => x.AllyNumber)).Union(_rectangles.Keys).Distinct())
+            foreach (int allyNumber = 1; allyNumber <= 32; allyNumber++)
             {
-                // get allies from each player, bot and rectangles (for koth)
                 script.AppendFormat("[ALLYTEAM{0}]\n", allyNumber);
                 script.AppendLine("{");
-                script.AppendFormat("     NumAllies={0};\n", 0);
-                double left = 0, top = 0, right = 1, bottom = 1;
+                script.AppendLine("     NumAllies=0;");
                 BattleRect rect;
-                if (_rectangles.TryGetValue(allyNumber, out rect)) rect.ToFractions(out left, out top, out right, out bottom);
-                // script.AppendFormat(CultureInfo.InvariantCulture, "     StartRectLeft={0};\n", left);
-                // script.AppendFormat(CultureInfo.InvariantCulture, "     StartRectTop={0};\n", top);
-                // script.AppendFormat(CultureInfo.InvariantCulture, "     StartRectRight={0};\n", right);
-                // script.AppendFormat(CultureInfo.InvariantCulture, "     StartRectBottom={0};\n", bottom);
-                startboxes.AppendFormat(CultureInfo.InvariantCulture, "[{0}] = ", allyNumber);
-                startboxes.Append("{ ");
-                startboxes.AppendFormat(CultureInfo.InvariantCulture, "{0}, {1}, {2}, {3}", left, top, right, bottom);
-                startboxes.Append(" }, ");
+                if (_rectangles.TryGetValue(allyNumber, out rect)) {
+                    double left = 0, top = 0, right = 1, bottom = 1;
+                    rect.ToFractions(out left, out top, out right, out bottom);
+                    startboxes.AppendFormat(CultureInfo.InvariantCulture, "[{0}] = ", allyNumber);
+                    startboxes.Append("{ ");
+                    startboxes.AppendFormat(CultureInfo.InvariantCulture, "{0}, {1}, {2}, {3}", left, top, right, bottom);
+                    startboxes.Append(" }, ");
+                }
                 script.AppendLine("}");
             }
 
