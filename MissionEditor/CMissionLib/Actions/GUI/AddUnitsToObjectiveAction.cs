23eef0ff204bca1c7fc7be2bbb7fba6a83a7c6f4
L.J. Lim <kingraptorowns@gmail.com>
2015-08-02 15:56:49 +0800

    Mission Editor: Objectives can be associated with units and/or points Use the new AddPointToObjective and AddUnitsToObjective actions Also refactor ListTemplates.xaml.cs a bit

---
+++ MissionEditor/CMissionLib/Actions/GUI/AddUnitsToObjectiveAction.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+
+namespace CMissionLib.Actions
+{
+	[DataContract]
+	public class AddUnitsToObjectiveAction : Action
+	{
+        string id;
+		string group = String.Empty;
+
+		public AddUnitsToObjectiveAction()
+		{
+		}
+
+        [DataMember]
+        public string ID
+        {
+            get { return id; }
+            set
+            {
+                id = value;
+                RaisePropertyChanged("ID");
+            }
+        }
+
+        [DataMember]
+        public string Group
+        {
+            get { return group; }
+            set
+            {
+                group = value;
+                RaisePropertyChanged("Group");
+            }
+        }
+
+		public override LuaTable GetLuaTable(Mission mission)
+		{
+			var map = new Dictionary<object, object>
+				{
+					{"id", ID},
+                    {"group", group},
+				};
+			return new LuaTable(map);
+		}
+
+		public override string GetDefaultName()
+		{
+			return "Add Units to Objective";
+		}
+	}
+}
