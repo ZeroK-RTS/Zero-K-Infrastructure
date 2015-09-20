145fadcab8ae8e728fdb5ac06aae6328b776bc41
Licho <licho@licho.eu>
2015-09-20 11:48:25 +0200

    u and s tags

---
+++ Shared/PlasmaShared/ForumParser/Tags/STag.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlasmaShared.ForumParser
+{
+    public class STagOpen : OpeningTag<STagClose>
+    {
+        public override string Match { get; } = "[s]";
+
+
+        public override LinkedListNode<Tag> Translate(StringBuilder sb, LinkedListNode<Tag> self)
+        {
+            sb.Append("<strike>");
+            return self.Next;
+        }
+
+        public override Tag Create()
+        {
+            return new STagOpen();
+        }
+    }
+
+    public class STagClose : ScanningTag
+    {
+        public override string Match { get; } = "[/s]";
+
+        public override LinkedListNode<Tag> Translate(StringBuilder sb, LinkedListNode<Tag> self)
+        {
+            sb.Append("</strike>");
+            return self.Next;
+        }
+
+        public override Tag Create()
+        {
+            return new STagClose();
+        }
+    }
+}
---
+++ Shared/PlasmaShared/ForumParser/Tags/UTag.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlasmaShared.ForumParser
+{
+    public class UTagOpen: OpeningTag<UTagClose>
+    {
+        public override string Match { get; } = "[u]";
+
+
+        public override LinkedListNode<Tag> Translate(StringBuilder sb, LinkedListNode<Tag> self) {
+            sb.Append("<u>");
+            return self.Next;
+        }
+
+        public override Tag Create() {
+            return new UTagOpen();
+        }
+    }
+
+    public class UTagClose: ScanningTag
+    {
+        public override string Match { get; } = "[/u]";
+
+        public override LinkedListNode<Tag> Translate(StringBuilder sb, LinkedListNode<Tag> self) {
+            sb.Append("</u>");
+            return self.Next;
+        }
+
+        public override Tag Create() {
+            return new UTagClose();
+        }
+    }
+}
+
