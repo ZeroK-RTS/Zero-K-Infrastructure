fbb658b70dfa637c4a774d214483a80cb1a8e217
Licho <licho@licho.eu>
2015-09-20 17:24:12 +0200

    add poll tag

---
+++ Zero-K.info/ForumParser/Tags/StarTag.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Text;
+using System.Web.Mvc;
+
+namespace ZeroKWeb.ForumParser
+{
+    /// <summary>
+    /// *literal* bold text alternative wiki
+    /// </summary>
+    public class StarTag: ScanningTag
+    {
+        public override string Match { get; } = "*";
+
+        public override LinkedListNode<Tag> Translate(StringBuilder sb, LinkedListNode<Tag> self, HtmlHelper html) {
+            if (self.Next?.Value is LiteralTag && self.Next?.Next?.Value is StarTag)
+            {
+                sb.Append("<strong>");
+                self.Next.Value.Translate(sb, self.Next, html);
+                sb.Append("</strong>");
+                return self.Next.Next.Next;
+            } else
+            {
+                sb.Append("*");
+                return self.Next;
+            }
+        }
+
+        public override Tag Create() {
+            return new StarTag();
+        }
+    }
+}
