931c2de24765d8425d288f2cecb24446a0222bcd
Licho <licho@licho.eu>
2015-09-26 09:47:14 +0200

    simplified header

--- Zero-K.info/ForumParser/Tags/HeaderTag.cs
+++ Zero-K.info/ForumParser/Tags/HeaderTag.cs
@@ -3,6 +3,9 @@ using System.Text;
 
 namespace ZeroKWeb.ForumParser
 {
+    /// <summary>
+    /// = header =  or == header ==  generates h1 to h6 headers along with anchor for table of content
+    /// </summary>
     public class HeaderTag: Tag
     {
         readonly StringBuilder content = new StringBuilder();
@@ -13,8 +16,8 @@ namespace ZeroKWeb.ForumParser
                 if (context.PreviousTag == null || context.PreviousTag.Value is SpaceTag || context.PreviousTag.Value is NewLineTag)
                 {
                     content.Append(letter);
-                    if (content.Length > 6) return false;
-                    if (context.NextChar == null || context.NextChar == ' ' || context.NextChar == '\n' || context.NextChar=='\r') return true;
+                    if (content.Length > 6) return false; // max is h6
+                    if (context.NextChar == null || context.NextChar == ' ' || context.NextChar == '\n' || context.NextChar == '\r') return true;
                     return null;
                 }
             }
@@ -22,24 +25,16 @@ namespace ZeroKWeb.ForumParser
         }
 
         public override LinkedListNode<Tag> Translate(TranslateContext context, LinkedListNode<Tag> self) {
-            var prev = self.Previous;
             var level = GetOriginalContent().Length;
 
-            if ((prev == null || prev.Value is SpaceTag || prev.Value is NewLineTag) && self.Next?.Value is SpaceTag)
             {
-                var ender = self.Next.FirstNode(x => x.Next == null || x.Next.Value is NewLineTag);
-                if (ender.Value is SpaceTag) ender = ender.Previous;
-                var hend = ender?.Value as HeaderTag;
-
-                if (hend != null && hend.GetOriginalContent().Length == level && ender.Previous?.Value is SpaceTag)
+                var ender = self.Next.FirstNode(x => x.Value is HeaderTag || x.Value is NewLineTag);
+                if (ender?.Value is HeaderTag)
                 {
-                    var name = self.Next.Next.GetOriginalContentUntilNode(ender.Previous);
+                    var name = self.Next.GetOriginalContentUntilNode(ender).Trim();
                     var link = name.Replace(" ", "_").Replace("\"", "_").Replace("'", "_");
 
-                    context.AppendFormat("<a name=\"{1}\"><h{0}>", level, link);
-                    self.Next.Next.TranslateUntilNode(context, ender.Previous);
-                    context.AppendFormat("</h{0}></a>", level);
-
+                    context.AppendFormat("<h{0}>{2}<a name=\"{1}\"/></h{0}>", level, link, name);
                     context.AddTocEntry(new TocEntry(name, link, level));
 
                     return ender.Next;
