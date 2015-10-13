14c1d26d1a4b0b18ffabd2fd6adb785ed5aa8517
Licho <licho@licho.eu>
2015-10-13 21:42:25 +0200

    try tofix infinite scroll

--- Zero-K.info/Scripts/site_main.js
+++ Zero-K.info/Scripts/site_main.js
@@ -163,7 +163,8 @@ function GlobalPageInit(root) {
             var page = $("body");
             // bugfix for ebkit based stuff 
             // chrome needs scrolLTop out of jquery by the page. Object Opera, IE, Firefox take the original dom property
-            var scrollTop = (typeof jQuery.browser.mozilla != "undefined" || typeof jQuery.browser.msie != "undefined" || typeof jQuery.browser.opera != "undefined") ? el.scrollTop : page.scrollTop();
+            var scrollTop = el.scrollTop;
+            if (scrollTop == null) scrollTop = page.scrollTop();
             if (el.scrollHeight - (scrollTop + el.clientHeight) < 50) {
                 ajaxScrollEnabled = false;
                 prg.show();
