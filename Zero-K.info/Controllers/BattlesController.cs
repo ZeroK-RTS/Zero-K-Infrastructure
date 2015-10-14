916411b1ea6dcd79b25b14dae533178f93c257b9
Licho <licho@licho.eu>
2015-10-14 11:48:39 +0200

    hide battle results by default for games not played

--- Zero-K.info/Controllers/BattlesController.cs
+++ Zero-K.info/Controllers/BattlesController.cs
@@ -1,77 +1,78 @@
- using System;
+using System;
 using System.Collections.Generic;
- using System.Data.Entity;
- using System.Linq;
-using System.Web;
+using System.Data.Entity;
+using System.Linq;
 using System.Web.Mvc;
 using ZkData;
-using ZeroKWeb.Models;
 
 namespace ZeroKWeb.Controllers
 {
-    public class BattlesController : Controller
+    public class BattlesController: Controller
     {
         //
         // GET: /Battles/
 
         /// <summary>
-        /// Returns the page of the <see cref="SpringBattle"/> with the specified ID
+        ///     Returns the page of the <see cref="SpringBattle" /> with the specified ID
         /// </summary>
-        public ActionResult Detail(int id)
-        {
-          var db = new ZkDataContext();
-          var bat = db.SpringBattles.FirstOrDefault(x => x.SpringBattleID == id);
+        public ActionResult Detail(int id, bool showWinners = false) {
+            var db = new ZkDataContext();
+            ViewBag.ShowWinners = showWinners;
+            var bat = db.SpringBattles.FirstOrDefault(x => x.SpringBattleID == id);
             if (bat == null) return Content("No such battle exists");
-          if (bat.ForumThread != null)
-          {
-            bat.ForumThread.UpdateLastRead(Global.AccountID, false);
-            db.SubmitChanges();
-          }
-          return View("BattleDetail", bat);
+
+            if (bat.ForumThread != null)
+            {
+                bat.ForumThread.UpdateLastRead(Global.AccountID, false);
+                db.SubmitChanges();
+            }
+
+            if (Global.AccountID != 0 && !showWinners && bat.SpringBattlePlayers.Any(y => y.AccountID == Global.AccountID && !y.IsSpectator)) ViewBag.ShowWinners = true; // show winners if player played thatbattle
+
+            return View("BattleDetail", bat);
         }
 
         /// <summary>
-        /// Returns the main battle replay list; params filter
+        ///     Returns the main battle replay list; params filter
         /// </summary>
-        public ActionResult Index(string battleTitle,
-                                  string map,
-                                  string mod,
-                                  string user,
-                                  int? players,
-                                  int? age,
-                                  int? duration,
-                                  bool? mission,
-                                  bool? bots,
-                                  int? offset) {
+        public ActionResult Index(
+            string battleTitle,
+            string map,
+            string mod,
+            string user,
+            int? players,
+            int? age,
+            int? duration,
+            bool? mission,
+            bool? bots,
+            int? offset) {
             var db = new ZkDataContext();
 
-            IQueryable<SpringBattle> q = db.SpringBattles.Include(x=>x.SpringBattlePlayers);
-            
-            if (!string.IsNullOrEmpty(battleTitle))
-                q = q.Where(b => b.Title.Contains(battleTitle));
+            var q = db.SpringBattles.Include(x => x.SpringBattlePlayers);
 
-            if (!string.IsNullOrEmpty(map))
-                q = q.Where(b => b.ResourceByMapResourceID.InternalName.Contains(map));
+            if (!string.IsNullOrEmpty(battleTitle)) q = q.Where(b => b.Title.Contains(battleTitle));
+
+            if (!string.IsNullOrEmpty(map)) q = q.Where(b => b.ResourceByMapResourceID.InternalName.Contains(map));
 
             if (mod == null) mod = "Zero-K";
-            if (!string.IsNullOrEmpty(mod))
-                q = q.Where(b => b.ResourceByModResourceID.InternalName.Contains( mod));
+            if (!string.IsNullOrEmpty(mod)) q = q.Where(b => b.ResourceByModResourceID.InternalName.Contains(mod));
 
             //if (user == null && Global.IsAccountAuthorized) user = Global.Account.Name;
-            if (!string.IsNullOrEmpty(user)) {
+            if (!string.IsNullOrEmpty(user))
+            {
                 var aid = (from account in db.Accounts
-                          where account.Name == user
-                           select account.AccountID).FirstOrDefault();
-                if(aid != 0)
-                    q = q.Where(b => b.SpringBattlePlayers.Any(p => !p.IsSpectator && p.AccountID == aid));
+                    where account.Name == user
+                    select account.AccountID).FirstOrDefault();
+                if (aid != 0) q = q.Where(b => b.SpringBattlePlayers.Any(p => !p.IsSpectator && p.AccountID == aid));
             }
 
-            if (players.HasValue)
-                q = q.Where(b => b.SpringBattlePlayers.Where(p => !p.IsSpectator).Count() == players.Value);
+            if (players.HasValue) q = q.Where(b => b.SpringBattlePlayers.Where(p => !p.IsSpectator).Count() == players.Value);
 
-            if (age.HasValue) {
-                DateTime limit = DateTime.UtcNow;
-                switch (age) {
+            if (age.HasValue)
+            {
+                var limit = DateTime.UtcNow;
+                switch (age)
+                {
                     case 1:
                         limit = DateTime.Now.AddHours(-1);
                         break;
@@ -85,53 +86,53 @@ namespace ZeroKWeb.Controllers
                 q = q.Where(b => b.StartTime >= limit);
             }
 
-            if (duration.HasValue)
-                q = q.Where(b => Math.Abs(b.Duration - duration.Value * 60) < 300);
+            if (duration.HasValue) q = q.Where(b => Math.Abs(b.Duration - duration.Value*60) < 300);
 
-            if (mission.HasValue)
-                q = q.Where(b => b.IsMission == mission.Value);
+            if (mission.HasValue) q = q.Where(b => b.IsMission == mission.Value);
 
-            if (bots.HasValue)
-                q = q.Where(b => b.HasBots == bots.Value);
+            if (bots.HasValue) q = q.Where(b => b.HasBots == bots.Value);
 
             q = q.OrderByDescending(b => b.StartTime);
-                
 
             if (offset.HasValue) q = q.Skip(offset.Value);
             q = q.Take(Global.AjaxScrollCount);
 
-            var result = q.ToList().Select(b =>
-                    new BattleQuickInfo() {
-                        Battle = b,
-                        Players = b.SpringBattlePlayers,
-                        Map = b.ResourceByMapResourceID,
-                        Mod = b.ResourceByModResourceID
-                    }).ToList();
+            var result =
+                q.ToList()
+                    .Select(
+                        b =>
+                            new BattleQuickInfo
+                            {
+                                Battle = b,
+                                Players = b.SpringBattlePlayers,
+                                Map = b.ResourceByMapResourceID,
+                                Mod = b.ResourceByModResourceID
+                            })
+                    .ToList();
 
 
             //if(result.Count == 0)
             //    return Content("");
-            if (offset.HasValue)
-                return View("BattleTileList", result);
-            else
-                return View("BattleIndex", result);
+            if (offset.HasValue) return View("BattleTileList", result);
+            return View("BattleIndex", result);
         }
 
         /// <summary>
-        /// Returns a page with the <see cref="SpringBattle"/> infolog
+        ///     Returns a page with the <see cref="SpringBattle" /> infolog
         /// </summary>
         [Auth(Role = AuthRole.ZkAdmin)]
-        public ActionResult Logs(int id)
-        {
-            using (var db = new ZkDataContext()) {
+        public ActionResult Logs(int id) {
+            using (var db = new ZkDataContext())
+            {
                 var bat = db.SpringBattles.Single(x => x.SpringBattleID == id);
-                return Content(System.IO.File.ReadAllText(string.Format(GlobalConst.InfologPathFormat, bat.EngineGameID)), "text/plain");//,string.Format("infolog_{0}.txt", bat.SpringBattleID)
+                return Content(System.IO.File.ReadAllText(string.Format(GlobalConst.InfologPathFormat, bat.EngineGameID)), "text/plain");
+                    //,string.Format("infolog_{0}.txt", bat.SpringBattleID)
             }
-            
         }
     }
 
-    public struct BattleQuickInfo {
+    public struct BattleQuickInfo
+    {
         public SpringBattle Battle { get; set; }
         public IEnumerable<SpringBattlePlayer> Players { get; set; }
         public Resource Map { get; set; }
