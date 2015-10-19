63a84bc7ce03b9df94b197c5db3386f6608033eb
Licho <licho@licho.eu>
2015-10-19 14:45:16 +0200

    GridHelpers dont need to pass HtmlHelpers as param

---
+++ Zero-K.info/AppCode/HelperPage.cs
@@ -0,0 +1,17 @@
+﻿using System.Web.Mvc;
+using System.Web.WebPages;
+
+namespace ZeroKWeb
+{
+    /// <summary>
+    /// This class gives access to Html. to @helper pages - like GridHelpers
+    /// </summary>
+    public class HelperPage : System.Web.WebPages.HelperPage
+    {
+        // Workaround - exposes the MVC HtmlHelper instead of the normal helper
+        public static new HtmlHelper Html
+        {
+            get { return ((System.Web.Mvc.WebViewPage)WebPageContext.Current.Page).Html; }
+        }
+    }
+}
