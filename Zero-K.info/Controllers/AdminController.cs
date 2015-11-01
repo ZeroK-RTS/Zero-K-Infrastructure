05cba2958290d1566b66e20e3b33a76d9bffe831
Licho <licho@licho.eu>
2015-11-01 09:57:20 +0100

    zk test site gains button  to copy DB from live

---
+++ Zero-K.info/Controllers/AdminController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PlasmaShared;
+using ZkData;
+
+namespace ZeroKWeb.Controllers
+{
+    public class AdminController : Controller
+    {
+        [Auth(Role = AuthRole.ZkAdmin)]
+        public ActionResult ResetDb() {
+            if (GlobalConst.Mode == ModeType.Test)
+            {
+                var cloner = new DbCloner("zero-k", "zero-k_test", GlobalConst.ZkDataContextConnectionString);
+                cloner.CloneAllTables();
+                return Content("DONE! Database copied");
+            } else return Content("Not allowed!");
+        }
+    }
+}
