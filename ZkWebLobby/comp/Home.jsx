75eb9beb3fb844de4515f2e21460076320293c54
ikinz <danny.b.eml@gmail.com>
2016-01-14 06:30:26 +1000

    Start implementing the cheap swl plan.

--- ZkWebLobby/comp/Home.jsx
+++ ZkWebLobby/comp/Home.jsx
@@ -1,67 +1,101 @@
 'use strict'
 
 require('style/Home.sass');
+var _ = require('lodash');
 var React = require('react');
-var classNames = require('classnames');
-var Chat = require('weblobby/comp/Chat.jsx');
+var Battle = require('weblobby/act/Battle.js');
+var ModalWindow = require('weblobby/comp/ModalWindow.jsx');
 
 module.exports = React.createClass({
-	displayName: 'Home',
+	displayName: 'Home@zkwl',
 	getInitialState: function(){
 		return {
-			currentContent: 'newsfeed',
-			chatOpen: false,
-			chatDisplay: false
-		}
+			selected: null,
+			pickinChickin: null,
+		};
 	},
-	handleChat: function(){
-		// We have to do this because if we do the first render with display: block
-		// on the chat div, the entire window will be scrolled to the right where it's
-		// supposed to be hidden. If we set display: none in .show, transitions won't work.
-		if (!this.state.chatDisplay) {
-			this.setState({ chatDisplay: true }, function(){
-				this.setState({ chatOpen: true });
+	handleSelect: function(sel){
+		this.setState({ selected: sel });
+	},
+	handleSkirmish: function(bot){
+		var engine = '100.0';
+		var gameInfo = this.props.gameInfoStore.getInitialState();
+		// Use the latest version of the game installed.
+		// This works by virtue of unitsync filling versions in order.
+		var modname = _(gameInfo.games).keys().filter(function(name){
+			return !!name.match(/^Zero-K v/);
+		}).last();
+		if (!modname) {
+			Log.infoBox('Downloading updates...');
+			Process.downloadGame('zk:stable');
+			if (!_.contains(gameInfo.engines, engine))
+				Process.downloadEngine(engine);
+			this.props.onToggleDownloads();
+			return;
+		}
+		Battle.openLocalBattle('Skirmish vs ' + bot, function(){
+			this.setEngine(engine);
+			this.setGame(modname);
+			this.setMap(_.sample(_.keys(gameInfo.maps)) || '');
+			bot && this.addBot({
+				team: 2,
+				name: 'Enemy',
+				type: bot,
 			});
-		} else {
-			this.setState({ chatOpen: !this.state.chatOpen });
-		}
-		
-		new Audio(`sounds/panel_move.wav`).play();
+		});
 	},
-	handleMenu: function(what){
-		this.setState({ currentContent: what });
-		new Audio(`sounds/button_click.wav`).play();
+	handlePickChicken: function(val){
+		this.setState({ pickinChickin: val });
 	},
-	renderContent: function(){
-		switch (this.state.currentContent) {
-			case 'newsfeed':
-				return <h1>[newsfeed]</h1>;
-			case 'skirmish':
-				return <h1>[skirmish]</h1>;
-			case 'missions':
-				return <h1>[missions]</h1>;
+	renderSubmenu: function(){
+		switch (this.state.selected) {
+
+		case 'missions':
+			return <div className="submenu">
+				<button>Tutorial</button>
+				<button>Sandbox</button>
+				<button>Sunrise Episode 1 - Awakening</button>
+				<button>Carrier has Arrived</button>
+			</div>;
+		case 'skirmish':
+			return <div className="submenu">
+				<button onClick={_.partial(this.handleSkirmish, 'CAI')}>Play vs AI</button>
+				<button onClick={_.partial(this.handlePickChicken, true)}>Play vs Chicken</button>
+				<button onClick={_.partial(this.handleSkirmish, null)}>Custom game</button>
+			</div>;
+		case 'multiplayer':
+			return <div className="submenu">
+				<button>Matchmaker</button>
+				<button onClick={_.partial(this.props.onSelect, 'battlelist')}>Open battles</button>
+			</div>;
+		default:
+			return null;
 		}
 	},
 	render: function(){
-		return <div id="zkHomeScreen">
-			<div id="mainMenu">
-				<button>FIND MATCH</button>
-				<button className="underbutton">Custom battles</button>
-				<button onClick={_.partial(this.handleMenu, 'skirmish')}>SKIRMISH</button>
-				<button onClick={_.partial(this.handleMenu, 'missions')}>MISSIONS</button>
-			</div>
-			<div id="content">{this.renderContent()}</div>
-			<button id="chatButton" onClick={this.handleChat}>Chat</button>
-			<div className={classNames({
-				chatDiv: true,
-				show: this.state.chatOpen,
-				display: this.state.chatDisplay
-			})}>
-				<Chat
-					serverStore={this.props.serverStore}
-					chatStore={this.props.chatStore}
-				/>
-			</div>
-		</div>;
+		return <div id="zkMainMenu">
+		<div className="menu">
+			<button onClick={_.partial(this.handleSelect, 'missions')}>Missions</button>
+			<button onClick={_.partial(this.handleSelect, 'skirmish')}>Skirmish</button>
+			<button onClick={_.partial(this.handleSelect, 'multiplayer')}>Multiplayer</button>
+			<button onClick={_.partial(this.props.onSelect, 'settings')}>Settings</button>
+			<button onClick={_.partial(this.props.onSelect, 'help')}>Help</button>
+			<button>Exit</button>
+		</div>
+		{this.renderSubmenu()}
+		{this.state.pickinChickin && <ModalWindow
+				onClose={_.partial(this.handlePickChicken, false)}
+				title="Pick difficulty"
+		>
+			{['Very Easy', 'Easy', 'Normal', 'Hard', 'Very Hard'].map(function(diff){
+				return <button
+					key={diff}
+					onClick={_.partial(this.handleSkirmish, 'Chicken: ' + diff)}
+				>
+					{diff}
+				</button>;
+			}.bind(this))}
+		</ModalWindow>}
+		</div>
 	}
 });
--- ZkWebLobby/zkwl.jsx
+++ ZkWebLobby/zkwl.jsx
@@ -1,16 +1,46 @@
 'use strict'
 
-var React = require('react');
-var ReactDOM = require('react-dom');
+// We require Applet before anything that uses localStorage, otherwise init()
+// won't set localStorage database path properly.
+var Applet = require('weblobby/store/Applet.js');
 
 require('style/main.sass');
-var Home = require('comp/Home.jsx');
 
-var server = new (require('weblobby/store/LobbyServer.js'))();
-var process = new (require('weblobby/store/Process.js'))();
-var chat = new (require('weblobby/store/Chat.js'))(server, process);
+var _ = require('lodash');
+var React = require('react');
+var ReactDOM = require('react-dom');
+var App = require('weblobby/comp/App.jsx');
 
-ReactDOM.render(<Home
-	serverStore={server}
-	chatStore={chat}
+// Create stores.
+var gameInfo = new (require('weblobby/store/GameInfo.js'))();
+var process = new (require('weblobby/store/Process.js'))(gameInfo);
+var sound = new (require('weblobby/store/Sound.js'))();
+var lobbyServer = new (require('weblobby/store/LobbyServer.js'))();
+var chatStore = new (require('weblobby/store/Chat.js'))(lobbyServer, process);
+var afkStatus = new (require('weblobby/store/AfkStatus.js'))(lobbyServer, process);
+var currentBattle = new (require('weblobby/store/CurrentBattle.js'))(gameInfo,
+	lobbyServer, chatStore, process);
+
+// Back when swl API was a Java applet this function was introduced to avoid
+// deadlocks on OpenJDK. It's kept to avoid breaking changes.
+window.__java_js_wrapper = _.defer;
+
+window.echo = function(){
+   console.log.apply(console, arguments ); //chrome has issue with direct assigning of this function
+}
+
+// Disable the default context menu on most things if running in the wrapper.
+Applet && document.addEventListener('contextmenu', function(evt){
+	if (evt.button === 2 && !(evt.target instanceof HTMLAnchorElement) &&
+			!(evt.target instanceof HTMLInputElement)) {
+		evt.preventDefault();
+	}
+});
+
+ReactDOM.render(<App
+	serverStore={lobbyServer}
+	gameInfoStore={gameInfo}
+	processStore={process}
+	chatStore={chatStore}
+	currentBattleStore={currentBattle}
 />, document.getElementById('main'));
