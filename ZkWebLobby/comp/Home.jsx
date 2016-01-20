b2068feb3ab2ae9791e736bada0af45fabca92e3
ikinz <danny.b.eml@gmail.com>
2016-01-21 00:07:03 +1000

    Continue cheap zkwl plan.
     - Transition effect for submenus.
     - Placeholder background.
     - Change menu structure.
     - Use newer swl with relevant changes.

--- ZkWebLobby/comp/Home.jsx
+++ ZkWebLobby/comp/Home.jsx
@@ -5,17 +5,17 @@ var _ = require('lodash');
 var React = require('react');
 var Battle = require('weblobby/act/Battle.js');
 var ModalWindow = require('weblobby/comp/ModalWindow.jsx');
+var Server = require('weblobby/act/LobbyServer.js');
+var ConState = require('weblobby/store/LobbyServerCommon.js').ConnectionState;
 
 module.exports = React.createClass({
 	displayName: 'Home@zkwl',
 	getInitialState: function(){
 		return {
 			selected: null,
+			hideSubmenu: true,
 			pickinChickin: null,
 		};
-	},
-	handleSelect: function(sel){
-		this.setState({ selected: sel });
 	},
 	handleSkirmish: function(bot){
 		var engine = '100.0';
@@ -33,7 +33,7 @@ module.exports = React.createClass({
 			this.props.onToggleDownloads();
 			return;
 		}
-		Battle.openLocalBattle('Skirmish vs ' + bot, function(){
+		Battle.openLocalBattle('Skirmish' + (bot ? ' vs ' + bot : ''), function(){
 			this.setEngine(engine);
 			this.setGame(modname);
 			this.setMap(_.sample(_.keys(gameInfo.maps)) || '');
@@ -47,26 +47,32 @@ module.exports = React.createClass({
 	handlePickChicken: function(val){
 		this.setState({ pickinChickin: val });
 	},
+	handleOpenBattlelist: function(){
+		if (this.props.serverStore.getInitialState().connection !== ConState.CONNECTED)
+			Server.connect();
+		else
+			this.props.onSelect('battlelist');
+	},
+	handleSelect: function(sel){
+		this.setState({ selected: sel, hideSubmenu: true }, function(){
+			_.defer(function(){ this.setState({ hideSubmenu: false }); }.bind(this));
+		});
+	},
 	renderSubmenu: function(){
+		var cx = 'submenu' + (this.state.hideSubmenu ? '' : ' show');
 		switch (this.state.selected) {
 
-		case 'missions':
-			return <div className="submenu">
-				<button>Tutorial</button>
-				<button>Sandbox</button>
-				<button>Sunrise Episode 1 - Awakening</button>
-				<button>Carrier has Arrived</button>
-			</div>;
-		case 'skirmish':
-			return <div className="submenu">
+		case 'singleplayer':
+			return <div className={cx}>
+				<button>Missions</button>
 				<button onClick={_.partial(this.handleSkirmish, 'CAI')}>Play vs AI</button>
 				<button onClick={_.partial(this.handlePickChicken, true)}>Play vs Chicken</button>
 				<button onClick={_.partial(this.handleSkirmish, null)}>Custom game</button>
 			</div>;
 		case 'multiplayer':
-			return <div className="submenu">
+			return <div className={cx}>
 				<button>Matchmaker</button>
-				<button onClick={_.partial(this.props.onSelect, 'battlelist')}>Open battles</button>
+				<button onClick={this.handleOpenBattlelist}>Open battles</button>
 			</div>;
 		default:
 			return null;
@@ -74,9 +80,9 @@ module.exports = React.createClass({
 	},
 	render: function(){
 		return <div id="zkMainMenu">
+		<div id="background" />
 		<div className="menu">
-			<button onClick={_.partial(this.handleSelect, 'missions')}>Missions</button>
-			<button onClick={_.partial(this.handleSelect, 'skirmish')}>Skirmish</button>
+			<button onClick={_.partial(this.handleSelect, 'singleplayer')}>Singleplayer</button>
 			<button onClick={_.partial(this.handleSelect, 'multiplayer')}>Multiplayer</button>
 			<button onClick={_.partial(this.props.onSelect, 'settings')}>Settings</button>
 			<button onClick={_.partial(this.props.onSelect, 'help')}>Help</button>
---
+++ ZkWebLobby/store/SettingsOverride.js
@@ -0,0 +1,11 @@
+module.exports.localStorageKey = 'zkwl_settings';
+
+module.exports.settings = {
+	"Login": {
+		useZkServer: { val: true },
+	},
+	"Games to show in battle list": {
+		selectedEvo: { val: false },
+		selectedZk: { val: true },
+	},
+};
