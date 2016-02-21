e6bd63d73bf5e1d667abde58cd6ddd9beab06aec
Licho <licho@licho.eu>
2016-02-21 11:26:07 +0100

    reorder

--- ZeroKLobby/Config.cs
+++ ZeroKLobby/Config.cs
@@ -9,9 +9,7 @@ using System.IO;
 using System.IO.IsolatedStorage;
 using System.Linq;
 using System.Text;
-using System.Windows.Forms;
 using System.Xml.Serialization;
-using JetBrains.Annotations;
 using PlasmaDownloader;
 using ZkData;
 
@@ -21,28 +19,49 @@ namespace ZeroKLobby
     {
         public const string ConfigFileName = "ZeroKLobbyConfig.xml";
         public const string LogFile = "ZeroKLobbyErrors.txt";
-
-        public readonly Color BgColor = Color.FromArgb(255,0,60,80);
         public static readonly Font MenuFont = new Font("Verdana", 20, GraphicsUnit.Pixel);
-        public readonly static Font ChatFont = new Font("Microsoft Sans Serif", 14, GraphicsUnit.Pixel);
-        public readonly static  Font GeneralFont = new Font("Microsoft Sans Serif", 14, GraphicsUnit.Pixel);
-        public readonly static Font GeneralFontBig = new Font("Microsoft Sans Serif", 15, FontStyle.Bold, GraphicsUnit.Pixel);
-        public readonly static Font GeneralFontSmall = new Font("Microsoft Sans Serif", 9, GraphicsUnit.Pixel);
+        public static readonly Font ChatFont = new Font("Microsoft Sans Serif", 14, GraphicsUnit.Pixel);
+        public static readonly Font GeneralFont = new Font("Microsoft Sans Serif", 14, GraphicsUnit.Pixel);
+        public static readonly Font GeneralFontBig = new Font("Microsoft Sans Serif", 15, FontStyle.Bold, GraphicsUnit.Pixel);
+        public static readonly Font GeneralFontSmall = new Font("Microsoft Sans Serif", 9, GraphicsUnit.Pixel);
 
 
         public static readonly Font ToolbarFontBig;
         public static readonly Font ToolbarFont;
         public static readonly Font ToolbarFontSmall;
-
         public static readonly Font MainPageFontBig;
         public static readonly Font MainPageFont;
         public static readonly Font MainPageFontSmall;
 
 
-        static readonly PrivateFontCollection pfc = new PrivateFontCollection();
+        private static readonly PrivateFontCollection pfc = new PrivateFontCollection();
 
-        static Config()
-        {
+        public readonly Color BgColor = Color.FromArgb(255, 0, 60, 80);
+        public readonly Color EmoteColor = Color.FromArgb(178, 0, 178);
+        public readonly Color FadeColor = Color.LightGray;
+        public readonly Color JoinColor = Color.FromArgb(42, 140, 42);
+        public readonly Color LeaveColor = Color.FromArgb(102, 54, 31);
+        public readonly Color NoticeColor = Color.Red;
+        public readonly Color TextColor = Color.White;
+        public readonly Color TooltipColor = Color.White;
+
+
+        [Browsable(false)]
+        public bool BlockNonFriendPm;
+
+        private string cachedPassword;
+        public bool IsFirstRun = true;
+
+
+        public Color LinkColor = Color.DodgerBlue;
+        public int OtherTextColorInt = Color.Black.ToArgb();
+
+        /// <summary>
+        ///     Keeps datetime of last topic change for each channel
+        /// </summary>
+        public SerializableDictionary<string, DateTime?> Topics = new SerializableDictionary<string, DateTime?>();
+
+        static Config() {
             FontFamily fancyFont;
 
             // TODO copy out from resources
@@ -50,11 +69,7 @@ namespace ZeroKLobby
             {
                 pfc.AddFontFile("Sm.ttf");
                 fancyFont = pfc.Families[0];
-            }
-            else
-            {
-                fancyFont = FontFamily.GenericSansSerif;
-            }
+            } else fancyFont = FontFamily.GenericSansSerif;
 
             MenuFont = new Font(fancyFont, 20, GraphicsUnit.Pixel);
 
@@ -65,32 +80,13 @@ namespace ZeroKLobby
             MainPageFontBig = new Font(fancyFont, 14, GraphicsUnit.Pixel);
             MainPageFont = new Font(fancyFont, 12, GraphicsUnit.Pixel);
             MainPageFontSmall = new Font(fancyFont, 9, GraphicsUnit.Pixel);
-
         }
 
-
-
-
-
-
-        StringCollection autoJoinChannels = new StringCollection() { };
-        bool connectOnStartup = true;
-        public readonly Color FadeColor = Color.LightGray;
-        StringCollection friends = new StringCollection(); // lacks events for adding friends immediatly
-        int idleTime = 5;
-        StringCollection ignoredUsers = new StringCollection();
-        bool showHourlyChimes = true;
-        bool showOfficialBattles = true;
-        bool hideEmptyBattles = false;
-        bool hideNonJoinableBattles = false;
-        bool hidePasswordedBattles = false;
-
-        string skirmisherEngine;
-        string skirmisherGame;
-        string skirmisherMap;
-
-        string springServerHost = GlobalConst.LobbyServerHost;
-        int springServerPort = GlobalConst.LobbyServerPort;
+        public Config() {
+            EnableVoiceChat = true;
+            SpringServerHost = GlobalConst.LobbyServerHost;
+            SpringServerPort = GlobalConst.LobbyServerPort;
+        }
 
         [Category("Chat")]
         [DisplayName("Automatically Joined Channels")]
@@ -98,32 +94,17 @@ namespace ZeroKLobby
         [Browsable(true)]
         [Editor("System.Windows.Forms.Design.StringCollectionEditor, System.Design, Version=2.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a",
             typeof(UITypeEditor))]
-        public StringCollection AutoJoinChannels
-        {
-            get { return autoJoinChannels; }
-            set { autoJoinChannels = value; }
-        }
+        public StringCollection AutoJoinChannels { get; set; } = new StringCollection();
 
 
         [Browsable(false)]
         public string BattleFilter { get; set; }
 
 
-
-
-
-        [Browsable(false)]
-        public bool BlockNonFriendPm;
-
-
         [Category("Connection")]
         [DisplayName("Connect on startup")]
         [Description("Connect and login player on program start?")]
-        public bool ConnectOnStartup
-        {
-            get { return connectOnStartup; }
-            set { connectOnStartup = value; }
-        }
+        public bool ConnectOnStartup { get; set; } = true;
 
         [Category("General")]
         [DisplayName("Content DATA FOLDER")]
@@ -166,8 +147,6 @@ namespace ZeroKLobby
         [Description("Disable ZKL tooltip that display tips on buttons and options.")]
         public bool DisableTextTooltip { get; set; }
 
-        public readonly Color EmoteColor = Color.FromArgb(178, 0, 178);
-
         [Category("General")]
         [DisplayName("Enable voice chat (push to talk)")]
         [Description("Needs steam running")]
@@ -188,11 +167,7 @@ namespace ZeroKLobby
         [Browsable(true)]
         [Editor("System.Windows.Forms.Design.StringCollectionEditor, System.Design, Version=2.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a",
             typeof(UITypeEditor))]
-        public StringCollection Friends
-        {
-            get { return friends; }
-            set { friends = value; }
-        }
+        public StringCollection Friends { get; set; } = new StringCollection();
         [Browsable(false)]
         public bool HasHosted { get; set; }
         [Browsable(false)]
@@ -200,32 +175,16 @@ namespace ZeroKLobby
         [Browsable(false)]
         public string HostBattle_Title { get; set; }
         [Browsable(false)]
-        public bool HideEmptyBattles
-        {
-            get { return hideEmptyBattles; }
-            set { hideEmptyBattles = value; }
-        }
+        public bool HideEmptyBattles { get; set; } = false;
         [Browsable(false)]
-        public bool HideNonJoinableBattles
-        {
-            get { return hideNonJoinableBattles; }
-            set { hideNonJoinableBattles = value; }
-        }
+        public bool HideNonJoinableBattles { get; set; } = false;
         [Browsable(false)]
-        public bool HidePasswordedBattles
-        {
-            get { return hidePasswordedBattles; }
-            set { hidePasswordedBattles = value; }
-        }
+        public bool HidePasswordedBattles { get; set; } = false;
 
         [Category("Quickmatching")]
         [DisplayName("Idle User Time")]
         [Description("Idle minutes after which Zero-K launcher assumes the user is gone and quickmatching is stopped.")]
-        public int IdleTime
-        {
-            get { return idleTime; }
-            set { idleTime = value; }
-        }
+        public int IdleTime { get; set; } = 5;
 
         [Category("Chat")]
         [DisplayName("Ignored Users")]
@@ -233,28 +192,14 @@ namespace ZeroKLobby
         [Browsable(true)]
         [Editor("System.Windows.Forms.Design.StringCollectionEditor, System.Design, Version=2.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a",
             typeof(UITypeEditor))]
-        public StringCollection IgnoredUsers
-        {
-            get { return ignoredUsers; }
-            set { ignoredUsers = value; }
-        }
-        public bool IsFirstRun = true;
+        public StringCollection IgnoredUsers { get; set; } = new StringCollection();
 
-
-        public readonly Color JoinColor = Color.FromArgb(42, 140, 42);
-
-        
-        public readonly Color LeaveColor = Color.FromArgb(102, 54, 31);
-        
 
         [Category("Chat")]
         [DisplayName("Disable Context Menu on Leftclick")]
         [Description("Only right clicking shows context menu. Left clicking on a playername will select them in the player list.")]
         public bool LeftClickSelectsPlayer { get; set; }
 
-
-        public Color LinkColor = Color.DodgerBlue;
-    
         [Category("Account")]
         [DisplayName("Lobby Player Name")]
         [Description("Player name from lobby (tasclient), needed for many features")]
@@ -266,38 +211,37 @@ namespace ZeroKLobby
         [PasswordPropertyText(true)]
         [XmlIgnore]
         [Description("Player password from lobby (tasclient), needed for widget online profile")]
-        public string LobbyPlayerPassword {
+        public string LobbyPlayerPassword
+        {
             get
             {
                 if (!string.IsNullOrEmpty(cachedPassword)) return cachedPassword;
-                try {
+                try
+                {
                     var isoStore = GetIsolatedStorage();
                     using (var file = isoStore.OpenFile("zkl_password.txt", FileMode.OpenOrCreate, FileAccess.Read, FileShare.ReadWrite)) using (var sr = new StreamReader(file)) cachedPassword = sr.ReadToEnd().Trim();
                     return cachedPassword;
-                } catch (Exception ex) {
-                    Trace.TraceWarning("Error loading password from local storage: {0}",ex);
+                }
+                catch (Exception ex)
+                {
+                    Trace.TraceWarning("Error loading password from local storage: {0}", ex);
                     return null;
                 }
             }
             set
             {
-                try {
-                    IsolatedStorageFile isoStore = IsolatedStorageFile.GetStore(IsolatedStorageScope.User | IsolatedStorageScope.Assembly, null, null);
+                try
+                {
+                    var isoStore = IsolatedStorageFile.GetStore(IsolatedStorageScope.User | IsolatedStorageScope.Assembly, null, null);
                     using (var file = isoStore.OpenFile("zkl_password.txt", FileMode.OpenOrCreate, FileAccess.ReadWrite, FileShare.ReadWrite)) using (var sr = new StreamWriter(file)) sr.Write(value);
-                } catch (Exception ex) {
-                    Trace.TraceWarning("Error saving password to local storage: {0}",ex);
+                }
+                catch (Exception ex)
+                {
+                    Trace.TraceWarning("Error saving password to local storage: {0}", ex);
                 }
                 cachedPassword = value;
             }
         }
-
-        static IsolatedStorageFile GetIsolatedStorage()
-        {
-            IsolatedStorageFile isoStore = IsolatedStorageFile.GetStore(IsolatedStorageScope.User | IsolatedStorageScope.Assembly, null, null);
-            return isoStore;
-        }
-
-        string cachedPassword;
 
         [Category("Account")]
         [DisplayName("Forget Player Name")]
@@ -310,93 +254,67 @@ namespace ZeroKLobby
         [Description("Tell ZKL to forget your Password and re-ask it each time it start. (Note: If ZKL crashed or forced to exit this might fail)")]
         public bool DiscardPassword { get; set; }
 
-        public readonly Color NoticeColor = Color.Red;
-        public readonly Color TooltipColor = Color.White;
-        public int OtherTextColorInt = Color.Black.ToArgb();
-
 
         [Category("Chat")]
         [DisplayName("Show Hourly Chat Message")]
         [Description("Show a notification in chat channels every hour.")]
-        public bool ShowHourlyChimes
-        {
-            get { return showHourlyChimes; }
-            set { showHourlyChimes = value; }
-        }
+        public bool ShowHourlyChimes { get; set; } = true;
 
         [Browsable(false)]
-        public bool ShowOfficialBattles
-        {
-            get { return showOfficialBattles; }
-            set { showOfficialBattles = value; }
-        }
+        public bool ShowOfficialBattles { get; set; } = true;
 
         [Browsable(false)]
-        public string SkirmisherEngine
-        {
-            get { return skirmisherEngine; }
-            set { skirmisherEngine = value; }
-        }
+        public string SkirmisherEngine { get; set; }
         [Browsable(false)]
-        public string SkirmisherGame
-        {
-            get { return skirmisherGame; }
-            set { skirmisherGame = value; }
-        }
+        public string SkirmisherGame { get; set; }
         [Browsable(false)]
-        public string SkirmisherMap
-        {
-            get { return skirmisherMap; }
-            set { skirmisherMap = value; }
-        }
+        public string SkirmisherMap { get; set; }
 
         [Category("Connection")]
         [DisplayName("Spring Server Address")]
         [Description("Hostname of spring server")]
-        public string SpringServerHost
-        {
-            get { return springServerHost; }
-            set { springServerHost = value; }
-        }
+        public string SpringServerHost { get; set; } = GlobalConst.LobbyServerHost;
         [Category("Connection")]
         [DisplayName("Spring Server Name")]
         [Description("Port of spring server")]
-        public int SpringServerPort
-        {
-            get { return springServerPort; }
-            set { springServerPort = value; }
-        }
-
-        public readonly Color TextColor = Color.White;
-        
-        /// <summary>
-        /// Keeps datetime of last topic change for each channel
-        /// </summary>
-        public SerializableDictionary<string, DateTime?> Topics = new SerializableDictionary<string, DateTime?>();
+        public int SpringServerPort { get; set; } = GlobalConst.LobbyServerPort;
 
 
         [Browsable(false)]
         public bool UseSafeMode { get; set; }
 
-        public Config()
-        {
-            EnableVoiceChat = true;
-            SpringServerHost = GlobalConst.LobbyServerHost;
-            springServerPort = GlobalConst.LobbyServerPort;
+
+        public object Clone() {
+            return MemberwiseClone();
         }
 
-        public static Config Load(string path)
-        {
+        [Browsable(false)]
+        public int RepoMasterRefresh { get { return 0; } }
+
+
+        [Browsable(false)]
+        public string PackageMasterUrl { get { return "http://repos.springrts.com/"; } }
+
+        private static IsolatedStorageFile GetIsolatedStorage() {
+            var isoStore = IsolatedStorageFile.GetStore(IsolatedStorageScope.User | IsolatedStorageScope.Assembly, null, null);
+            return isoStore;
+        }
+
+        public static Config Load(string path) {
             Config conf;
-            if (File.Exists(path)) {
+            if (File.Exists(path))
+            {
                 var xs = new XmlSerializer(typeof(Config));
-                try {
+                try
+                {
                     conf = (Config)xs.Deserialize(new StringReader(File.ReadAllText(path)));
                     conf.SpringServerHost = GlobalConst.LobbyServerHost;
-                    conf.springServerPort = GlobalConst.LobbyServerPort;
+                    conf.SpringServerPort = GlobalConst.LobbyServerPort;
 
                     return conf;
-                } catch (Exception ex) {
+                }
+                catch (Exception ex)
+                {
                     Trace.TraceError("Error reading config file: {0}", ex);
                 }
             }
@@ -404,9 +322,9 @@ namespace ZeroKLobby
             return conf;
         }
 
-        public void Save(string path)
-        {
-            try {
+        public void Save(string path) {
+            try
+            {
                 var cols = new StringCollection();
                 cols.AddRange(AutoJoinChannels.OfType<string>().Distinct().ToArray());
                 AutoJoinChannels = cols;
@@ -414,28 +332,11 @@ namespace ZeroKLobby
                 var sb = new StringBuilder();
                 using (var stringWriter = new StringWriter(sb)) xs.Serialize(stringWriter, this);
                 File.WriteAllText(path, sb.ToString());
-            } catch (Exception ex) {
+            }
+            catch (Exception ex)
+            {
                 Trace.TraceError("Error saving config: {0}", ex);
             }
-        }
-
-
-        public object Clone()
-        {
-            return MemberwiseClone();
-        }
-
-        [Browsable(false)]
-        public int RepoMasterRefresh
-        {
-            get { return 0; }
-        }
-
-
-        [Browsable(false)]
-        public string PackageMasterUrl
-        {
-            get { return "http://repos.springrts.com/"; }
         }
     }
 }
