992e2ebe1b9e99164b7b17aa8ba4808e4decd9bb
Licho <licho@licho.eu>
2016-04-11 12:35:53 +0200

    fix last letter of tooltip sometimes missing

--- ZeroKLobby/ToolTips/TextTooltipRenderer.cs
+++ ZeroKLobby/ToolTips/TextTooltipRenderer.cs
@@ -3,25 +3,25 @@ using System.Windows.Forms;
 
 namespace ZeroKLobby
 {
-    class TextTooltipRenderer: IToolTipRenderer
+    internal class TextTooltipRenderer: IToolTipRenderer
     {
-        string text;
+        private string text;
 
-        public void SetTextTooltipRenderer(string text)
-        {
+        public void Draw(Graphics g, Font font, Color foreColor) {
+            var size = (Size)GetSize(font);
+
+            using (var fbrush = new SolidBrush(foreColor)) g.DrawString(text, font, fbrush, new Rectangle(0, 0, size.Width, size.Height));
+        }
+
+        public Size? GetSize(Font font) {
+            var size = TextRenderer.MeasureText(text, font, new Size(300, 40), TextFormatFlags.WordBreak);
+            size.Width += 5; // silly hack for measuretext vs drawstring slight mismatch in sizes (missing last letter in tooltip)
+            size.Height += 5;
+            return size;
+        }
+
+        public void SetTextTooltipRenderer(string text) {
             this.text = text;
-        }
-
-        public void Draw(Graphics g, Font font, Color foreColor)
-        {
-          var size = (Size)GetSize(font);  
-          
-          using (var fbrush = new SolidBrush(foreColor)) g.DrawString(text, font, fbrush, new Rectangle(0,0, size.Width, size.Height));
-        }
-
-        public Size? GetSize(Font font)
-        {
-            return TextRenderer.MeasureText(text, font, new Size(300, 40), TextFormatFlags.WordBreak);
         }
     }
 }
