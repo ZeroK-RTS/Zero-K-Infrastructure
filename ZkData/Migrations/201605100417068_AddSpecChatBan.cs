65d5b02b380353bbc1b73b6172f67ae545f87e23
sprunk <gnurps@o2.pl>
2016-05-10 06:25:55 +0200

    add migration

---
+++ ZkData/Migrations/201605100417068_AddSpecChatBan.cs
@@ -0,0 +1,18 @@
+namespace ZkData.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddSpecChatBan : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Punishments", "BanSpecChat", c => c.Boolean(nullable: false));
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.Punishments", "BanSpecChat");
+        }
+    }
+}
