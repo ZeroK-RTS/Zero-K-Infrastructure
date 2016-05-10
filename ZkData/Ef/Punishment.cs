9adc41ac14b83b953889156c4d3ad0ae62775a03
sprunk <gnurps@o2.pl>
2016-05-10 07:50:03 +0200

    Revert previous

--- ZkData/Ef/Punishment.cs
+++ ZkData/Ef/Punishment.cs
@@ -18,7 +18,6 @@ namespace ZkData
         public bool BanMute { get; set; }
         public bool BanCommanders { get; set; }
         public bool BanUnlocks { get; set; }
-        public bool BanSpecChat { get; set; }
         public bool BanSite { get; set; }
         public bool BanLobby { get; set; }
         [StringLength(1000)]
