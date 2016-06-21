93ad267876faa1366e783e1d7fb4efd6c089dcc0
Licho <licho@licho.eu>
2016-06-21 22:17:00 +0200

    added !names command to show linked users from other lobby fix #1065

--- ZkLobbyServer/ChatRelay.cs
+++ ZkLobbyServer/ChatRelay.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
 using System.Threading;
 using LobbyClient;
 using ZkData;
@@ -32,7 +34,18 @@ namespace ZkLobbyServer
 
         void OnZkServerSaid(object sender, Say say)
         {
-            if (say.AllowRelay && say.Place == SayPlace.Channel && channels.Contains(say.Target)) springTas.Say(TasClient.SayPlace.Channel, say.Target, string.Format("<{0}> {1}", say.User, say.Text), say.IsEmote);
+            try
+            {
+                if (say.AllowRelay && say.Place == SayPlace.Channel && channels.Contains(say.Target))
+                {
+                    if (say.Text.StartsWith("!names")) zkServer.GhostChanSay(say.Target, string.Join(", ", springTas.JoinedChannels[say.Target].ChannelUsers), true, false);
+                    springTas.Say(TasClient.SayPlace.Channel, say.Target, string.Format("<{0}> {1}", say.User, say.Text), say.IsEmote);
+                }
+            }
+            catch (Exception ex)
+            {
+                Trace.TraceWarning("Error relaying message  {0} {1} {2} : {3}", say?.Target, say?.User, say?.Text, ex);
+            }
         }
 
         void SetupSpringTasConnection(string password)
@@ -52,16 +65,33 @@ namespace ZkLobbyServer
 
         void OnSpringTasSaid(object sender, TasSayEventArgs args)
         {
-            if (args.Place == TasSayEventArgs.Places.Channel && channels.Contains(args.Channel) && args.UserName != springTas.UserName) {
-                zkServer.GhostSay(new Say() {
-                    Place = SayPlace.Channel,
-                    Text = args.Text,
-                    IsEmote = args.IsEmote,
-                    Time = DateTime.UtcNow,
-                    Target = args.Channel,
-                    User = args.UserName,
-                    AllowRelay = false
-                });
+            try
+            {
+                if (args.Place == TasSayEventArgs.Places.Channel && channels.Contains(args.Channel) && args.UserName != springTas.UserName)
+                {
+                    if (args.Text.StartsWith("!names"))
+                    {
+                        springTas.Say(TasClient.SayPlace.Channel,
+                            args.Channel,
+                            string.Join(", ", zkServer.Rooms[args.Channel].Users.Select(x => x.Key)),
+                            true);
+                    }
+
+                    zkServer.GhostSay(new Say()
+                    {
+                        Place = SayPlace.Channel,
+                        Text = args.Text,
+                        IsEmote = args.IsEmote,
+                        Time = DateTime.UtcNow,
+                        Target = args.Channel,
+                        User = args.UserName,
+                        AllowRelay = false
+                    });
+                }
+            }
+            catch (Exception ex)
+            {
+                Trace.TraceWarning("Error relaying message  {0} : {1}", args,ex);
             }
         }
     }
