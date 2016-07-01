76b9b013fbaccd8efafc428fd5fb5ad13ef8382c
Licho <licho@licho.eu>
2016-07-01 20:12:06 +0200

    give notify bars some margin

--- ZeroKLobby/Notifications/NotifySection.Designer.cs
+++ ZeroKLobby/Notifications/NotifySection.Designer.cs
@@ -33,13 +33,11 @@
             // NotifySection
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
             this.BackColor = System.Drawing.Color.Transparent;
-            this.Margin = new System.Windows.Forms.Padding(0);
+            this.Margin = new System.Windows.Forms.Padding(5);
             this.Name = "NotifySection";
             this.Size = new System.Drawing.Size(418, 10);
             this.ResumeLayout(false);
-
         }
 
         #endregion
