ead7a0b4d199249685d1523759cd0363acdf5d1b
Licho <licho@licho.eu>
2016-08-03 23:37:34 +0200

    bury it

--- Springie/merge.cmd
+++
@@ -1,4 +0,0 @@
-cd bin
-del *.pdb
-ilmerge /v4 /out:../Springie.exe Springie.exe LobbyClient.dll MonoTorrent.dll PlasmaShared.dll PlasmaDownloader.dll Ionic.Zip.Reduced.dll Newtonsoft.Json.dll
-cd ..
