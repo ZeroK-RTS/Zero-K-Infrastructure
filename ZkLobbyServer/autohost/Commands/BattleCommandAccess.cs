1a2a1e1d3f4790ae5c3e5a669ec75a2ac5843296
Licho <licho@licho.eu>
2016-08-24 19:21:28 +0200

    better permissions explanation

--- ZkLobbyServer/autohost/Commands/BattleCommandAccess.cs
+++ ZkLobbyServer/autohost/Commands/BattleCommandAccess.cs
@@ -1,30 +1,36 @@
 ﻿using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 
 namespace ZkLobbyServer.autohost
 {
     public enum BattleCommandAccess
     {
+        [Description("At any time, by anyone, no vote needed")]
         NoCheck = 0,
 
         /// <summary>
         /// Can be executed ingame/offgame by non-spectators (vote) or admins or founder (direct)
         /// </summary>
+        [Description("At any time, by non-spectators, non-owners need a vote")]
         Anywhere = 1,
 
         /// <summary>
         /// Can be executed ingame by non-spectators (vote) or admins or founder (direct)
         /// </summary>
+        [Description("When game running, by non-spectators, non-owners need a vote")]
         Ingame = 2,
 
         /// <summary>
         /// Can be executed not-ingame by non-spectators (vote) or admins or founder (direct)
         /// </summary>
+        [Description("When game not running, by non-spectators, non-owners need a vote")]
         NotIngame = 3,
 
         /// <summary>
         /// Can be executed ingame only as a vote by non-spectators or admins or founder
         /// </summary>
+        [Description("When game running, by non-spectators, needs vote")]
         IngameVote = 4,
     }
 }
--- ZkLobbyServer/autohost/Commands/CmdHelp.cs
+++ ZkLobbyServer/autohost/Commands/CmdHelp.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using LobbyClient;
+using ZkData;
 using ZkLobbyServer.autohost;
 
 namespace ZkLobbyServer
@@ -25,9 +26,9 @@ namespace ZkLobbyServer
         {
             foreach (var grp in ServerBattle.Commands.Values.GroupBy(x => x.Access).OrderBy(x=>x.Key))
             {
-                await battle.Respond(e, $"--- {grp.Key} ---");
+                await battle.Respond(e, $"=== {grp.Key} {grp.Key.Description()} ===");
                 foreach (var com in grp.OrderBy(x=>x.Shortcut)) await battle.Respond(e, $"!{com.Shortcut} {com.Help}");
-                await battle.Respond(e, $"--- {grp.Key} ---");
+                await battle.Respond(e, $"===");
             }
         }
     }
