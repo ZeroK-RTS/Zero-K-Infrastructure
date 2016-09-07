15622b0cb32fe2413b9d2d2af823ad4505c9d182
Licho <licho@licho.eu>
2016-09-07 13:10:12 +0200

    give chobbyla some style

--- ChobbyLauncher/Program.cs
+++ ChobbyLauncher/Program.cs
@@ -1,59 +1,47 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
-using System.IO.Compression;
 using System.Linq;
-using System.Threading;
-using System.Threading.Tasks;
 using System.Windows.Forms;
-using Neo.IronLua;
-using PlasmaDownloader;
 using ZkData;
 
 namespace ChobbyLauncher
 {
-    static class Program
+    internal static class Program
     {
         /// <summary>
-        /// The main entry point for the application.
+        ///     The main entry point for the application.
         /// </summary>
-        
-        static void Main(string[] args)
+        [STAThread]
+        private static void Main(string[] args)
         {
-            var rootPath = Path.GetFullPath(Application.ExecutablePath);
+            Trace.Listeners.Add(new ConsoleTraceListener());
 
-            var startupPath = Path.GetDirectoryName(rootPath);
-            var paths = new SpringPaths(startupPath, false);
-            var down = new PlasmaDownloader.PlasmaDownloader(new SpringScanner(paths), paths);
-
-            var tag = "chobby:test";
-
-            var chd = down.GetResource(DownloadType.MOD, tag);
-            chd?.WaitHandle.WaitOne();
-
-            string engineVersion = null;
-
-            var ver = down.PackageDownloader.GetByTag(tag);
-            if (ver != null)
+            string chobbyTag = null;
+            if (args.Length > 0)
             {
-                var mi = ver.ReadFile(paths, "modinfo.lua");
-                var lua = new Lua();
-                var luaEnv = lua.CreateEnvironment();
-                dynamic result = luaEnv.DoChunk(new StreamReader(mi), "dummy.lua");
-                engineVersion = result.engine;
+                if (args[0] == "--help" || args[0] == "-h" || args[0] == "/?")
+                {
+                    MessageBox.Show("chobby.exe [rapid_tag] \n\nUse chobby:stable or chobby:test");
+                }
+
+                chobbyTag = args[0];
             }
 
-            
-            var eng = down.GetResource(DownloadType.ENGINE, engineVersion);
-            eng?.WaitHandle.WaitOne();
+            var startupPath = Path.GetDirectoryName(Path.GetFullPath(Application.ExecutablePath));
+            if (!SpringPaths.IsDirectoryWritable(startupPath))
+            {
+                MessageBox.Show("Please move this program to a writable folder");
+                return;
+            }
 
-            var chobbyla = new Chobbyla();
-            chobbyla.LaunchChobby(paths, ver.InternalName, engineVersion);
+            var paths = new SpringPaths(startupPath, false);
 
-        //    Application.EnableVisualStyles();
-            //Application.SetCompatibleTextRenderingDefault(false);
-            //Application.Run(new Form1());
+            Application.EnableVisualStyles();
 
+            var cf = new ChobbylaForm(chobbyTag, paths) { StartPosition = FormStartPosition.CenterScreen };
+            cf.ShowDialog();
         }
     }
 }
