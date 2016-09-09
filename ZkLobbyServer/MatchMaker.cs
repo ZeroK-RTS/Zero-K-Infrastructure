7596752efa2283621dcaadcb2a7aa9df3390e512
Licho <licho@licho.eu>
2016-09-09 23:11:05 +0200

    mm class

--- ZkLobbyServer/MatchMaker.cs
+++ ZkLobbyServer/MatchMaker.cs
@@ -3,10 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using LobbyClient;
+using ZkData;
 
 namespace ZkLobbyServer
 {
     public class MatchMaker
     {
+        ZkLobbyServer server;
+
+
+        List<MatchMakerSetup.Queue> QueueTypes = new List<MatchMakerSetup.Queue>();
+
+
+
+        public MatchMaker(ZkLobbyServer server)
+        {
+            this.server = server;
+            var db = new ZkDataContext();
+        }
     }
 }
