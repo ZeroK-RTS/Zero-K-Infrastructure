4ae6a964fa4125a9e7d298f8732667fdb348d5e7
Licho <licho@licho.eu>
2016-10-22 13:26:36 +0200

    go back to JsonNET serializer (from NETJson chinnese variant - failed to serialize enums as properties)

--- Shared/PlasmaShared/CommandJsonSerializer.cs
+++ Shared/PlasmaShared/CommandJsonSerializer.cs
@@ -1,17 +1,15 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using NetJSON;
 using Newtonsoft.Json;
-
 
 namespace PlasmaShared
 {
- 
     public class CommandJsonSerializer
     {
-        readonly Dictionary<string, Type> knownTypes = new Dictionary<string, Type>();
-        NetJSONSettings ns = new NetJSONSettings() {DateFormat = NetJSONDateFormat.ISO , UseStringOptimization = true};
+        private readonly Dictionary<string, Type> knownTypes = new Dictionary<string, Type>();
+        //   NetJSONSettings ns = new NetJSONSettings() {DateFormat = NetJSONDateFormat.ISO, UseEnumString = false};
+        private JsonSerializerSettings settings = new JsonSerializerSettings() { Formatting = Formatting.None, NullValueHandling = NullValueHandling.Ignore};
 
         public CommandJsonSerializer(IEnumerable<Type> types)
         {
@@ -22,27 +20,26 @@ namespace PlasmaShared
 
         public object DeserializeLine(string line)
         {
-            if (!string.IsNullOrEmpty(line)) {
-                string[] parts = line.Split(new[] { " " }, 2, StringSplitOptions.RemoveEmptyEntries);
+            if (!string.IsNullOrEmpty(line))
+            {
+                var parts = line.Split(new[] { " " }, 2, StringSplitOptions.RemoveEmptyEntries);
                 if (parts.Length != 2) throw new Exception(string.Format("Invalid json data {0} : {1}", this, line));
                 Type type;
                 if (!knownTypes.TryGetValue(parts[0], out type)) throw new Exception(string.Format("Invalid json type {0} : {1}", this, parts[0]));
-                return NetJSON.NetJSON.Deserialize(type, parts[1]) ?? type.GetConstructor(new Type[] { })?.Invoke(null);
+                return JsonConvert.DeserializeObject(parts[1], type, settings) ?? type.GetConstructor(new Type[] { })?.Invoke(null);
             }
             return null;
         }
 
         public void RegisterTypes(params Type[] types)
         {
-            foreach (Type t in types) knownTypes[t.Name] = t;
+            foreach (var t in types) knownTypes[t.Name] = t;
         }
 
         public string SerializeToLine<T>(T value)
         {
-            var send = $"{typeof(T).Name} {NetJSON.NetJSON.Serialize(value, ns)}\n";
+            var send = $"{typeof(T).Name} {JsonConvert.SerializeObject(value, settings)}\n";
             return send;
         }
     }
-
-   
 }
