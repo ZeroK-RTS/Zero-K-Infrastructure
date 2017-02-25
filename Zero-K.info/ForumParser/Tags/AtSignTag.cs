b4918641d3f86f4e1ed6e8e94099551116fe8289
Licho <licho@licho.eu>
2017-02-25 11:56:06 +0100

    fix @ punctuation devouring fix #1402

--- Zero-K.info/ForumParser/Tags/AtSignTag.cs
+++ Zero-K.info/ForumParser/Tags/AtSignTag.cs
@@ -15,41 +15,57 @@ namespace ZeroKWeb.ForumParser
             if (!(self.Previous?.Value is LiteralTag)) // previous is not a continuous text
             {
                 var ender = self.Next.FirstNode(x => !(x.Value is LiteralTag || x.Value is UnderscoreTag));
-                var val = self.Next.GetOriginalContentUntilNode(ender); // get next string
+                var text = self.Next.GetOriginalContentUntilNode(ender); // get next string
 
-                if (!string.IsNullOrEmpty(val))
+                if (!string.IsNullOrEmpty(text))
                 {
-                    val = Utils.StripInvalidLobbyNameChars(val.Trim());
-                    var db = new ZkDataContext();
+                    int idx;
+                    for (idx = 0; idx < text.Length; idx++) if (!Utils.ValidLobbyNameCharacter(text[idx])) break;
+                    string remainder = null;
+                    string val = text;
+                    if (idx != 0 && idx < text.Length)
+                    {
+                        remainder = text.Substring(idx, text.Length - idx);
+                        val = text.Substring(0, idx);
+                    }
+                    
+                    if (!string.IsNullOrEmpty(val))
+                    {
+                        var db = new ZkDataContext();
 
-                    var acc = Account.AccountByName(db, val);
-                    if (acc != null)
-                    {
-                        context.Append(context.Html.PrintAccount(acc));
-                        return ender;
-                    }
-                    var clan = db.Clans.FirstOrDefault(x => x.Shortcut == val);
-                    if (clan != null)
-                    {
-                        context.Append(context.Html.PrintClan(clan));
-                        return ender;
-                    }
-                    var fac = db.Factions.FirstOrDefault(x => x.Shortcut == val);
-                    if (fac != null)
-                    {
-                        context.Append(context.Html.PrintFaction(fac, false));
-                        return ender;
-                    }
-                    if (val.StartsWith("b", StringComparison.InvariantCultureIgnoreCase))
-                    {
-                        var bid = 0;
-                        if (int.TryParse(val.Substring(1), out bid))
+                        var acc = Account.AccountByName(db, val);
+                        if (acc != null)
                         {
-                            var bat = db.SpringBattles.FirstOrDefault(x => x.SpringBattleID == bid);
-                            if (bat != null)
+                            context.Append(context.Html.PrintAccount(acc));
+                            context.Append(remainder);
+                            return ender;
+                        }
+                        var clan = db.Clans.FirstOrDefault(x => x.Shortcut == val);
+                        if (clan != null)
+                        {
+                            context.Append(context.Html.PrintClan(clan));
+                            context.Append(remainder);
+                            return ender;
+                        }
+                        var fac = db.Factions.FirstOrDefault(x => x.Shortcut == val);
+                        if (fac != null)
+                        {
+                            context.Append(context.Html.PrintFaction(fac, false));
+                            context.Append(remainder);
+                            return ender;
+                        }
+                        if (val.StartsWith("b", StringComparison.InvariantCultureIgnoreCase))
+                        {
+                            var bid = 0;
+                            if (int.TryParse(val.Substring(1), out bid))
                             {
-                                context.Append(context.Html.PrintBattle(bat));
-                                return ender;
+                                var bat = db.SpringBattles.FirstOrDefault(x => x.SpringBattleID == bid);
+                                if (bat != null)
+                                {
+                                    context.Append(context.Html.PrintBattle(bat));
+                                    context.Append(remainder);
+                                    return ender;
+                                }
                             }
                         }
                     }
