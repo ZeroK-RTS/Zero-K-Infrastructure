a6a5913c2b83060629c3a049fe6f5ed03b3e93ee
sprunk <gnurps@o2.pl>
2017-02-28 03:26:41 +0100

    fix compile

--- ZkLobbyServer/autohost/Commands/CmdGame.cs
+++ ZkLobbyServer/autohost/Commands/CmdGame.cs
@@ -1,8 +1,9 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using LobbyClient;
+using PlasmaShared;
 using ZeroKWeb.SpringieInterface;
 using ZkData;
 
