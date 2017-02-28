d7392e22eb1da27b4280a4d9ee12b033897754ff
sprunk <gnurps@o2.pl>
2017-02-28 03:20:09 +0100

    ditto, !forcegame

--- ZkLobbyServer/autohost/Commands/CmdForceGame.cs
+++ ZkLobbyServer/autohost/Commands/CmdForceGame.cs
@@ -19,9 +19,9 @@ namespace ZkLobbyServer
         {
             this.gameName = arguments;
 
-            if (!battle.IsPassworded)
+            if (battle.Mode != AutohostMode.None)
             {
-                battle.Respond(e, $"You can only do this on private, passworded hosts.");
+                battle.Respond(e, $"You can only do this on private hosts.");
                 return null;
             }
 
