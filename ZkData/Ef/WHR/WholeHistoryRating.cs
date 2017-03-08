598f512611b460f6269d9e38e7feee5dbfa9b121
DeinFreund <deinbestrfreund@gmail.com>
2017-03-08 12:15:43 +0100

    less spam

--- ZkData/Ef/WHR/WholeHistoryRating.cs
+++ ZkData/Ef/WHR/WholeHistoryRating.cs
@@ -56,15 +56,15 @@ namespace Ratings
 
         public void ProcessBattle(SpringBattle battle)
         {
-            if (++battlesRegistered % 1000 == 0)
-            {
-                Trace.TraceInformation(battlesRegistered + " battles registered for WHR");
-            }
             latestBattle = battle;
             ICollection<int> winners = battle.SpringBattlePlayers.Where(p => p.IsInVictoryTeam).Select(p => p.AccountID).ToList();
             ICollection<int> losers = battle.SpringBattlePlayers.Where(p => !p.IsInVictoryTeam).Select(p => p.AccountID).ToList();
             if (winners.Count > 0 && losers.Count > 0)
             {
+                if (RatingSystems.Initialized)
+                {
+                    Trace.TraceInformation(battlesRegistered + " battles registered for WHR");
+                }
                 createGame(losers, winners, false, ConvertDate(battle.StartTime));
             }
         }
@@ -94,7 +94,7 @@ namespace Ratings
                 if (lastUpdate == null)
                 {
                     updateAction = (() => {
-                        Trace.TraceInformation("Initializing all WHR ratings, this will take some time..");
+                        Trace.TraceInformation("Initializing WHR ratings for " + battlesRegistered + " battles, this will take some time..");
                         runIterations(50);
                     });
                 }
