81d898cd5439a620aee320c211a16ebf8d82b91a
DeinFreund <deinbestrfreund@gmail.com>
2018-01-12 02:30:24 +0100

    add another opengl error message, redirect linux users to our existing linux graphics driver guide. fix #1784

--- ChobbyLauncher/Program.cs
+++ ChobbyLauncher/Program.cs
@@ -159,16 +159,30 @@ namespace ChobbyLauncher
             var syncError = CrashReportHelper.IsDesyncMessage(logStr);
             if (syncError) Trace.TraceWarning("Sync error detected");
 
-                        var openGlFail = logStr.Contains("No OpenGL drivers installed.") ||
-                            logStr.Contains("Please go to your GPU vendor's website and download their drivers.") ||
-                            logStr.Contains("minimum required OpenGL version not supported, aborting") ||
-                            logStr.Contains("Update your graphic-card driver!");
+            var openGlFail = logStr.Contains("No OpenGL drivers installed.") ||
+                logStr.Contains("This stack trace indicates a problem with your graphic card driver") ||
+                logStr.Contains("Please go to your GPU vendor's website and download their drivers.") ||
+                logStr.Contains("minimum required OpenGL version not supported, aborting") ||
+                logStr.Contains("Update your graphic-card driver!");
             
 
             if (openGlFail)
             {
                 Trace.TraceWarning("Outdated OpenGL detected");
-                MessageBox.Show("You have outdated graphics card drivers!\r\nPlease try finding ones for your graphics card and updating them.", "Outdate graphics card driver detected", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+
+                if (Environment.OSVersion.Platform == PlatformID.Unix)
+                {
+                    if (MessageBox.Show("You have outdated graphics card drivers!\r\nPlease try finding ones for your graphics card and updating them. \r\n\r\nWould you like to see our Linux graphics driver guide?", "Outdated graphics card driver detected", MessageBoxButtons.YesNo, MessageBoxIcon.Warning) == DialogResult.Yes)
+                    {
+                        Process.Start("http://zero-k.info/mediawiki/index.php?title=Optimized_Graphics_Linux");
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("You have outdated graphics card drivers!\r\nPlease try finding ones for your graphics card and updating them.", "Outdated graphics card driver detected", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+
+                    
             }
 
             if ((!springRunOk && !openGlFail) || syncError) // crash has occured
