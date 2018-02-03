77be3bd5b2fb5ad9e192457fdeaf338d15e84c9c
Licho <licho@licho.eu>
2018-02-03 13:19:34 +0100

    adding batles

--- Zero-K.info/Controllers/TourneyController.cs
+++ Zero-K.info/Controllers/TourneyController.cs
@@ -10,12 +10,20 @@ namespace ZeroKWeb.Controllers
 {
     public class TourneyController : Controller
     {
+        public class TourneyModel
+        {
+            public List<TourneyBattle> Battles;
+            public List<int> Team1Ids { get; set; } = new List<int>();
+            public List<int> Team2Ids { get; set; } = new List<int>();
+            public string Title { get; set; }
+        }
+
         // GET: Tourney
         public ActionResult Index()
         {
             var tourneyBattles = Global.Server.Battles.Values.Where(x => x != null).OfType<TourneyBattle>().ToList();
 
-            return View("TourneyIndex", tourneyBattles);
+            return View("TourneyIndex", new TourneyModel() {Battles = tourneyBattles});
         }
 
         public ActionResult JoinBattle(string battleHost)
@@ -40,6 +48,25 @@ namespace ZeroKWeb.Controllers
                 {
                     Global.Server.ForceJoinBattle(p, bat);
                 }
+            }
+            return RedirectToAction("Index");
+        }
+
+        [Auth(Role = AdminLevel.Moderator)]
+        public ActionResult AddBattle(TourneyModel model)
+        {
+            var db = new ZkDataContext();
+            {
+                var tb = new TourneyBattle(Global.Server, new TourneyBattle.TourneyPrototype()
+                {
+                    Title = model.Title,
+                    TeamPlayers = new List<List<string>>()
+                    {
+                        model.Team1Ids.Select(x=> db.Accounts.Find(x)?.Name).Where(x=>x!=null).ToList(),
+                        model.Team2Ids.Select(x=> db.Accounts.Find(x)?.Name).Where(x=>x!=null).ToList()
+                    }
+                });
+                Global.Server.AddBattle(tb);
             }
             return RedirectToAction("Index");
         }
