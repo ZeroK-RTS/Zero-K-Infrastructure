451bf0800afdf1236f6b7df370eb8fad9c4f53a1
Licho <licho@licho.eu>
2018-03-11 11:16:40 +0100

    manually cancelling treaty gives guarantee metal to other faction for issue #2064

--- Zero-K.info/Controllers/FactionsController.cs
+++ Zero-K.info/Controllers/FactionsController.cs
@@ -190,6 +190,18 @@ namespace ZeroKWeb.Controllers
             if (treaty.CanCancel(acc)) {
                 treaty.TreatyState = TreatyState.Invalid;
                 db.Events.InsertOnSubmit(PlanetwarsEventCreator.CreateEvent("Treaty {0} between {1} and {2} cancelled by {3}", treaty, treaty.FactionByProposingFactionID, treaty.FactionByAcceptingFactionID, acc));
+
+                if (acc.FactionID == treaty.AcceptingFactionID)
+                {
+                    treaty.FactionByProposingFactionID.ProduceMetal(treaty.AcceptingFactionGuarantee ?? 0);
+                    treaty.FactionByProposingFactionID.ProduceMetal(treaty.ProposingFactionGuarantee ?? 0);
+                }
+                else
+                {
+                    treaty.FactionByAcceptingFactionID.ProduceMetal(treaty.AcceptingFactionGuarantee ?? 0);
+                    treaty.FactionByAcceptingFactionID.ProduceMetal(treaty.ProposingFactionGuarantee ?? 0);
+                }
+
                 db.SaveChanges();
 
                 return RedirectToAction("Detail", new { id = Global.FactionID });
