194d3b6e798d8b138327927a4ac78fede8c370d8
Anarchid <anarchid@mail.ru>
2018-04-14 21:32:11 +0300

    fix launcher scripts, pass arguments to wrapper

--- SteamRoot/Zero-K_linux32.sh
+++ SteamRoot/Zero-K_linux32.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-ln -fs /lib/x86_64-linux-gnu/libc.so.6 libc.so
+ln -fs /lib/i386-linux-gnu/libc.so.6 linux32/libc.so
 
-export LD_LIBRARY_PATH=".:$LD_LIBRARY_PATH"
-./Zero-K_linux64 
+export LD_LIBRARY_PATH="./linux32:$LD_LIBRARY_PATH"
+./Zero-K_linux32 $@
