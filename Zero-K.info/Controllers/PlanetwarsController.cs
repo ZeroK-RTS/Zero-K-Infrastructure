4f83a1f166c02ddd55c9c24f1af89e7393f34921
sprunk <gnurps@o2.pl>
2018-04-22 00:06:19 +0200

    Fix rush not announced in faction channel

--- Zero-K.info/Controllers/PlanetwarsController.cs
+++ Zero-K.info/Controllers/PlanetwarsController.cs
@@ -831,9 +831,10 @@ namespace ZeroKWeb.Controllers
                 var acc = db.Accounts.Single(x => x.AccountID == Global.AccountID);
                 var structure = planet.PlanetStructures.Single(x => x.StructureTypeID == structureTypeID);
                 if (structure.RushStructure(acc))
-                    db.Events.InsertOnSubmit(PlanetwarsEventCreator.CreateEvent("{0} has rushed activation of {1} on {2}.",
+                    db.Events.InsertOnSubmit(PlanetwarsEventCreator.CreateEvent("{0} has rushed activation of {1} on {2} planet {3}.",
                         acc,
                         structure.StructureType,
+                        planet.Faction,
                         planet));
                 else return Content("You cannot rush this");
 
