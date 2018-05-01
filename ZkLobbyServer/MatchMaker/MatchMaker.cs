6adb44e4776402b67a620eb1d948c8d3afbaf1e0
Licho <licho@licho.eu>
2018-05-01 22:46:58 +0200

    safety first, low ban times again

--- ZkLobbyServer/MatchMaker/MatchMaker.cs
+++ ZkLobbyServer/MatchMaker/MatchMaker.cs
@@ -16,9 +16,9 @@ namespace ZkLobbyServer
         private const int TimerSeconds = 30;
         private const int MapModChangePauseSeconds = 30;
 
-        private const int BanSecondsIncrease = 30;
-        private const int BanSecondsMax = 300;
-        private const int BanReset = 600;
+        private const int BanSecondsIncrease = 1;
+        private const int BanSecondsMax = 15;
+        private const int BanReset = 60;
 
 
         private struct QueueConfig
