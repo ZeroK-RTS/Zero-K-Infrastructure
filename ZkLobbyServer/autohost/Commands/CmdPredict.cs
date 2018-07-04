7e8dcfbbef0c8ae68972ad0c6c3cc502e0ffe02d
DeinFreund <deinbestrfreund@gmail.com>
2018-07-04 18:24:31 +0200

    name teams in !predict  by the highest rated player

--- ZkLobbyServer/autohost/Commands/CmdPredict.cs
+++ ZkLobbyServer/autohost/Commands/CmdPredict.cs
@@ -76,7 +76,7 @@ namespace ZkLobbyServer
                 var chances = RatingSystems.GetRatingSystem(cat).PredictOutcome(teams, DateTime.UtcNow);
                 for (int i = 0; i < teams.Count; i++)
                 {
-                    await battle.SayBattle( $"Team {teams[i].First().Name} has a {Math.Round(1000 * chances[i]) / 10}% chance to win");
+                    await battle.SayBattle( $"Team {teams[i].OrderByDescending(x => x.GetRating(cat).Elo).First().Name} has a {Math.Round(1000 * chances[i]) / 10}% chance to win");
                 }
             }
         }
