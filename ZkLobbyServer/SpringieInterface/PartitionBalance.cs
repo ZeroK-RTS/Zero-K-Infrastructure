7d0d4d60961ab7332113e9cd6e696593559c7245
DeinFreund <deinbestrfreund@gmail.com>
2018-07-21 13:10:47 +0200

    add fallback and detailed comments to PartitionBalance

--- ZkLobbyServer/SpringieInterface/PartitionBalance.cs
+++ ZkLobbyServer/SpringieInterface/PartitionBalance.cs
@@ -50,20 +50,24 @@ namespace ZeroKWeb.SpringieInterface
             UInt64 all = (1UL << nums.Count) - 1;
             UInt64 prevI = 0;
             UInt64 change;
+
+            //Iterate over all 2^N subsets. Each subset is identified by i. Each bit represents one number of the set.
             for (UInt64 i = 0; i < (1UL << (nums.Count - 1)); i++)
             {
-                change = (i ^ prevI);
+                change = (i ^ prevI); 
                 int j = 0;
+
+                //Iterate changed bits since last iteration
                 while (change > 0)
                 {
                     if ((change & 0x1) != 0)
                     {
-                        if ((prevI & 0x1) == 0)
+                        if ((prevI & 0x1) == 0) // 0 -> 1
                         {
                             sum1 += nums[j];
                             sum2 -= nums[j];
                         }
-                        else
+                        else // 1 -> 0
                         {
                             sum2 += nums[j];
                             sum1 -= nums[j];
@@ -80,15 +84,17 @@ namespace ZeroKWeb.SpringieInterface
             return ret;
         }
 
-
+        //Balances N players in O(2^(n/2))
+        //Algorithm described in http://web.cecs.pdx.edu/~bart/cs510ai/papers/korf-ckk.pdf Section 2.2
         public static DualBalanceResult Balance(BalanceMode mode, ICollection<PlayerItem> players)
         {
             List<PlayerItem> unmodifiedPlayers = players.ToList();
             List<List<int>> playerGroups = new List<List<int>>();
             List<double> groupSkill = new List<double>();
-            players.ForEach(x => x.Elo += 1e6); //Purple for everyone
+            players.ForEach(x => x.Elo += 1e6); //Purple for everyone, this assures that both teams get an equal number of players
             if (players.Count % 2 != 0)
             {
+                //Pad teams to an even number
                 players.Add(new PlayerItem(-1, players.Average(x => x.Elo), null, null));
             }
             int maxTeamSize = players.Count / 2;
@@ -137,14 +143,16 @@ namespace ZeroKWeb.SpringieInterface
             maxTeamSize = playerGroups.Count / 2;
             List<Subset> firstList = generateAllSubsets(groupSkill.Where((x, i) => i < maxTeamSize).ToList());
             List<Subset> secondList = generateAllSubsets(groupSkill.Where((x, i) => i >= maxTeamSize).ToList());
+            //Any partition can be represented as a combination of a partition of the first and the second half
+            //There are N^2 possibilities, but we only look at the N ones where the sum of both is as close to the target as possible
 
             firstList.Sort();
             secondList.Sort();
             
-            int a = 0;
-            int b = secondList.Count - 1;
+            int a = 0; //Iterates first list in ascending order
+            int b = secondList.Count - 1; //Iterates second list in descending order
             double bestDiff = Double.MaxValue;
-            UInt64 best = 0;
+            UInt64 bestAssignment = 0; //Each bit from right to left represents whether the ith player is in the first or second team
 
             while (a < firstList.Count && b >= 0)
             {
@@ -155,7 +163,7 @@ namespace ZeroKWeb.SpringieInterface
                 if (b >= 0 && sum - 2 * (firstList[a].Sum + secondList[b].Sum) < bestDiff)
                 {
                     bestDiff = sum - 2 * (firstList[a].Sum + secondList[b].Sum);
-                    best = firstList[a].Elements | (secondList[b].Elements << (maxTeamSize));
+                    bestAssignment = firstList[a].Elements | (secondList[b].Elements << (maxTeamSize));
                 }
                 a++;
             }
@@ -182,7 +190,8 @@ namespace ZeroKWeb.SpringieInterface
             int j = 0;
             while (j < playerGroups.Count)
             {
-                if ((best & 0x1) == 0)
+                //iterate through assignment bit by bit
+                if ((bestAssignment & 0x1) == 0)
                 {
                     ret.Players.Where(p => playerGroups[j].Contains(p.LobbyID)).ForEach(x => x.AllyID = 0);
                 }
@@ -191,7 +200,7 @@ namespace ZeroKWeb.SpringieInterface
                     ret.Players.Where(p => playerGroups[j].Contains(p.LobbyID)).ForEach(x => x.AllyID = 1);
                 }
                 j++;
-                best >>= 1;
+                bestAssignment >>= 1;
             }
             
 
@@ -211,49 +220,57 @@ namespace ZeroKWeb.SpringieInterface
             return ret;
         }
 
+        //Interface function to comply with LegacyBalance
         public static BalanceTeamsResult BalanceInterface(int teamCount, BalanceMode mode, LobbyHostingContext b, params List<Account>[] unmovablePlayers)
         {
             if (teamCount != 2)
             {
-                Trace.TraceWarning("DualBalance called with invalid number of teams: " + teamCount);
+                Trace.TraceWarning("PartitionBalance called with invalid number of teams: " + teamCount);
                 return new Balancer().LegacyBalance(teamCount, mode, b, unmovablePlayers);
             }
             if (unmovablePlayers.Length > 0)
             {
-                Trace.TraceWarning("DualBalance called with too many unmovable players: " + unmovablePlayers.Length);
+                Trace.TraceWarning("PartitionBalance called with too many unmovable players: " + unmovablePlayers.Length);
                 return new Balancer().LegacyBalance(teamCount, mode, b, unmovablePlayers);
             }
             if (mode == BalanceMode.FactionWise)
             {
-                Trace.TraceWarning("DualBalance called with FactionWise balance mode, which is unsupported");
+                Trace.TraceWarning("PartitionBalance called with FactionWise balance mode, which is unsupported");
                 return new Balancer().LegacyBalance(teamCount, mode, b, unmovablePlayers);
             }
-
-            if (b.IsMatchMakerGame) mode = BalanceMode.Party;
-
-            BalanceTeamsResult ret = new BalanceTeamsResult();
-
-            ret.CanStart = true;
-            ret.Players = b.Players.ToList();
-            ret.Bots = b.Bots.ToList();
-
-            List<PlayerItem> players = new List<PlayerItem>();
-            using (var db = new ZkDataContext())
+            try
             {
-                var nonSpecList = b.Players.Where(y => !y.IsSpectator).Select(y => (int?)y.LobbyID).ToList();
-                var accs = db.Accounts.Where(x => nonSpecList.Contains(x.AccountID)).ToList();
-                if (accs.Count < 1)
-                {
-                    ret.CanStart = false;
-                    return ret;
-                }
-                players = b.Players.Where(y => !y.IsSpectator).Select(x => new PlayerItem(x.LobbyID, accs.First(a => a.AccountID == x.LobbyID).GetRating(b.ApplicableRating).Elo, x.Clan, x.PartyID)).ToList();
-            }
+                if (b.IsMatchMakerGame) mode = BalanceMode.Party;
 
-            var dualResult = Balance(mode, players);
-            dualResult.Players.ForEach(r => ret.Players.Where(x => x.LobbyID == r.LobbyID).ForEach(x => x.AllyID = r.AllyID));
-            ret.Message = dualResult.Message;
-            return ret;
+                BalanceTeamsResult ret = new BalanceTeamsResult();
+
+                ret.CanStart = true;
+                ret.Players = b.Players.ToList();
+                ret.Bots = b.Bots.ToList();
+
+                List<PlayerItem> players = new List<PlayerItem>();
+                using (var db = new ZkDataContext())
+                {
+                    var nonSpecList = b.Players.Where(y => !y.IsSpectator).Select(y => (int?)y.LobbyID).ToList();
+                    var accs = db.Accounts.Where(x => nonSpecList.Contains(x.AccountID)).ToList();
+                    if (accs.Count < 1)
+                    {
+                        ret.CanStart = false;
+                        return ret;
+                    }
+                    players = b.Players.Where(y => !y.IsSpectator).Select(x => new PlayerItem(x.LobbyID, accs.First(a => a.AccountID == x.LobbyID).GetRating(b.ApplicableRating).Elo, x.Clan, x.PartyID)).ToList();
+                }
+
+                var dualResult = Balance(mode, players);
+                dualResult.Players.ForEach(r => ret.Players.Where(x => x.LobbyID == r.LobbyID).ForEach(x => x.AllyID = r.AllyID));
+                ret.Message = dualResult.Message;
+                return ret;
+            }
+            catch (Exception ex)
+            {
+                Trace.TraceWarning("PartitionBalance encountered an error: " + ex);
+                return new Balancer().LegacyBalance(teamCount, mode, b, unmovablePlayers);
+            }
         }
     }
 
