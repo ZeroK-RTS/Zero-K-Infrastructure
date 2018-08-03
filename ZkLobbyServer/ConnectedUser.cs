eaaaad9a83eec05e7973f77e539b6e0605117880
DeinFreund <deinbestrfreund@gmail.com>
2018-08-03 10:32:08 +0200

    Explain that somebody can't play in a battle.

--- ZkLobbyServer/ConnectedUser.cs
+++ ZkLobbyServer/ConnectedUser.cs
@@ -423,7 +423,10 @@ namespace ZkLobbyServer
                 if (bat.Users.TryGetValue(status.Name, out ubs))
                 {
                     // enfoce player count limit
-                    if (status.IsSpectator == false && !bat.CanUserPlay(this)) status.IsSpectator = true;
+                    if (status.IsSpectator == false && ubs.IsSpectator == true && !bat.CanUserPlay(this)) {
+                        status.IsSpectator = true;
+                        await Respond("You do not fulfill the minimum requirements to play in this battle.");
+                    }
 
                     ubs.UpdateWith(status);
                     bat.ValidateBattleStatus(ubs);
