ZeroK-RTS/Zero-K-Infrastructure

commit cbe8edd91ac53f4212969adf348d6f9639c844b1

exact map search match no longer overrides sorting by support level

ZkLobbyServer/autohost/MapPicker.cs
@@ -146,13 +146,8 @@ namespace ZeroKWeb.SpringieInterface
         {
             var db = new ZkDataContext();
             
-
             var ret = db.Resources.AsQueryable();
             ret = ret.Where(x => x.TypeID == type && x.MapSupportLevel >= minimumSupportLevel);
-            
-            var test = ret.Where(x => x.RapidTag == term || x.InternalName == term);
-            if (test.Any()) return test.OrderByDescending(x => x.MapSupportLevel).ThenByDescending(x=>x.ResourceID);
-
 
             int i;
             var words = term.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
@@ -165,7 +160,8 @@ namespace ZeroKWeb.SpringieInterface
                     ret = ret.Where(x => SqlFunctions.PatIndex("%" + w1 + "%", x.InternalName) > 0);
                 }
             }
-            return ret.OrderByDescending(x => x.MapSupportLevel);
+
+            return ret.OrderByDescending(x => x.MapSupportLevel).ThenByDescending(x=>x.InternalName == term || x.RapidTag == term).ThenByDescending(x=>x.ResourceID);
         }
     }
 }

commit 6608aaf9d62ff948a24978dd32ef40113a5313dc

fix #2313, cbe8edd caused it to be impossible for a battle to use maps (even when forced) that have similar sounding versions with higher supportlevel

Zero-K.info/Views/Maps/Detail.cshtml
@@ -78,7 +78,7 @@
         </table>
     }
 	
-    <h3 class="textbutton"><a href="@Html.PrintSpringLink("chat/battle@select_map:"+ m.InternalName)">PLAY ON THIS MAP</a></h3><br />
+    <h3 class="textbutton"><a href="@Html.PrintSpringLink("chat/battle@select_map:"+ m.ResourceID)">PLAY ON THIS MAP</a></h3><br />
     Downloads: @m.DownloadCount<br />
     @if (!ZeroKWeb.Global.IsLobbyAccess) {
         <div style="font-size: small">

ZkLobbyServer/autohost/Commands/CmdMap.cs
@@ -20,7 +20,7 @@ namespace ZkLobbyServer
         {
             map = string.IsNullOrEmpty(arguments)
                 ? MapPicker.GetRecommendedMap(battle.GetContext(), (battle.MinimalMapSupportLevel > MapSupportLevel.Featured) ? battle.MinimalMapSupportLevel : MapSupportLevel.Featured)
-                : MapPicker.FindResources(ResourceType.Map, arguments, battle.MinimalMapSupportLevel).FirstOrDefault();
+                : MapPicker.FindResources(ResourceType.Map, arguments, battle.MinimalMapSupportLevel, true).FirstOrDefault();
 
 
             if (map == null)
@@ -50,7 +50,7 @@ namespace ZkLobbyServer
             }
             else if (!string.IsNullOrEmpty(arguments) && map.MapSupportLevel < MapSupportLevel.Supported)
             {
-                alternativeMap = MapPicker.FindResources(ResourceType.Map, arguments, MapSupportLevel.Supported).FirstOrDefault();
+                alternativeMap = MapPicker.FindResources(ResourceType.Map, arguments, MapSupportLevel.Supported, true).FirstOrDefault();
             }
 
 

ZkLobbyServer/autohost/MapPicker.cs
@@ -142,12 +142,15 @@ namespace ZeroKWeb.SpringieInterface
             return FindResources(type, string.Join(" ", words));
         }
 
-        public static IQueryable<Resource> FindResources(ResourceType type, string term, MapSupportLevel minimumSupportLevel = MapSupportLevel.None)
+        public static IQueryable<Resource> FindResources(ResourceType type, string term, MapSupportLevel minimumSupportLevel = MapSupportLevel.None, bool ignoreExactMatches = false)
         {
             var db = new ZkDataContext();
             
             var ret = db.Resources.AsQueryable();
             ret = ret.Where(x => x.TypeID == type && x.MapSupportLevel >= minimumSupportLevel);
+            
+            var test = ret.Where(x => x.RapidTag == term || x.InternalName == term);
+            if (test.Any() && !ignoreExactMatches) return test.OrderByDescending(x => x.MapSupportLevel).ThenByDescending(x => x.ResourceID);
 
             int i;
             var words = term.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);