10b71f296add7798b1fd9619e4cc540abfce5d04
DeinFreund <deinbestrfreund@gmail.com>
2019-03-07 14:54:52 +0100

    wait for 10 seconds with starting when people are afk or unsynced, fix #2487

--- ZkLobbyServer/autohost/Commands/CmdStart.cs
+++ ZkLobbyServer/autohost/Commands/CmdStart.cs
@@ -1,5 +1,6 @@
 ﻿using System.Linq;
 using System.Threading.Tasks;
+using System.Timers;
 using LobbyClient;
 using ZeroKWeb.SpringieInterface;
 using ZkData;
@@ -14,6 +15,8 @@ namespace ZkLobbyServer
 
         public override BattleCommand Create() => new CmdStart();
 
+        private Timer startTimer;
+
         public override string Arm(ServerBattle battle, Say e, string arguments = null)
         {
             battle.RunCommandDirectly<CmdRing>(e);
@@ -23,9 +26,38 @@ namespace ZkLobbyServer
 
         public override async Task ExecuteArmed(ServerBattle battle, Say e)
         {
+            var afkers = battle.Users.Values.Where(x => !x.IsSpectator && x.LobbyUser.IsAway);
+            var unready = battle.Users.Values.Where(x => !x.IsSpectator && x.SyncStatus != SyncStatuses.Synced);
+            bool wait = false;
+            if (afkers.Any())
+            {
+                wait = true;
+                await battle.SayBattle("The following users are afk and will be spectated: " + afkers.Select(x => x.Name).StringJoin());
+            }
+            if (unready.Any())
+            {
+                wait = true;
+                await battle.SayBattle("The following users are still downloading the map: " + unready.Select(x => x.Name).StringJoin());
+            }
+            if (wait)
+            {
+                await battle.SayBattle("Game starting in 10 seconds...");
+                startTimer = new Timer(10000);
+                startTimer.Enabled = true;
+                startTimer.AutoReset = false;
+                startTimer.Elapsed += (t, s) => { StartGame(battle); };
+            }
+            else
+            {
+                await StartGame(battle);
+            }
+
+        }
+
+        private async Task StartGame(ServerBattle battle)
+        {
             await battle.RunCommandDirectly<CmdSpecAfk>(null);
             await battle.StartGame();
-
         }
     }
 }
