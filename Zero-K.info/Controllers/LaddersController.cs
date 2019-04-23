e24eb5f71caab541e82831582fef80ee261f136b
DeinFreund <deinbestrfreund@gmail.com>
2019-04-23 11:23:08 +0200

    fix #2547

--- Zero-K.info/Controllers/LaddersController.cs
+++ Zero-K.info/Controllers/LaddersController.cs
@@ -95,6 +95,8 @@ namespace ZeroKWeb.Controllers
             if (model.SizeFrom.HasValue) ret = ret.Where(x => x.Map.MapWidth >= model.SizeFrom && x.Map.MapHeight >= model.SizeFrom);
             if (model.SizeTo.HasValue) ret = ret.Where(x => x.Map.MapWidth <= model.SizeTo && x.Map.MapHeight <= model.SizeTo);
 
+            if (model.SupportLevel.HasValue) ret = ret.Where(x => x.Map.MapSupportLevel >= model.SupportLevel);
+
             model.Data = ret;
 
             return View("LaddersMaps", model);
@@ -108,6 +110,8 @@ namespace ZeroKWeb.Controllers
 
             public int? SizeFrom { get; set; } = 0;
             public int? SizeTo { get; set; } = 1000;
+
+            public MapSupportLevel? SupportLevel { get; set; }
 
             public IQueryable<MapRatings.Rating> Data;
         }
