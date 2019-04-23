beb8f371b27b4b2169464531701530da809da6f2
DeinFreund <deinbestrfreund@gmail.com>
2019-04-23 11:43:18 +0200

    block polls when start is imminent, fix #2546

--- ZkLobbyServer/ServerBattle.cs
+++ ZkLobbyServer/ServerBattle.cs
@@ -36,7 +36,7 @@ namespace ZkLobbyServer
         private static string hostingIp;
 
 
-        public int DiscussionTime = 25;
+        public int DiscussionSeconds = 25;
         public readonly List<string> toNotify = new List<string>();
         public Resource HostedMap;
 
@@ -48,7 +48,7 @@ namespace ZkLobbyServer
         private int? dbAutohostIndex;
 
         protected bool isZombie;
-        protected bool IsPostBattleDiscussion => IsAutohost && DateTime.UtcNow.Subtract(EndedSince).TotalSeconds < DiscussionTime;
+        protected bool IsPollsBlocked => IsAutohost && DateTime.UtcNow < BlockPollsUntil;
 
         private List<KickedPlayer> kickedPlayers = new List<KickedPlayer>();
         public List<BattleDebriefing> Debriefings { get; private set; } = new List<BattleDebriefing>();
@@ -98,7 +98,7 @@ namespace ZkLobbyServer
             pollTimer.Enabled = false;
             pollTimer.AutoReset = false;
             pollTimer.Elapsed += pollTimer_Elapsed;
-            discussionTimer = new Timer(DiscussionTime * 1000);
+            discussionTimer = new Timer(DiscussionSeconds * 1000);
             discussionTimer.Enabled = false;
             discussionTimer.AutoReset = false;
             discussionTimer.Elapsed += discussionTimer_Elapsed;
@@ -427,9 +427,9 @@ namespace ZkLobbyServer
             else if (perm == BattleCommand.RunPermission.Vote)
             {
 
-                if (IsPostBattleDiscussion)
+                if (IsPollsBlocked)
                 {
-                    await Respond(e, "Please wait for a few seconds before starting a poll. Feel free to discuss the last battle.");
+                    await Respond(e, "Please wait for a few seconds before starting a poll.");
                     return false;
                 }
                 await StartVote(cmd, e, arg);
@@ -731,6 +731,12 @@ namespace ZkLobbyServer
             SaveToDb();
         }
 
+        public void BlockPolls(int seconds)
+        {
+            var target = DateTime.UtcNow.AddSeconds(seconds);
+            if (BlockPollsUntil < target) BlockPollsUntil = target;
+        }
+
 
         public void UpdateWith(Autohost autohost)
         {
@@ -868,7 +874,7 @@ namespace ZkLobbyServer
             StopVote();
             IsInGame = false;
             RunningSince = null;
-            EndedSince = DateTime.UtcNow;
+            BlockPollsUntil = DateTime.UtcNow.AddSeconds(DiscussionSeconds);
 
             bool result = BattleResultHandler.SubmitSpringBattleResult(springBattleContext, server, (debriefing) =>
             {
@@ -902,13 +908,13 @@ namespace ZkLobbyServer
                 var teamsQueues = server.MatchMaker.PossibleQueues.Where(x => x.Mode == AutohostMode.Teams).ToList();
                 var availableUsers = Users.Values.Where(x => !x.LobbyUser.IsAway && x.Name != null).Select(x => server.ConnectedUsers[x.Name]).ToList();
                 await server.MatchMaker.MassJoin(availableUsers, teamsQueues);
-                DiscussionTime = MatchMaker.TimerSeconds + 2;
+                DiscussionSeconds = MatchMaker.TimerSeconds + 2;
             }
             else
             {
-                DiscussionTime = 5;
+                DiscussionSeconds = 5;
             }
-            EndedSince = DateTime.UtcNow;
+            BlockPollsUntil = DateTime.UtcNow.AddSeconds(DiscussionSeconds);
 
 
             if (IsAutohost || (!Users.ContainsKey(FounderName) || Users[FounderName].LobbyUser?.IsAway == true) && Mode != AutohostMode.None && Mode != AutohostMode.Planetwars && !IsPassworded)
@@ -916,12 +922,12 @@ namespace ZkLobbyServer
                 if (!result)
                 {
                     //Game was aborted/exited/invalid, allow manual commands
-                    EndedSince = EndedSince.AddSeconds(-DiscussionTime);
+                    BlockPollsUntil = DateTime.UtcNow;
                 }
                 else
                 {
                     //Initiate discussion time, then map vote, then start vote
-                    discussionTimer.Interval = (DiscussionTime - 1) * 1000;
+                    discussionTimer.Interval = (DiscussionSeconds - 1) * 1000;
                     discussionTimer.Start();
                 }
             }
