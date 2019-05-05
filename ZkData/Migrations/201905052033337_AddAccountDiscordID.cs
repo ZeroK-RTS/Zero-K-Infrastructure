229175be78a6a2be20dd2a71c22e575e89deaa56
DeinFreund <deinbestrfreund@gmail.com>
2019-05-05 22:42:06 +0200

    fix migration because microsoft sql thinks NULL == NULL

--- ZkData/Migrations/201905052033337_AddAccountDiscordID.cs
+++ ZkData/Migrations/201905052033337_AddAccountDiscordID.cs
@@ -8,7 +8,11 @@ namespace ZkData.Migrations
         public override void Up()
         {
             AddColumn("dbo.Accounts", "DiscordID", c => c.Decimal(precision: 38, scale: 0));
-            CreateIndex("dbo.Accounts", "DiscordID", unique: true);
+            var indexName = "DiscordID";
+            var tableName = "dbo.Accounts";
+            var columnName = "DiscordID";
+
+            Sql(string.Format(@"CREATE UNIQUE NONCLUSTERED INDEX {0} ON {1}({2}) WHERE {2} IS NOT NULL;", indexName, tableName, columnName));
         }
         
         public override void Down()
