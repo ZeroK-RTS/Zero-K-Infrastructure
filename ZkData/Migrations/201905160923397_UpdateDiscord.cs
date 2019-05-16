37f2545d9b93f476d18b928a9b8e304d72f63612
DeinFreund <deinbestrfreund@gmail.com>
2019-05-16 11:25:52 +0200

    add migration

---
+++ ZkData/Migrations/201905160923397_UpdateDiscord.cs
@@ -0,0 +1,16 @@
+namespace ZkData.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class UpdateDiscord : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
