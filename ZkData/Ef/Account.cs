ccd902fa3f56b12503ec86b37b3da96bf513e51e
DeinFreund <deinbestrfreund@gmail.com>
2020-02-16 21:54:14 +0100

    External developer -> Content Contributor (more generic description)

--- ZkData/Ef/Account.cs
+++ ZkData/Ef/Account.cs
@@ -631,7 +631,7 @@ namespace ZkData
             donator_2 = 4,
             [Description("Diamond donator")]
             donator_3 = 8,
-            [Description("External developer")]
+            [Description("Content contributor")]
             dev_content =  5,
             [Description("Game developer")]
             dev_game = 6,
