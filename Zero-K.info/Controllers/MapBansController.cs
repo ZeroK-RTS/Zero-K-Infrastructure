7234c10e0b2b382b405c8846fbe607fa6ea2a331
Legomenon-gh <64239901+Legomenon-gh@users.noreply.github.com>
2020-06-18 13:51:07 -0700

    Code improvements for MM map banning
    - Fix where clause comparison when filtering maps by support level
    - Simplify MapBansIndex view and remove need for ViewModel
    - Add link to matchmaking map pool in view.

--- Zero-K.info/Controllers/MapBansController.cs
+++ Zero-K.info/Controllers/MapBansController.cs
@@ -16,18 +16,17 @@ namespace ZeroKWeb.Controllers
         public ActionResult Index()
         {
             var db = new ZkDataContext();
-            List<AccountMapBan> bans = db.AccountMapBans
+            List<Resource> bannedMaps = db.AccountMapBans
                 .Where(x => x.AccountID == Global.AccountID)
                 .OrderBy(x => x.Rank)
+                .ToList()
+                .Select(x => x.Resource)
                 .ToList();
 
-            var viewModel = new MapBansViewModel
-            {
-                accountBans = bans,
-                maxBans = MapBanConfig.GetMaxBanCount()
-            };
+            var unusedBans = MapBanConfig.GetMaxBanCount() - bannedMaps.Count;
+            bannedMaps.AddRange(Enumerable.Repeat(new Resource(), unusedBans));
 
-            return View("MapBansIndex", viewModel);
+            return View("MapBansIndex", bannedMaps);
         }
 
         [Auth]
@@ -36,29 +35,27 @@ namespace ZeroKWeb.Controllers
         /// </summary>
         /// 
 
-        public ActionResult Update(List<String> mapName)
+        public ActionResult Update(List<Resource> resources)
         {
-            if (mapName == null) return Content("No input given");
+            if (resources == null) return Content("No input given");
 
+            var names = resources.Select(x => x.InternalName).ToList();
             // Duplicates would not break anything but they're probably a sign of user error so validate against them.
-            var hasDuplicate = mapName.Where(x => x != "").GroupBy(x => x).Any(g => g.Count() > 1);
+            var hasDuplicate = names.Where(x => x != null).GroupBy(x => x).Any(g => g.Count() > 1);
             if (hasDuplicate)
             {
                 return Content("The same map cannot be banned multiple times.");
             }
 
-            // TODO: It would be nicer to use IDs instead of the map names to drive this
-            // Fetch the actual resources to sanity check user input and silently ignore
-            // any input that does not actually exist.
-            // Filter against current matchmaker to remove any existing bans for a map 
-            // that has been removed from the MM pool.
+            // Fetch the actual resources to sanity check user input and populate IDs for newly selected maps.
+            // Filter against current matchmaker to remove any existing bans for a map that has been removed from the MM pool.
             var db = new ZkDataContext();
             var mapIDs = db.Resources
-                .Where(x => x.TypeID == ResourceType.Map && mapName.Contains(x.InternalName) && x.MapSupportLevel == MapSupportLevel.MatchMaker)
+                .Where(x => x.TypeID == ResourceType.Map && names.Contains(x.InternalName) && x.MapSupportLevel == MapSupportLevel.MatchMaker)
                 .ToList();
 
             // Ban rank matters so sort the maps according to the user provided ordering
-            mapIDs = mapIDs.OrderBy(x => mapName.IndexOf(x.InternalName)).ToList();
+            mapIDs = mapIDs.OrderBy(x => names.IndexOf(x.InternalName)).ToList();
 
             var newMapBans = new List<AccountMapBan>();
             for (int i = 0; i < mapIDs.Count; i++)
@@ -81,11 +78,5 @@ namespace ZeroKWeb.Controllers
 
             return RedirectToAction("Index");
         }
-    }
-
-    public class MapBansViewModel
-    {
-        public List<AccountMapBan> accountBans;
-        public int maxBans;
     }
 }
