1ad88e9a7a36f91bdefc645cca29b3b907ad56ee
DeinFreund <deinbestrfreund@gmail.com>
2020-08-14 16:30:58 +0200

    try to only update rank and no other fields in whr

--- ZkData/Ef/WHR/WholeHistoryRating.cs
+++ ZkData/Ef/WHR/WholeHistoryRating.cs
@@ -584,14 +584,13 @@ namespace Ratings
                         Trace.TraceInformation("WHR Debriefing players: " + involvedAccounts.Values.Select(x => x.Name).StringJoin());
                         oldRanks = lastBattlePlayers.ToDictionary(p => p.AccountID, p => p.Account.Rank);
                         updatedRanks = lastBattlePlayers.Where(p => Ranks.UpdateRank(p.Account, p.IsInVictoryTeam, !p.IsInVictoryTeam, db)).Select(x => x.Account).ToDictionary(p => p.AccountID, p => p);
-                        updatedRanks.Values.ForEach(p => db.Entry(p).State = EntityState.Modified);
+                        updatedRanks.Values.ForEach(p => db.Entry(p).Property(x => x.Rank).IsModified = true);
                         playersWithRatingChange = lastBattlePlayers.Select(x => x.AccountID).ToList();
 
-                        lastBattlePlayers.Where(p => /*playerOldRatings.ContainsKey((p.AccountID)) && */ !p.EloChange.HasValue).ForEach(p =>
+                        lastBattlePlayers.Where(p => !p.EloChange.HasValue).ForEach(p =>
                         {
-                            //p.EloChange = playerRatings[(p.AccountID)].RealElo - playerOldRatings[(p.AccountID)].RealElo;
                             p.EloChange = playerRatings[(p.AccountID)].LadderElo - oldRatings[(p.AccountID)];
-                            db.Entry(p).State = EntityState.Modified;
+                            db.Entry(p).Property(x => x.EloChange).IsModified = true;
                         });
 
                         lastBattlePlayers.ForEach(x => playerOldRatings[x.AccountID] = playerRatings[x.AccountID]);
