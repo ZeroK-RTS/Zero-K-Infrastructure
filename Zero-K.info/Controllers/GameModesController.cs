8e7d0ef69937ddcff422d9f2dea6995fab7ae07c
Licho <licho@licho.eu>
2020-11-20 16:34:46 +0100

    add game mode controller

---
+++ Zero-K.info/Controllers/GameModesController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+using System.Web.Mvc;
+using ZkData;
+
+namespace ZeroKWeb.Controllers
+{
+    public class GameModesController: Controller
+    {
+        public class GameModesModel
+        {
+            public string SearchName { get; set; }
+            public bool? IsFeaturedFilter { get; set; }
+
+            public IQueryable<GameMode> Data;
+        }
+        
+        
+        // GET
+        public ActionResult Index(GameModesModel model)
+        {
+            model = model ?? new GameModesModel();
+            
+            
+            return View("GameModesIndex", model);
+        }
+    }
+}
