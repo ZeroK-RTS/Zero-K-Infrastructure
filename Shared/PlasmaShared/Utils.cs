e6dcc071ab2aadfff852bef7ff982391b5f2ef67
Licho <licho@licho.eu>
2020-12-20 12:31:59 +0100

    created basic rsa wrapper class + test

--- Shared/PlasmaShared/Utils.cs
+++ Shared/PlasmaShared/Utils.cs
@@ -938,6 +938,19 @@ namespace ZkData
             if (int.TryParse(value, out intval)) return intval;
             return null;
         }
+        
+        public static string Base64Encode(this string plainText)
+        {
+            var plainTextBytes = Encoding.UTF8.GetBytes(plainText);
+            return System.Convert.ToBase64String(plainTextBytes);
+        }
+
+        public static string Base64Decode(this string base64EncodedData)
+        {
+            var base64EncodedBytes = System.Convert.FromBase64String(base64EncodedData);
+            return Encoding.UTF8.GetString(base64EncodedBytes);
+        }
+        
     }
 
     public struct Indexed<T>
---
+++ Tests/TestRsa.cs
@@ -0,0 +1,41 @@
+﻿using NUnit.Framework;
+using PlasmaShared;
+
+namespace Tests
+{
+    [TestFixture]
+    public class TestRsa
+    {
+        [Test]
+        public void TestRsaSignatures() {
+            var keys = RsaSignatures.GenerateKeys();
+            
+            Assert.AreNotEqual(keys.privKey, keys.pubKey);
+            
+            Assert.IsTrue(keys.pubKey.Length < keys.privKey.Length);
+            
+            var dataToSign = "some super secret data we need to sign";
+
+            var signature = RsaSignatures.Sign(dataToSign, keys.privKey);
+
+
+            var resultOk = RsaSignatures.VerifySignature(dataToSign, signature, keys.pubKey);
+            Assert.AreEqual(true, resultOk);
+            
+            
+            var resultFail = RsaSignatures.VerifySignature(dataToSign + "modify", signature, keys.pubKey);
+            Assert.AreEqual(false, resultFail);
+            
+            
+            var dataToEncrypt = "some super secret data we need to encrypt";
+
+            var encrypted = RsaSignatures.Encrypt(dataToEncrypt, keys.pubKey);
+            
+            Assert.AreNotEqual(encrypted, dataToEncrypt);
+
+            var decrypted = RsaSignatures.Decrypt(encrypted, keys.privKey);
+            
+            Assert.AreEqual(decrypted, dataToEncrypt);        }
+
+    }
+}
