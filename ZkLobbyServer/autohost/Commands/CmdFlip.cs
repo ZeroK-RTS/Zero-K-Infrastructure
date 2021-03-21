0de69418e9a7b1949d2a8935d6d84cf04e8d8beb
Legomenon <64239901+Legomenon-gh@users.noreply.github.com>
2021-03-21 14:34:03 -0700

    Add CmdFlip and CmdRoll
    This allows to support randomizing outcomes within a game for
    tournament support, this is likely a temporary solution until bot
    support is available within a game lobby.

---
+++ ZkLobbyServer/autohost/Commands/CmdFlip.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading.Tasks;
+using LobbyClient;
+
+namespace ZkLobbyServer
+{
+    public class CmdFlip : BattleCommand
+    {
+        public override string Help => "Flips a coin";
+        public override string Shortcut => "flip";
+        public override AccessType Access => AccessType.NotIngame;
+
+        public override BattleCommand Create() => new CmdFlip();
+
+        public override string Arm(ServerBattle battle, Say e, string arguments = null)
+        {
+            return string.Empty;
+        }
+
+        public override async Task ExecuteArmed(ServerBattle battle, Say e)
+        {
+            string result = new Random().Next(2) == 1 ? "heads" : "tails";
+            await battle.SayBattle($"Flipped a coin, got {result}");
+        }
+    }
+}
---
+++ ZkLobbyServer/autohost/Commands/CmdRoll.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading.Tasks;
+using LobbyClient;
+
+namespace ZkLobbyServer
+{
+    public class CmdRoll : BattleCommand
+    {
+        public override string Help => "<N> - rolls a 1dN sided die";
+        public override string Shortcut => "roll";
+        public override AccessType Access => AccessType.NotIngame;
+
+        private int maximum;
+
+        public override BattleCommand Create() => new CmdRoll();
+
+        public override string Arm(ServerBattle battle, Say e, string arguments = null)
+        {
+            if (int.TryParse(arguments, out maximum)) return string.Empty;
+            else return null;
+        }
+
+        public override async Task ExecuteArmed(ServerBattle battle, Say e)
+        {
+            if (maximum >= 1)
+            {
+                var result = new Random().Next(1, maximum);
+                await battle.SayBattle($"Rolled 1d{maximum}, got {result}");
+            }
+        }
+    }
+}
