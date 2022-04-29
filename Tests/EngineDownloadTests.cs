46533a3ce426c0d6c48ecaad591fb7d298d5a13d
Licho <licho@licho.eu>
2022-04-29 11:29:30 +0200

    * add github action to test PR and update PR from master  * unit tests changed to mstest - all tests labeled with Category("Basic") are run each PR

--- Tests/EngineDownloadTests.cs
+++ Tests/EngineDownloadTests.cs
@@ -2,14 +2,16 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using NUnit.Framework;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using PlasmaDownloader;
 
 namespace Tests
 {
-    [TestFixture]
+    [TestClass]
     public class EngineDownloadTests
     {
+        [TestMethod]
+        [TestCategory("Basic")]
         public void GetDevelopList() {
             var list = EngineDownload.GetEngineList();
             Assert.IsTrue(list.Count > 1);
